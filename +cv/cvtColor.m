52909e299aacbabc116242c302e16118e4913c58
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-01 17:10:16 +0900

    Added cvtColor

---
+++ +cv/cvtColor.m
@@ -0,0 +1,45 @@
+%CVTCOLOR  Converts an image from one color space to another
+%
+%    dst = cvtColor(src, code)
+%    dst = cvtColor(src, code, 'DstCn', 1)
+%
+%  Input:
+%    src: Source image: 8-bit unsigned, 16-bit unsigned (CV_16UC...), or
+%		  single-precision floating-point.
+%    code: Color space conversion code string. e.g., 'RGB2GRAY'
+%  Output:
+%    dst: Destination image of the same size and depth as src.
+%  Options:
+%    'DstCn': Number of channels in the destination image. If the parameter is
+%             0, the number of the channels is derived automatically from src
+%             and code.
+%
+% The function converts an input image from one color space to another. In case
+% of a transformation to-from RGB color space, the order of the channels should
+% be specified explicitly (RGB or BGR). Note that the default color format in
+% OpenCV is often referred to as RGB but it is actually BGR (the bytes are
+% reversed). So the first byte in a standard (24-bit) color image will be an
+% 8-bit Blue component, the second byte will be Green, and the third byte will
+% be Red. The fourth, fifth, and sixth bytes would then be the second pixel
+% (Blue, then Green, then Red), and so on.
+%
+% The conventional ranges for R, G, and B channel values are:
+%
+%     0 to 255 for uint8 images
+%     0 to 65535 for uint16 images
+%     0 to 1 for float images
+% 
+% In case of linear transformations, the range does not matter. But in case of
+% a non-linear transformation, an input RGB image should be normalized to the
+% proper value range to get the correct results, for example, for RGB  L*u*v*
+% transformation. For example, if you have a 32-bit floating-point image
+% directly converted from an 8-bit image without any scaling, then it will have
+% the 0..255 value range instead of 0..1 assumed by the function. So, before
+% calling cvtColor, you need first to scale the image down:
+%
+%     img = cvtColor(img/255, 'BGR2Luv');
+%
+% If you use cvtColor with 8-bit images, the conversion will have some
+% information lost. For many applications, this will not be noticeable but it
+% is recommended to use 32-bit images in applications that need the full range
+% of colors or that convert an image before an operation and then convert back.
---
+++ test/unit_tests/TestCvtColor.m
@@ -0,0 +1,28 @@
+classdef TestCvtColor
+    %TestCvtColor
+    properties (Constant)
+        path = fileparts(fileparts(mfilename('fullpath')))
+        img = imread([TestCvtColor.path,filesep,'img001.jpg'])
+    end
+    
+    methods (Static)
+        function test_1
+            result = cvtColor(TestCvtColor.img, 'RGB2GRAY');
+        end
+        
+        function test_2
+            result = cvtColor(TestCvtColor.img, 'RGB2GRAY', 'DstCn', 0);
+        end
+        
+        function test_error_1
+            try
+                cvtColor();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+    
+end
+
