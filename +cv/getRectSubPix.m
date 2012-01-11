24f42bf2cae576135ee34b003a87dfec9613691e
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-11 15:23:46 -0500

    Added getRectSubPix

---
+++ +cv/getRectSubPix.m
@@ -0,0 +1,23 @@
+%GETRECTSUBPIX  Retrieves a pixel rectangle from an image with sub-pixel accuracy
+%
+%   dst = getRectSubPix(src, siz, center)
+%
+% Input:
+%     src: Source image.
+%     siz: Size of the extracted patch.
+%     center: Floating point coordinates of the center of the extracted
+%             rectangle within the source image. The center must be inside the
+%             image.
+% Output:
+%     dst: Extracted patch that has the size patchSize and the same number of
+%          channels as src.
+%
+% The function getRectSubPix extracts pixels from src:
+% 
+%   dst(x,y) = src(x+center.x-(dst.cols-1)*0.5, y+center.y-(dst.rows-1)*0.5))
+%
+% where the values of the pixels at non-integer coordinates are retrieved using
+% bilinear interpolation. Every channel of multi-channel images is processed
+% independently. While the center of the rectangle must be inside the image,
+% parts of the rectangle may be outside. In this case, the replication border
+% mode is used to extrapolate the pixel values outside of the image.
---
+++ test/unit_tests/TestGetRectSubPix.m
@@ -0,0 +1,25 @@
+classdef TestGetRectSubPix
+    %TestGetRectSubPix
+    properties (Constant)
+    end
+    
+    methods (Static)
+        function test_1
+        	src = single([0,0,0;0,1,1;0,1,1]);
+        	ref = single([0,0;0,0.5625]);
+        	dst = getRectSubPix(src,[2,2],[.25,.25]);
+        	assert(all(dst(:)==ref(:)));
+        end
+        
+        function test_error_1
+            try
+                getRectSubPix();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+    
+end
+
