8b9f032f2c4fee915bec46b382fb33e2676064a2
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-22 01:18:08 -0500

    Added NormalBayesClassifier

---
+++ +cv/NormalBayesClassifier.m
@@ -0,0 +1,121 @@
+classdef NormalBayesClassifier
+    %NormalBayesClassifier  Bayes classifier for normally distributed data
+    %
+    % This simple classification model assumes that feature vectors from
+    % each class are normally distributed (though, not necessarily
+    % independently distributed). So, the whole data distribution function
+    % is assumed to be a Gaussian mixture, one component per class. Using
+    % the training data the algorithm estimates mean vectors and covariance
+    % matrices for every class, and then it uses them for prediction.
+    %
+    % See also cv.NormalBayesClassifier.NormalBayesClassifier
+    % cv.NormalBayesClassifier.train cv.NormalBayesClassifier.predict
+    %
+    
+    properties (SetAccess = private)
+        id
+    end
+    
+    methods
+        function this = NormalBayesClassifier
+            %NORMALBAYESCLASSIFIER
+            %
+            %    classifier = cv.NormalBayesClassifier
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            this.id = NormalBayesClassifier_();
+        end
+        
+        function delete(this)
+            %DELETE  Destructor
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            NormalBayesClassifier_(this.id, 'delete');
+        end
+        
+        function clear(this)
+            %CLEAR  Deallocates memory and resets the model state
+            %
+            %    classifier.clear()
+            %
+            % The method clear does the same job as the destructor: it 
+            % deallocates all the memory occupied by the class members. But
+            % the object itself is not destructed and can be reused
+            % further. This method is called from the destructor, from the
+            % train() methods of the derived classes, from the methods
+            % load(), or even explicitly by the user.
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            NormalBayesClassifier_(this.id, 'clear');
+        end
+        
+        function save(this, filename)
+            %SAVE  Saves the model to a file
+            %
+            %    classifier.save(filename)
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            NormalBayesClassifier_(this.id, 'save', filename);
+        end
+        
+        function load(this, filename)
+            %LOAD  Loads the model from a file
+            %
+            %    classifier.load(filename)
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            NormalBayesClassifier_(this.id, 'load', filename);
+        end
+        
+        function status = train(this, trainData, responses, varargin)
+            %TRAIN  Trains the model
+            %
+            %    classifier.train(trainData, responses)
+            %    classifier.train(trainData, responses, 'OptionName', optionValue, ...)
+            %
+            % Options:
+            %     'VarIdx': Indicator variables (features) of interest.
+            %         Must have the same size to responses.
+            %     'SampleIdx': Indicator samples of interest. Must have the
+            %         the same size to responses.
+            %     'Update': Identifies whether the model should be trained
+            %         from scratch (update=false) or should be updated using
+            %         the new training data (update=true).
+            %
+            % The method trains the statistical model using a set of input
+            % feature vectors and the corresponding output values (responses).
+            % Both input and output vectors/values are passed as matrices.
+            % The input feature vectors are stored as trainData rows, that
+            % is, all the components (features) of a training vector are
+            % stored continuously.
+            %
+            % Responses are usually stored in a 1D vector (a row or a column)
+            % of integers.
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            status = NormalBayesClassifier_(this.id, 'train', trainData,...
+                responses, varargin{:});
+        end
+        
+        function results = predict(this, samples)
+            %PREDICT  Predicts the response for a sample
+            %
+            %    results = classifier.predict(samples)
+            %
+            % The method estimates the most probable classes for input vectors.
+            % Input vectors (one or more) are stored as rows of the matrix
+            % samples.
+            %
+            % See also cv.NormalBayesClassifier
+            %
+            results = NormalBayesClassifier_(this.id, 'predict', samples);
+        end
+    end
+    
+end
---
+++ src/+cv/private/NormalBayesClassifier_.cpp
@@ -0,0 +1,101 @@
+/**
+ * @file NormalBayesClassifier.cpp
+ * @brief mex interface for NormalBayesClassifier
+ * @author Kota Yamaguchi
+ * @date 2012
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+// Persistent objects
+
+/// Last object id to allocate
+int last_id = 0;
+/// Object container
+map<int,NormalBayesClassifier> obj_;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] )
+{
+	if (nlhs>1)
+        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    
+	// Determine argument format between constructor or (id,method,...)
+	vector<MxArray> rhs(prhs,prhs+nrhs);
+	int id = 0;
+	string method;
+	if (nrhs==0) {
+		// Constructor is called. Create a new object from argument
+		obj_[++last_id] = NormalBayesClassifier();
+		plhs[0] = MxArray(last_id);
+		return;
+	}
+	else if (rhs[0].isNumeric() && rhs[0].numel()==1 && nrhs>1) {
+		id = rhs[0].toInt();
+		method = rhs[1].toString();
+	}
+	else
+        mexErrMsgIdAndTxt("mexopencv:error","Invalid arguments");
+	
+	// Big operation switch
+	NormalBayesClassifier& obj = obj_[id];
+    if (method == "delete") {
+    	if (nrhs!=2 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Output not assigned");
+    	obj_.erase(id);
+    }
+    else if (method == "clear") {
+    	if (nrhs!=2 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.clear();
+    }
+    else if (method == "load") {
+    	if (nrhs!=3 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.load(rhs[2].toString().c_str());
+    }
+    else if (method == "save") {
+    	if (nrhs!=3 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.save(rhs[2].toString().c_str());
+    }
+    else if (method == "train") {
+    	if (nrhs<4 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat trainData(rhs[2].toMatND(CV_32F));
+    	Mat responses(rhs[3].toMatND(CV_32S));
+    	Mat varIdx;
+    	Mat sampleIdx;
+    	bool update=false;
+    	for (int i=4; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="VarIdx")
+    			varIdx = rhs[i].toMatND(CV_32S);
+    		else if (key=="VarIdx")
+    			sampleIdx = rhs[i].toMatND(CV_32S);
+    		else if (key=="Update")
+    			update = rhs[i].toBool();
+    		else
+    			mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
+    	}
+    	bool b = obj.train(trainData,responses,varIdx,sampleIdx,update);
+    	plhs[0] = MxArray(b);
+    }
+    else if (method == "predict") {
+    	if (nrhs<3 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat samples(rhs[2].toMatND(CV_32F)),results;
+    	obj.predict(samples,&results);
+    	plhs[0] = MxArray(results);
+    }
+    else
+		mexErrMsgIdAndTxt("mexopencv:error","Unrecognized operation");
+}
