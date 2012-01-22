8367e8a7ea6af5047152d8ab262707060c80181a
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-22 13:11:15 -0500

    Added KNearest

---
+++ +cv/KNearest.m
@@ -0,0 +1,163 @@
+classdef KNearest
+    %KNearest  The class implements K-Nearest Neighbors model
+    %
+    % The algorithm caches all training samples and predicts the response for a
+    % new sample by analyzing a certain number (K) of the nearest neighbors of
+    % the sample using voting, calculating weighted sum, and so on. The method
+    % is sometimes referred to as “learning by example” because for prediction
+    % it looks for the feature vector with a known response that is closest to
+    % the given vector.
+    %
+    % See also cv.KNearest.KNearest
+    % cv.KNearest.train cv.KNearest.find_nearest
+    %
+    
+    properties (SetAccess = private)
+        id
+    end
+    
+    properties (SetAccess = private, Dependent)
+        MaxK
+        VarCount
+        SampleCount
+        IsRegression
+    end
+    
+    methods
+        function this = KNearest
+            %KNEAREST  K-Nearest Neighbors constructor
+            %
+            %    classifier = cv.KNearest
+            %
+            % See also cv.KNearest
+            %
+            this.id = KNearest_();
+        end
+        
+        function delete(this)
+            %DELETE  Destructor
+            %
+            % See also cv.KNearest
+            %
+            KNearest_(this.id, 'delete');
+        end
+        
+        function clear(this)
+            %CLEAR  Deallocates memory and resets the model state
+            %
+            %    classifier.clear()
+            %
+            % The method clear does the same job as the destructor: it 
+            % deallocates all the memory occupied by the class members. But
+            % the object itself is not destructed and can be reused
+            % further. This method is called from the destructor, from the
+            % train() methods of the derived classes, from the methods
+            % load(), or even explicitly by the user.
+            %
+            % See also cv.KNearest
+            %
+            KNearest_(this.id, 'clear');
+        end
+        
+        function save(this, filename)
+            %SAVE  Saves the model to a file
+            %
+            %    classifier.save(filename)
+            %
+            % See also cv.KNearest
+            %
+            KNearest_(this.id, 'save', filename);
+        end
+        
+        function load(this, filename)
+            %LOAD  Loads the model from a file
+            %
+            %    classifier.load(filename)
+            %
+            % See also cv.KNearest
+            %
+            KNearest_(this.id, 'load', filename);
+        end
+        
+        function status = train(this, trainData, responses, varargin)
+            %TRAIN  Trains the model
+            %
+            %    classifier.train(trainData, responses)
+            %    classifier.train(trainData, responses, 'OptionName', optionValue, ...)
+            %
+            % Options:
+            %     'IsRegression': Type of the problem: true for regression and
+            %         false for classification.
+            %     'MaxK': Number of maximum neighbors that may be passed to the
+            %         method find_nearest.
+            %     'SampleIdx': Indicator samples of interest. Must have the
+            %         the same size to responses.
+            %     'UpdateBase': Specifies whether the model is trained from
+            %         scratch (update_base=false), or it is updated using the
+            %         new training data (update_base=true). In the latter case,
+            %         the parameter maxK must not be larger than the original
+            %         value.
+            %
+            % The method trains the K-Nearest model.
+            %
+            % See also cv.KNearest
+            %
+            status = KNearest_(this.id, 'train', trainData,...
+                responses, varargin{:});
+        end
+        
+        function results = find_nearest(this, samples, varargin)
+            %FIND_NEAREST  Finds the neighbors and predicts responses for input vectors
+            %
+            %    results = classifier.find_nearest(samples)
+            %    results = classifier.find_nearest(samples, 'OptionName', optionValue, ...)
+            %
+            % For each input vector (a row of the matrix samples), the method
+            % finds the k nearest neighbors. In case of regression, the
+            % predicted result is a mean value of the particular vector’s
+            % neighbor responses. In case of classification, the class is
+            % determined by voting.
+            %
+            % For each input vector, the neighbors are sorted by their distances
+            % to the vector.
+            %
+            % See also cv.KNearest
+            %
+            results = KNearest_(this.id, 'find_nearest', samples, varargin{:});
+        end
+        
+        function results = predict(this, varargin)
+            %PREDICT  Predicts the response for a sample
+            %
+            %    results = classifier.predict(samples)
+            %    results = classifier.predict(samples, 'OptionName', optionValue, ...)
+            %
+            % The method is an alias for find_nearest
+            %
+            % See also cv.KNearest.find_nearest
+            %
+            results = this.find_nearest(varargin{:});
+        end
+        
+        function value = get.MaxK(this)
+        	%MAXK
+        	value = KNearest_(this.id, 'get_max_k');
+        end
+        
+        function value = get.VarCount(this)
+        	%VARCOUNT
+        	value = KNearest_(this.id, 'get_var_count');
+        end
+        
+        function value = get.SampleCount(this)
+        	%SAMPLECOUNT
+        	value = KNearest_(this.id, 'get_sample_count');
+        end
+        
+        function value = get.IsRegression(this)
+        	%ISREGRESSION
+        	value = KNearest_(this.id, 'is_regression');
+        end
+    end
+    
+end
--- src/+cv/private/KNearest_.cpp
+++ src/+cv/private/KNearest_.cpp
@@ -1,6 +1,6 @@
 /**
- * @file NormalBayesClassifier.cpp
- * @brief mex interface for NormalBayesClassifier
+ * @file KNearest.cpp
+ * @brief mex interface for KNearest
  * @author Kota Yamaguchi
  * @date 2012
  */
@@ -13,7 +13,7 @@ using namespace cv;
 /// Last object id to allocate
 int last_id = 0;
 /// Object container
-map<int,NormalBayesClassifier> obj_;
+map<int,KNearest> obj_;
 
 /**
  * Main entry called from Matlab
@@ -34,7 +34,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
 	string method;
 	if (nrhs==0) {
 		// Constructor is called. Create a new object from argument
-		obj_[++last_id] = NormalBayesClassifier();
+		obj_[++last_id] = KNearest();
 		plhs[0] = MxArray(last_id);
 		return;
 	}
@@ -46,7 +46,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
         mexErrMsgIdAndTxt("mexopencv:error","Invalid arguments");
 	
 	// Big operation switch
-	NormalBayesClassifier& obj = obj_[id];
+	KNearest& obj = obj_[id];
     if (method == "delete") {
     	if (nrhs!=2 || nlhs!=0)
     		mexErrMsgIdAndTxt("mexopencv:error","Output not assigned");
@@ -72,29 +72,65 @@ void mexFunction( int nlhs, mxArray *plhs[],
     		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
     	Mat trainData(rhs[2].toMatND(CV_32F));
     	Mat responses(rhs[3].toMatND(CV_32S));
-    	Mat varIdx;
     	Mat sampleIdx;
-    	bool update=false;
+    	bool isRegression=false;
+    	int maxK=32;
+    	bool updateBase=false;
     	for (int i=4; i<nrhs; i+=2) {
     		string key(rhs[i].toString());
-    		if (key=="VarIdx")
-    			varIdx = rhs[i].toMatND(CV_32S);
-    		else if (key=="SampleIdx")
+    		if (key=="SampleIdx")
     			sampleIdx = rhs[i].toMatND(CV_32S);
-    		else if (key=="Update")
-    			update = rhs[i].toBool();
+    		else if (key=="IsRegression")
+    			isRegression = rhs[i].toBool();
+    		else if (key=="MaxK")
+    			maxK = rhs[i].toInt();
+    		else if (key=="Update"||key=="UpdateBase")
+    			updateBase = rhs[i].toBool();
     		else
     			mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     	}
-    	bool b = obj.train(trainData,responses,varIdx,sampleIdx,update);
+    	bool b = obj.train(trainData,responses,sampleIdx,isRegression,maxK,updateBase);
     	plhs[0] = MxArray(b);
     }
-    else if (method == "predict") {
-    	if (nrhs<3 || nlhs>1)
+    else if (method == "find_nearest") {
+    	if (nrhs<3 || nlhs>3)
     		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
     	Mat samples(rhs[2].toMatND(CV_32F)),results;
-    	obj.predict(samples,&results);
+    	int k=1;
+    	for (int i=3; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="K")
+    			k = rhs[i].toInt();
+    		else
+    			mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
+    	}
+    	Mat neighborResponses, dists;
+    	obj.find_nearest(samples,k,results,neighborResponses,dists);
     	plhs[0] = MxArray(results);
+    	if (nlhs>1)
+    		plhs[1] = MxArray(neighborResponses);
+    	if (nlhs>2)
+    		plhs[2] = MxArray(dists);
+    }
+    else if (method == "get_max_k") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.get_max_k());
+    }
+    else if (method == "get_var_count") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.get_var_count());
+    }
+    else if (method == "get_sample_count") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.get_sample_count());
+    }
+    else if (method == "is_regression") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(static_cast<bool>(obj.is_regression()));
     }
     else
 		mexErrMsgIdAndTxt("mexopencv:error","Unrecognized operation");
