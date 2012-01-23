a19fe964d8bc982cb1d067029e4043cbb57433e7
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-23 17:48:31 -0500

    Updated classification demo

--- samples/classification_demo.m
+++ samples/classification_demo.m
@@ -16,20 +16,22 @@ test_idx = mod(1:numel(Y),3)==0;
 % Try ANN_MLP
 ann_mlp_demo(cv.ANN_MLP([5,2]), X, Y, test_idx);
 
-% Try trees
-trees = {cv.DTree, cv.Boost, cv.RTrees, cv.ERTrees};
-for i=1:numel(trees)
-    trees_demo(trees{i}, X, Y, test_idx);
+% Try other classifiers
+classifiers = {...
+	cv.DTree,   {'VarType', 'Categorical'},...
+	cv.Boost,   {'VarType', 'Categorical'},...
+	cv.RTrees,  {'VarType', 'Categorical'},...
+	cv.ERTrees, {'VarType', 'Categorical'},...
+...% cv.GBTrees, {'VarType', 'Categorical', 'LossFunctionType', 'Deviance'},...
+	cv.NormalBayesClassifier, {},...
+	cv.KNearest, {},...
+	cv.SVM, {}...
+};
+for i = 1:2:numel(classifiers)
+    classifier_demo(classifiers{i}, X, Y, test_idx, classifiers{i+1});
 end
 
-% Try Others
-classifiers = {cv.NormalBayesClassifier, cv.KNearest, cv.SVM};
-for i=1:numel(classifiers)
-    classifier_demo(classifiers{i}, X, Y, test_idx);
 end
-
-end
-
 
 function ann_mlp_demo(classifier, X, Y, test_idx)
 %ANN_MLP_DEMO
@@ -51,28 +53,12 @@ fprintf('Accuracy: %f\n',accuracy);
 
 end
 
-function trees_demo(classifier, X, Y, test_idx)
-%TREES_DEMO
-
-fprintf('=== %s ===\n',class(classifier));
-tic;
-% 'VarType' option specifies a classification problem in trees
-classifier.train(X(~test_idx,:),Y(~test_idx),'VarType','Categorical');
-fprintf('Training time %f seconds\n',toc);
-tic;
-Yhat = classifier.predict(X(test_idx,:));
-fprintf('Prediction time %f seconds\n',toc);
-accuracy = nnz(Yhat==Y(test_idx)) / nnz(test_idx);
-fprintf('Accuracy: %f\n',accuracy);
-
-end
-
-function classifier_demo(classifier, X, Y, test_idx)
+function classifier_demo(classifier, X, Y, test_idx, option)
 %CLASSIFIER_DEMO
 
 fprintf('=== %s ===\n',class(classifier));
 tic;
-classifier.train(X(~test_idx,:),Y(~test_idx));
+classifier.train(X(~test_idx,:),Y(~test_idx), option{:});
 fprintf('Training time %f seconds\n',toc);
 tic;
 Yhat = classifier.predict(X(test_idx,:));
