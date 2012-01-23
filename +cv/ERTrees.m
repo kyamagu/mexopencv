397e88d1f7282eb6665e8b14af4299b62d251dd9
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-23 02:11:24 -0500

    Added ERTrees

---
+++ +cv/ERTrees.m
@@ -0,0 +1,247 @@
+classdef ERTrees
+    %ERTREES  Extremely randomized trees
+    %
+    % Extremely randomized trees have been introduced by Pierre Geurts, Damien
+    % Ernst and Louis Wehenkel in the article “Extremely randomized trees”,
+    % 2006 [http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.65.7485&rep=rep1&type=pdf].
+    % The algorithm of growing Extremely randomized trees is similar to Random
+    % Trees (Random Forest), but there are two differences:
+    %
+    %   1. Extremely randomized trees don’t apply the bagging procedure to
+    %      constract the training samples for each tree. The same input training
+    %      set is used to train all trees.
+    %   2. Extremely randomized trees pick a node split very extremely (both a
+    %      variable index and variable spliting value are chosen randomly),
+    %      whereas Random Forest finds the best split (optimal one by variable
+    %      index and variable spliting value) among random subset of variables.
+    %
+    % See also cv.RTrees cv.ERTrees.ERTrees cv.ERTrees.train cv.ERTrees.predict
+    %
+    
+    properties (SetAccess = private)
+        id
+    end
+    
+    properties (SetAccess = private, Dependent)
+    end
+    
+    methods
+        function this = ERTrees
+            %ERTREES  Random Trees classifier
+            %
+            %    classifier = cv.ERTrees
+            %
+            % See also cv.ERTrees
+            %
+            this.id = ERTrees_();
+        end
+        
+        function delete(this)
+            %DELETE  Destructor
+            %
+            % See also cv.ERTrees
+            %
+            ERTrees_(this.id, 'delete');
+        end
+        
+        function clear(this)
+            %CLEAR  Deallocates memory and resets the model state
+            %
+            %    classifier.clear()
+            %
+            % The method clear does the same job as the destructor: it 
+            % deallocates all the memory occupied by the class members. But
+            % the object itself is not destructed and can be reused
+            % further. This method is called from the destructor, from the
+            % train() methods of the derived classes, from the methods
+            % load(), or even explicitly by the user.
+            %
+            % See also cv.ERTrees
+            %
+            ERTrees_(this.id, 'clear');
+        end
+        
+        function save(this, filename)
+            %SAVE  Saves the model to a file
+            %
+            %    classifier.save(filename)
+            %
+            % See also cv.ERTrees
+            %
+            ERTrees_(this.id, 'save', filename);
+        end
+        
+        function load(this, filename)
+            %LOAD  Loads the model from a file
+            %
+            %    classifier.load(filename)
+            %
+            % See also cv.ERTrees
+            %
+            ERTrees_(this.id, 'load', filename);
+        end
+        
+        function status = train(this, trainData, responses, varargin)
+            %TRAIN  Trains a Gradient boosted tree model
+            %
+            %    classifier.train(trainData, responses)
+            %    classifier.train(trainData, responses, 'OptionName', optionValue, ...)
+            %
+            % Input:
+            %     trainData: Row vectors of feature.
+            %     responses: Output of the corresponding feature vectors.
+            % Options:
+            %     'VarIdx': Indicator variables (features) of interest.
+            %         Must have the same size to responses.
+            %     'SampleIdx': Indicator samples of interest. Must have the
+            %         the same size to responses.
+            %     'VarType': 
+			%     'MissingMask': 
+			%     'MaxDepth': The maximum possible depth of the tree. That is
+			%         the training algorithms attempts to split a node while its
+			%         depth is less than max_depth. The actual depth may be
+			%         smaller if the other termination criteria are met, and/or
+			%         if the tree is pruned. default 5.
+			%     'MinSampleCount': If the number of samples in a node is less
+			%         than this parameter then the node will not be splitted.
+			%         default 10.
+			%     'RegressionAccuracy': Termination criteria for regression
+			%         trees. If all absolute differences between an estimated
+			%         value in a node and values of train samples in this node
+			%         are less than this parameter then the node will not be
+			%         splitted. default 0.
+			%     'UseSurrogates': If true then surrogate splits will be built.
+			%         These splits allow to work with missing data and compute
+			%         variable importance correctly. default false.
+			%     'MaxCategories': Cluster possible values of a categorical
+			%         variable into K <= MaxCategories clusters to find a
+			%         suboptimal split. If a discrete variable, on which the
+			%         training procedure tries to make a split, takes more than
+			%         MaxCategories values, the precise best subset estimation
+			%         may take a very long time because the algorithm is
+			%         exponential. Instead, many decision trees engines
+			%         (including ML) try to find sub-optimal split in this case
+			%         by clustering all the samples into max_categories clusters
+			%         that is some categories are merged together. The
+			%         clustering is applied only in n>2-class classification
+			%         problems for categorical variables with N > max_categories
+			%         possible values. In case of regression and 2-class
+			%         classification the optimal split can be found efficiently
+			%         without employing clustering, thus the parameter is not
+			%         used in these cases. default 0.
+			%     'Priors': The array of a priori class probabilities, sorted by
+			%         the class label value. The parameter can be used to tune
+			%         the decision tree preferences toward a certain class. For
+			%         example, if you want to detect some rare anomaly
+			%         occurrence, the training base will likely contain much
+			%         more normal cases than anomalies, so a very good
+			%         classification performance will be achieved just by
+			%         considering every case as normal. To avoid this, the
+			%         priors can be specified, where the anomaly probability is
+			%         artificially increased (up to 0.5 or even greater), so the
+			%         weight of the misclassified anomalies becomes much bigger,
+			%         and the tree is adjusted properly. You can also think
+			%         about this parameter as weights of prediction categories
+			%         which determine relative weights that you give to
+			%         misclassification. That is, if the weight of the first
+			%         category is 1 and the weight of the second category is 10,
+			%         then each mistake in predicting the second category is
+			%         equivalent to making 10 mistakes in predicting the first
+			%         category. default none.
+			%     'calc_var_importance': If true then variable importance will
+			%         be calculated and then it can be retrieved by
+			%         getVarImportance(). default false.
+			%     'NActiveVars': The size of the randomly selected subset of
+			%         features at each tree node and that are used to find the
+			%         best split(s). If you set it to 0 then the size will be
+			%         set to the square root of the total number of features.
+			%         default 0.
+			%     'MaxNumOfTreesInTheForest': The maximum number of trees in the
+			%         forest (suprise, suprise). default 50.
+			%     'ForestAccuracy': Sufficient accuracy (OOB error). default 0.1
+			%     'TermCritType': The type of the termination criteria. One of
+			%         'Iter', 'EPS', or 'Iter+EPS'. default 'Iter+EPS'
+            %
+            % The method trains the ERTrees model.
+            %
+            % See also cv.ERTrees cv.ERTrees.predict cv.ERTrees.predict_prob
+            %
+            status = ERTrees_(this.id, 'train', trainData, responses, varargin{:});
+        end
+        
+        function results = predict(this, samples, varargin)
+            %PREDICT  Predicts a response for an input sample
+            %
+            %    results = classifier.predict(samples)
+            %
+            % Input:
+            %     samples: Input row vectors
+            % Output:
+            %     results: Output labels or regression values
+            % Options:
+            %     'MissingDataMask':  Missing values mask, which is a
+            %         dimentional matrix of the same size as sample having the
+            %         uint8 type. 1 corresponds to the missing value in the same
+            %         position in the sample vector. If there are no missing
+            %         values in the feature vector, an empty matrix can be
+            %         passed instead of the missing mask. default none.
+            %
+            % This method returns the cumulative result from all the trees in
+            % the forest (the class that receives the majority of voices, or the
+            % mean of the regression function estimates).
+            %
+            % See also cv.ERTrees cv.ERTrees.train cv.ERTrees.predict_prob
+            %
+            results = ERTrees_(this.id, 'predict', samples, varargin{:});
+        end
+        
+        function results = predict_prob(this, samples, varargin)
+            %PREDICT_PROB  Returns a fuzzy-predicted class label
+            %
+            %    results = classifier.predict(samples)
+            %
+            % Input:
+            %     samples: Input row vectors
+            % Output:
+            %     results: Output labels or regression values
+            % Options:
+            %     'MissingDataMask':  Missing values mask, which is a
+            %         dimentional matrix of the same size as sample having the
+            %         uint8 type. 1 corresponds to the missing value in the same
+            %         position in the sample vector. If there are no missing
+            %         values in the feature vector, an empty matrix can be
+            %         passed instead of the missing mask. default none.
+            %
+            % The function works for binary classification problems only. It
+            % returns the number between 0 and 1. This number represents
+            % probability or confidence of the sample belonging to the second
+            % class. It is calculated as the proportion of decision trees that
+            % classified the sample to the second class.
+            %
+            % See also cv.ERTrees cv.ERTrees.train cv.ERTrees.predict
+            %
+            results = ERTrees_(this.id, 'predict_prob', samples, varargin{:});
+        end
+        
+        function value = getVarImportance(this)
+        	%GETVARIMPORTANCE  Returns the variable importance array
+        	value = ERTrees_(this.id, 'getVarImportance');
+        end
+        
+        function value = get_proximity(this, sample1, sample2, varargin)
+        	%GET_PROXIMITY  Returns the variable importance array
+        	value = ERTrees_(this.id, 'get_proximity', sample1, sample2, varargin{:});
+        end
+        
+        function value = get_train_error(this)
+        	%GET_TRAIN_ERROR  Returns the variable importance array
+        	value = ERTrees_(this.id, 'get_train_error');
+        end
+        
+        function value = get_tree_count(this)
+        	%GET_TREE_COUNT  Returns the variable importance array
+        	value = ERTrees_(this.id, 'get_tree_count');
+        end
+    end
+    
+end
---
+++ src/+cv/private/ERTrees_.cpp
@@ -0,0 +1,225 @@
+/**
+ * @file ERTrees.cpp
+ * @brief mex interface for ERTrees
+ * @author Kota Yamaguchi
+ * @date 2012
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+// Persistent objects
+namespace {
+/// Last object id to allocate
+int last_id = 0;
+/// Object container
+map<int,CvERTrees> obj_;
+
+/** Obtain CvRTParams object from input arguments
+ * @param it iterator at the beginning of the argument vector
+ * @param end iterator at the end of the argument vector
+ * @return CvRTParams objects
+ */
+CvRTParams getParams(vector<MxArray>::iterator it,
+						vector<MxArray>::iterator end)
+{
+	CvRTParams params;
+	for (;it<end;it+=2) {
+		string key((*it).toString());
+		MxArray& val = *(it+1);
+		if (key=="MaxDepth")
+			params.max_depth = val.toInt();
+		else if (key=="MinSampleCount")
+			params.min_sample_count = val.toInt();
+		else if (key=="RegressionAccuracy")
+			params.regression_accuracy = val.toDouble();
+		else if (key=="UseSurrogates")
+			params.use_surrogates = val.toBool();
+		else if (key=="MaxCategories")
+			params.max_categories = val.toInt();
+		else if (key=="CalcVarImportance")
+			params.calc_var_importance = val.toBool();
+		else if (key=="NActiveVars")
+			params.nactive_vars = val.toInt();
+		else if (key=="MaxNumOfTreesInTheForest")
+			params.term_crit.max_iter = val.toInt();
+		else if (key=="ForestAccuracy")
+			params.term_crit.epsilon = val.toDouble();
+		else if (key=="TermCritType") {
+			if (val.isChar()) {
+				string s(val.toString());
+				if (s=="Iter")
+					params.term_crit.type = CV_TERMCRIT_ITER;
+				else if (s=="EPS")
+					params.term_crit.type = CV_TERMCRIT_EPS;
+				else if (s=="Iter+EPS")
+					params.term_crit.type = CV_TERMCRIT_ITER|CV_TERMCRIT_EPS;
+				else
+					mexErrMsgIdAndTxt("mexopencv:error","Unrecognized TermCritType");
+			}
+			else
+				params.term_crit.type = val.toInt();
+		}
+		//else
+		//	mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
+	}
+	return params;
+}
+
+}
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] )
+{
+	if (nlhs>1)
+        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    
+	// Determine argument format between constructor or (id,method,...)
+	vector<MxArray> rhs(prhs,prhs+nrhs);
+	int id = 0;
+	string method;
+	if (nrhs==0) {
+		// Constructor is called. Create a new object from argument
+		obj_[++last_id] = CvERTrees();
+		plhs[0] = MxArray(last_id);
+		return;
+	}
+	else if (rhs[0].isNumeric() && rhs[0].numel()==1 && nrhs>1) {
+		id = rhs[0].toInt();
+		method = rhs[1].toString();
+	}
+	else
+        mexErrMsgIdAndTxt("mexopencv:error","Invalid arguments");
+	
+	// Big operation switch
+	CvERTrees& obj = obj_[id];
+    if (method == "delete") {
+    	if (nrhs!=2 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Output not assigned");
+    	obj_.erase(id);
+    }
+    else if (method == "clear") {
+    	if (nrhs!=2 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.clear();
+    }
+    else if (method == "load") {
+    	if (nrhs!=3 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.load(rhs[2].toString().c_str());
+    }
+    else if (method == "save") {
+    	if (nrhs!=3 || nlhs!=0)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	obj.save(rhs[2].toString().c_str());
+    }
+    else if (method == "train") {
+    	if (nrhs<4 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat trainData(rhs[2].toMatND(CV_32F));
+    	Mat responses(rhs[3].toMatND(CV_32S));
+    	Mat varIdx, sampleIdx, varType, missingMask;
+    	CvRTParams params = getParams(rhs.begin()+4,rhs.end());
+    	vector<float> priors;
+    	for (int i=4; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="VarIdx")
+    			varIdx = rhs[i+1].toMatND(CV_32S);
+    		else if (key=="SampleIdx")
+    			sampleIdx = rhs[i+1].toMatND(CV_32S);
+    		else if (key=="VarType")
+    			varType = rhs[i+1].toMatND();
+    		else if (key=="MissingMask")
+    			missingMask = rhs[i+1].toMatND();
+    		else if (key=="Priors") {
+    			MxArray& m = rhs[i+1];
+				priors.reserve(m.numel());
+    			for (int j=0; j<m.numel(); ++j)
+    				priors.push_back(m.at<float>(j));
+    			params.priors = &priors[0];
+    		}
+    	}
+    	bool b = obj.train(trainData, CV_ROW_SAMPLE, responses, varIdx,
+    		sampleIdx, varType, missingMask, params);
+    	plhs[0] = MxArray(b);
+    }
+    else if (method == "predict") {
+    	if (nrhs<3 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat samples(rhs[2].toMatND(CV_32F)), missing;
+    	for (int i=3; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="MissingDataMask")
+    			missing = rhs[i+1].toMatND(CV_8U);
+    	}
+		Mat results(samples.rows,1,CV_64F);
+		if (missing.empty())
+			for (int i=0; i<samples.rows; ++i)
+				results.at<double>(i) = obj.predict(samples.row(i),missing);
+		else
+			for (int i=0; i<samples.rows; ++i)
+				results.at<double>(i) = obj.predict(samples.row(i),missing.row(i));
+		plhs[0] = MxArray(results);
+    }
+    else if (method == "predict_prob") {
+    	if (nrhs<3 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat samples(rhs[2].toMatND(CV_32F)), missing;
+    	for (int i=3; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="MissingDataMask")
+    			missing = rhs[i+1].toMatND(CV_8U);
+    	}
+		Mat results(samples.rows,1,CV_64F);
+		if (missing.empty())
+			for (int i=0; i<samples.rows; ++i)
+				results.at<double>(i) = obj.predict_prob(samples.row(i),missing);
+		else
+			for (int i=0; i<samples.rows; ++i)
+				results.at<double>(i) = obj.predict_prob(samples.row(i),missing.row(i));
+		plhs[0] = MxArray(results);
+    }
+    else if (method == "getVarImportance") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.getVarImportance());
+    }
+    else if (method == "get_proximity") {
+    	if (nrhs<4 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	Mat sample1(rhs[2].toMatND(CV_32F)), sample2(rhs[3].toMatND(CV_32F));
+    	Mat missing1, missing2;
+    	for (int i=4; i<nrhs; i+=2) {
+    		string key(rhs[i].toString());
+    		if (key=="Missing1")
+    			missing1 = rhs[i+1].toMatND(CV_8U);
+    		else if (key=="Missing2")
+    			missing2 = rhs[i+1].toMatND(CV_8U);
+    	}
+		CvMat _sample1 = sample1, _sample2 = sample2;
+		CvMat _missing1 = missing1, _missing2 = missing2;
+		float x = obj.get_proximity(&_sample1, &_sample2,
+			(missing1.empty()) ? NULL : &_missing1,
+			(missing2.empty()) ? NULL : &_missing2);
+		plhs[0] = MxArray(x);
+    }
+    else if (method == "get_train_error") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.get_train_error());
+    }
+    else if (method == "get_tree_count") {
+    	if (nrhs!=2 || nlhs>1)
+    		mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    	plhs[0] = MxArray(obj.get_tree_count());
+    }
+    else
+		mexErrMsgIdAndTxt("mexopencv:error","Unrecognized operation");
+}
