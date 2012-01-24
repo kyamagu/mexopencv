61906c27f9b5ead580bd01730cc46d0b3f3a4c5e
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-24 13:05:17 -0500

    Added cornerSubPix

---
+++ +cv/cornerSubPix.m
@@ -0,0 +1,27 @@
+%CORNERSUBPIX  Refines the corner locations
+%
+%   corners = cv.cornerSubPix(im, corners)
+%   corners = cv.cornerSubPix(im, corners, 'OptionName', optionValue, ...)
+%
+% Input:
+%     im: Input image.
+%     corners: Initial coordinates of the input corners.
+% Output:
+%     corners: Output refined coordinates.
+% Options:
+%     'WinSize': Half of the side length of the search window. For example, if
+%         winSize=Size(3,3), then a 3 * 2 + 1 x 3 * 2 + 1 = 7 x 7 search window
+%         is used. default [3, 3].
+%     'ZeroZone':  Half of the size of the dead region in the middle of the
+%         search zone over which the summation in the formula below is not done.
+%         It is used sometimes to avoid possible singularities of the
+%         autocorrelation matrix. The value of (-1,-1) indicates that there is
+%         no such a size. default [-1,-1].
+%     'Criteria': Criteria for termination of the iterative process of corner
+%         refinement. That is, the process of corner position refinement stops
+%         either after criteria.maxCount iterations or when the corner position
+%         moves by less than criteria.epsilon on some iteration.
+%
+% The function iterates to find the sub-pixel accurate location of corners or
+% radial saddle points.
+%
---
+++ src/+cv/cornerSubPix.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file cornerSubPix.cpp
+ * @brief mex interface for cornerSubPix
+ * @author Kota Yamaguchi
+ * @date 2011
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] )
+{
+	// Check the number of arguments
+	if (nrhs<2 || ((nrhs%2)!=0) || nlhs>1)
+        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    
+	// Argument vector
+	vector<MxArray> rhs(prhs,prhs+nrhs);
+	
+	// Option processing
+	Size winSize(3,3);
+	Size zeroZone(-1,-1);
+	TermCriteria criteria(TermCriteria::COUNT+TermCriteria::EPS,50,0.001);
+	for (int i=2; i<nrhs; i+=2) {
+		string key = rhs[i].toString();
+		if (key=="WinSize")
+			winSize = rhs[i+1].toSize();
+		else if (key=="ZeroZone")
+			zeroZone = rhs[i+1].toSize();
+		else if (key=="Criteria")
+			criteria = rhs[i+1].toTermCriteria();
+		else
+			mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
+	}
+	
+	// Process
+	Mat src = (rhs[0].isUint8()) ? rhs[0].toMat(CV_8U) : rhs[0].toMat(CV_32F);
+	vector<MxArray> vm(rhs[1].toStdVector<MxArray>());
+	vector<Point2f> corners(vm.size());
+	for (int i=0; i<vm.size(); ++i)
+		corners[i] = vm[i].toPoint_<float>();
+	cornerSubPix(src, corners, winSize, zeroZone, criteria);
+	plhs[0] = MxArray(corners);
+}
