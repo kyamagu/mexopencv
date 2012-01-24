8377106183cff7db6ed95c1b156324baf31fe0eb
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-01-24 13:24:55 -0500

    Added goodFeaturesToTrack

---
+++ +cv/goodFeaturesToTrack.m
@@ -0,0 +1,50 @@
+%GOODFEATURESTOTRACK  Determines strong corners on an image
+%
+%   corners = cv.goodFeaturesToTrack(image)
+%   corners = cv.goodFeaturesToTrack(image, 'OptionName', optionValue, ...)
+%
+% Input:
+%     image: Input single-channel 8-bit or floating-point image.
+% Output:
+%     corners: Output vector of detected corners.
+% Options:
+%     'MaxCorners': Maximum number of corners to return. If there are more
+%         corners than are found, the strongest of them is returned. default 1000.
+%     'QualityLevel': Parameter characterizing the minimal accepted quality of
+%         image corners. The parameter value is multiplied by the best corner
+%         quality measure, which is the minimal eigenvalue (see
+%         cv.cornerMinEigenVal ) or the Harris function response (see
+%         cv.cornerHarris ). The corners with the quality measure less than the
+%         product are rejected. For example, if the best corner has the quality
+%         measure = 1500, and the qualityLevel=0.01 , then all the corners with
+%         the quality measure less than 15 are rejected.
+%     'MinDistance': Minimum possible Euclidean distance between the returned
+%         corners.
+%     'Mask': Optional region of interest. If the image is not empty (it needs
+%         to have the type CV_8UC1 and the same size as image ), it specifies
+%         the region in which the corners are detected.
+%     'BlockSize': Size of an average block for computing a derivative
+%         covariation matrix over each pixel neighborhood. See
+%         cv.cornerEigenValsAndVecs.
+%     'UseHarrisDetector': Parameter indicating whether to use a Harris detector
+%         (see cv.cornerHarris ) or cv.cornerMinEigenVal
+%     'K': Harris detector free parameter.
+%
+% The function finds the most prominent corners in the image or in the specified
+% image region, as described in [Shi94]:
+%
+%  1. Function calculates the corner quality measure at every source image pixel
+%     using the cornerMinEigenVal() or cornerHarris().
+%  2. Function performs a non-maximum suppression (the local maximums in 3 x 3
+%     neighborhood are retained).
+%  3. The corners with the minimal eigenvalue less than QualityLevel * max_{x,y}
+%     (qualityMeasureMap(x,y)) are rejected.
+%  4. The remaining corners are sorted by the quality measure in the descending
+%     order.
+%  5. Function throws away each corner for which there is a stronger corner at
+%     a distance less than maxDistance.
+%
+% The function can be used to initialize a point-based tracker of an object.
+%
+% See also cv.cornerMinEigenVal cv.cornerHarris
+%
---
+++ src/+cv/goodFeaturesToTrack.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file goodFeaturesToTrack.cpp
+ * @brief mex interface for goodFeaturesToTrack
+ * @author Kota Yamaguchi
+ * @date 2011
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] )
+{
+	// Check the number of arguments
+	if (nrhs<1 || ((nrhs%2)!=1) || nlhs>1)
+        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    
+	// Argument vector
+	vector<MxArray> rhs(prhs,prhs+nrhs);
+	int maxCorners=1000;
+	double qualityLevel=0.01;
+	double minDistance=2.0;
+	Mat mask;
+	int blockSize=3;
+	bool useHarrisDetector=false;
+	double k=0.04;
+	for (int i=1; i<nrhs; i+=2) {
+		string key = rhs[i].toString();
+		if (key=="MaxCorners")
+			maxCorners = rhs[i+1].toInt();
+		else if (key=="QuarlityLevel")
+			qualityLevel = rhs[i+1].toDouble();
+		else if (key=="MinDistance")
+			minDistance = rhs[i+1].toDouble();
+		else if (key=="Mask")
+			mask = rhs[i+1].toMat(CV_8U);
+		else if (key=="BlockSize")
+			blockSize = rhs[i+1].toInt();
+		else if (key=="UseHarrisDetector")
+			useHarrisDetector = rhs[i+1].toBool();
+		else if (key=="K")
+			k = rhs[i+1].toInt();
+		else
+			mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
+	}
+	
+	// Process
+	Mat image = (rhs[0].isUint8()) ? rhs[0].toMat(CV_8U) : rhs[0].toMat(CV_32F);
+	vector<Point2f> corners;
+	goodFeaturesToTrack(image, corners, maxCorners, qualityLevel, minDistance,
+		mask, blockSize, useHarrisDetector, k);
+	plhs[0] = MxArray(corners);
+}
