a8b9f650c0d91ae39701ef9ce24167e2b2f12184
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2012-02-03 14:01:22 -0500

    Added transform

---
+++ +cv/transform.m
@@ -0,0 +1,35 @@
+%TRANSFORM  Performs the matrix transformation of every array element
+%
+%   dst = cv.transform(src, mtx)
+%
+% Input:
+%    src: Source array that must have as many channels (1 to 4) as columns
+%        of mtx or columns -1 of mtx
+%    mtx: floating-point transformation matrix.
+% Output:
+%    dst: Destination array of the same size and depth as src. It has as
+%        many channels as rows of mtx
+%
+% The function transform performs the matrix transformation of every
+% element of the array src and stores the results in dst:
+%
+%    dst(I) = mtx * src(I)
+%
+% when columns of mtx = src channels, or
+%
+%    dst(I) = mtx * [src(I); 1]
+%
+% when columns of mtx = src channels +1.
+%
+% Every element of the N -channel array src is interpreted as N -element
+% vector that is transformed using the M x N or M x (N+1) matrix mtx to
+% M-element vector - the corresponding element of the destination array
+% dst.
+%
+% The function may be used for geometrical transformation of N -dimensional
+% points, arbitrary linear color space transformation (such as various
+% kinds of RGB to YUV transforms), shuffling the image channels, and so
+% forth.
+%
+% See also cv.warpAffine cv.warpPerspective cv.perspectiveTransform
+%
---
+++ src/+cv/transform.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file transform.cpp
+ * @brief mex interface for transform
+ * @author Kota Yamaguchi
+ * @date 2011
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] )
+{
+	// Check the number of arguments
+	if (nrhs!=2 || nlhs>1)
+        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    
+	// Argument vector
+	vector<MxArray> rhs(prhs,prhs+nrhs);
+	
+	// Process
+	Mat src(rhs[0].toMat()), dst, mtx(rhs[1].toMat());
+	transform(src,dst,mtx);
+	plhs[0] = MxArray(dst);
+}
