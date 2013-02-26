49aab1b216418c5398f29643cb1bff4757480f11
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2013-02-26 12:35:59 -0500

    Updated help

--- +cv/calcOpticalFlowPyrLK.m
+++ +cv/calcOpticalFlowPyrLK.m
@@ -22,25 +22,30 @@
 % ## Options
 % * __InitialFlow__ Vector of 2D points to be used for the initial estimate of
 %         nextPts. If not specified, prevPts will be used as an initial value.
-% * __WinSize__ Size of the search window at each pyramid level.
+% * __WinSize__ Size of the search window at each pyramid level. Default to
+%         [21, 21].
 % * __MaxLevel__ 0-based maximal pyramid level number. If set to 0, pyramids are
 %         not used (single level). If set to 1, two levels are used, and so on.
+%         Default to 3.
 % * __Criteria__ Parameter specifying the termination criteria of the iterative
 %         search algorithm (after the specified maximum number of iterations
 %         criteria.maxCount or when the search window moves by less than
 %         criteria.epsilon. Struct with {'type','maxCount','epsilon'} fields is
 %         accepted. The type field should have one of 'Count', 'EPS', or
-%         'Count+EPS' to indicate which criteria to use.
+%         'Count+EPS' to indicate which criteria to use. Default to
+%         struct('type', 'Count+EPS', 'maxCount', 30, 'epsilon', 0.01).
 % * __GetMinEigenvals__ Use minimum eigen values as an error measure
 %         (see MinEigThreshold description); if the flag is not set, then L1
 %         distance between patches around the original and a moved point,
 %         divided by number of pixels in a window, is used as a error measure.
+%         Default to false.
 % * __MinEigThreshold__ The algorithm calculates the minimum eigen value of a
 %         2x2 normal matrix of optical flow equations (this matrix is called a
 %         spatial gradient matrix in [Bouguet00]), divided by number of pixels
 %         in a window; if this value is less than minEigThreshold, then a
 %         corresponding feature is filtered out and its flow is not processed,
 %         so it allows to remove bad points and get a performance boost.
+%         Default to 1e-4.
 %
 % The function implements a sparse iterative version of the Lucas-Kanade optical
 % flow in pyramids. See [Bouguet00].
