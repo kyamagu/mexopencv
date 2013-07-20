19258cf76da5894c517db875987dbb1a620d0517
Amro <amroamroamro@gmail.com>
2013-07-20 06:38:44 +0300

    add usage example
    code adapted from OpenCV documentation page

--- +cv/getTextSize.m
+++ +cv/getTextSize.m
@@ -30,5 +30,38 @@
 % The function cv.getTextSize calculates and returns the size of a box that
 % contains the specified text.
 %
+% ## Example
+% The following code renders some text, the tight box surrounding it, and
+% the baseline:
+%
+%    txt = 'Funny text inside the box';
+%    fontFace = 'HersheyScriptSimplex';
+%    fontScale = 2;
+%    thickness = 3;
+%
+%    img = zeros(300,800,3, 'uint8');
+%
+%    [textSize,baseline] = cv.getTextSize(txt, 'FontFace',fontFace, ...
+%        'FontScale',fontScale, 'Thickness',thickness);
+%    baseline = baseline + thickness;
+%
+%    % center the text
+%    textOrg = [size(img,2) - textSize(1), size(img,1) + textSize(2)] ./ 2;
+%
+%    % draw the box
+%    img = cv.rectangle(img, textOrg + [0, baseline], ...
+%        textOrg + [textSize(1), -textSize(2)], 'Color',[0,0,255]);
+%
+%    % ... and the baseline first
+%    img = cv.line(img, textOrg + [0, thickness], ...
+%        textOrg + [textSize(1), thickness], 'Color',[0,255,0]);
+%
+%    % then put the text itself
+%    img = cv.putText(img, txt, textOrg, 'Color',[255,255,255], ...
+%        'Thickness',thickness, 'FontFace',fontFace, 'FontScale',fontScale);
+%
+%    imshow(img, 'InitialMagnification',100, 'Border','tight')
+%
+%
 % See also cv.putText
 %
