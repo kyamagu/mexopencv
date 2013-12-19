2e6e317accb5e939f5ab483f597ba90e7aec2c65
Amro <amroamroamro@gmail.com>
2013-12-19 08:13:48 +0300

    factor common data in tests

--- test/unit_tests/TestSVM.m
+++ test/unit_tests/TestSVM.m
@@ -1,16 +1,17 @@
 classdef TestSVM
     %TestSVM
     properties (Constant)
+        X = [randn(50,3)+1;randn(50,3)-1];
+        Y = [ones(50,1);-ones(50,1)];
+        YReg = [ones(50,1);-ones(50,1)]+randn(100,1)*0.5;
     end
     
     methods (Static)
         function test_1
             % classification
-            X = [randn(50,3)+1;randn(50,3)-1];
-            Y = [ones(50,1);-ones(50,1)];
             classifier = cv.SVM;
-            classifier.train(X,Y);
-            Yhat = classifier.predict(X);
+            classifier.train(TestSVM.X, TestSVM.Y);
+            Yhat = classifier.predict(TestSVM.X);
             assert(isscalar(classifier.VarCount));
             assert(isstruct(classifier.Params));
             assert(isscalar(classifier.SupportVectorCount));
@@ -19,20 +20,17 @@ classdef TestSVM
         
         function test_2
             % regression
-            X = [randn(50,3)+1;randn(50,3)-1];
-            Y = [ones(50,1);-ones(50,1)]+randn(100,1)*0.5;
             regressor = cv.SVM;
-            regressor.train(X,Y,'SVMType','EPS_SVR','P',0.001);
-            Yhat = regressor.predict(X);
+            regressor.train(TestSVM.X, TestSVM.YReg, ...
+                'SVMType','EPS_SVR', 'P',0.001);
+            Yhat = regressor.predict(TestSVM.X);
         end
         
         function test_3
             % train auto
-            X = [randn(50,3)+1;randn(50,3)-1];
-            Y = [ones(50,1);-ones(50,1)];
             classifier = cv.SVM;
-            classifier.train_auto(X,Y,'KFold',2);
-            Yhat = classifier.predict(X);
+            classifier.train_auto(TestSVM.X, TestSVM.Y, 'KFold',2);
+            Yhat = classifier.predict(TestSVM.X);
         end
     end
     
