677db24119ca695c7b32c4c897a0d3de07c8802f
Amro <amroamroamro@gmail.com>
2014-01-09 01:48:56 +0300

    add simple unit tests

--- test/unit_tests/TestSolvePnP.m
+++ test/unit_tests/TestSolvePnP.m
@@ -3,7 +3,15 @@ classdef TestSolvePnP
     properties (Constant)
     end
     
-    methods (Static)        
+    methods (Static)
+        function test_1
+            objPoints = rand(10,3);
+            imgPoints = rand(10,2);
+            camMatrix = eye(3);
+            distCoeffs = zeros(5,1);
+            [rvec,tvec] = cv.solvePnP(objPoints, imgPoints, camMatrix, distCoeffs);
+        end
+
         function test_error_1
             try
                 cv.solvePnP();
--- test/unit_tests/TestSolvePnPRansac.m
+++ test/unit_tests/TestSolvePnPRansac.m
@@ -3,7 +3,16 @@ classdef TestSolvePnPRansac
     properties (Constant)
     end
     
-    methods (Static)        
+    methods (Static)
+        function test_1
+            objPoints = rand(10,3);
+            imgPoints = rand(10,2);
+            camMatrix = eye(3);
+            distCoeffs = zeros(5,1);
+            [rvec,tvec,inliers] = cv.solvePnPRansac(objPoints, imgPoints, ...
+                camMatrix, distCoeffs);
+        end
+
         function test_error_1
             try
                 cv.solvePnPRansac();
