f67fe38a00ff84e17aeda36ed17ee804c04c7d54
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2014-08-03 21:15:11 +0900

    Updated documents

--- html/search/all_66.js
+++ html/search/all_66.js
@@ -19,5 +19,6 @@ var searchData=
   ['floodfill_2ecpp',['floodFill.cpp',['../flood_fill_8cpp.html',1,'']]],
   ['fmmethod',['FMMethod',['../find_fundamental_mat_8cpp.html#a289c266928a50bf49a467dd1d021046f',1,'findFundamentalMat.cpp']]],
   ['fontface',['FontFace',['../mexopencv_8hpp.html#a01fe0cabb6a84344ab199d2d896fb982',1,'mexopencv.hpp']]],
-  ['fontstyle',['FontStyle',['../mexopencv_8hpp.html#af088b3d7445aaa5262efb7e42d6a0cf3',1,'mexopencv.hpp']]]
+  ['fontstyle',['FontStyle',['../mexopencv_8hpp.html#af088b3d7445aaa5262efb7e42d6a0cf3',1,'mexopencv.hpp']]],
+  ['fromvector',['fromVector',['../class_mx_array.html#a3f71538a3c51b5317824fc4781100cbe',1,'MxArray::fromVector(const std::vector&lt; char &gt; &amp;v)'],['../class_mx_array.html#a7c61b96f657d9b734324e8ee9e406e4d',1,'MxArray::fromVector(const std::vector&lt; bool &gt; &amp;v)']]]
 ];
--- html/search/classes_6d.js
+++ html/search/classes_6d.js
@@ -1,4 +1,17 @@
 var searchData=
 [
-  ['mxarray',['MxArray',['../class_mx_array.html',1,'']]]
+  ['mxarray',['MxArray',['../class_mx_array.html',1,'']]],
+  ['mxtypes',['MxTypes',['../struct_mx_types.html',1,'']]],
+  ['mxtypes_3c_20bool_20_3e',['MxTypes&lt; bool &gt;',['../struct_mx_types_3_01bool_01_4.html',1,'']]],
+  ['mxtypes_3c_20char_20_3e',['MxTypes&lt; char &gt;',['../struct_mx_types_3_01char_01_4.html',1,'']]],
+  ['mxtypes_3c_20double_20_3e',['MxTypes&lt; double &gt;',['../struct_mx_types_3_01double_01_4.html',1,'']]],
+  ['mxtypes_3c_20float_20_3e',['MxTypes&lt; float &gt;',['../struct_mx_types_3_01float_01_4.html',1,'']]],
+  ['mxtypes_3c_20int16_5ft_20_3e',['MxTypes&lt; int16_t &gt;',['../struct_mx_types_3_01int16__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20int32_5ft_20_3e',['MxTypes&lt; int32_t &gt;',['../struct_mx_types_3_01int32__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20int64_5ft_20_3e',['MxTypes&lt; int64_t &gt;',['../struct_mx_types_3_01int64__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20int8_5ft_20_3e',['MxTypes&lt; int8_t &gt;',['../struct_mx_types_3_01int8__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20uint16_5ft_20_3e',['MxTypes&lt; uint16_t &gt;',['../struct_mx_types_3_01uint16__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20uint32_5ft_20_3e',['MxTypes&lt; uint32_t &gt;',['../struct_mx_types_3_01uint32__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20uint64_5ft_20_3e',['MxTypes&lt; uint64_t &gt;',['../struct_mx_types_3_01uint64__t_01_4.html',1,'']]],
+  ['mxtypes_3c_20uint8_5ft_20_3e',['MxTypes&lt; uint8_t &gt;',['../struct_mx_types_3_01uint8__t_01_4.html',1,'']]]
 ];
