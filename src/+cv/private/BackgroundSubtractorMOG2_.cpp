0983f8f13b143190d516e7e4bdc71679f1aca7a8
Amro <amroamroamro@gmail.com>
2015-05-09 06:29:17 +0300

    fix BackgroundSubtractorMOG2

--- src/+cv/private/BackgroundSubtractorMOG2_.cpp
+++ src/+cv/private/BackgroundSubtractorMOG2_.cpp
@@ -5,7 +5,7 @@
  * @date 2012
  */
 #include "mexopencv.hpp"
-#include "opencv2/video/background_segm.hpp"
+#include "opencv2/video.hpp"
 using namespace std;
 using namespace cv;
 
@@ -14,7 +14,7 @@ using namespace cv;
 /// Last object id to allocate
 int last_id = 0;
 /// Object container
-map<int,BackgroundSubtractorMOG2> obj_;
+map<int,Ptr<BackgroundSubtractorMOG2> > obj_;
 
 /**
  * Main entry called from Matlab
@@ -29,42 +29,36 @@ void mexFunction( int nlhs, mxArray *plhs[],
     if (nrhs<2 || nlhs>1)
         mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
 
-    // Determine argument format between constructor or (id,method,...)
     vector<MxArray> rhs(prhs,prhs+nrhs);
-    int id = 0;
-    string method;
-    if (nrhs>1 && rhs[0].isNumeric() && rhs[1].isChar()) {
-        id = rhs[0].toInt();
-        method = rhs[1].toString();
-    }
-    else
-        mexErrMsgIdAndTxt("mexopencv:error","Invalid arguments");
+    int id = rhs[0].toInt();
+    string method(rhs[1].toString());
 
-    // Big operation switch
+    // constructor call
     if (method == "new") {
-        if (nrhs>3  && (nrhs%2)==0) {
-            int history = rhs[2].toInt();
-            float varThreshold = rhs[3].toDouble();
-            bool bShadowDetection=true;
-            for (int i=4;i<nrhs;i+=2) {
-                string key(rhs[i].toString());
-                if (key=="BShadowDetection")
-                    bShadowDetection = rhs[i+1].toBool();
-                else
-                    mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
-            }
-            obj_[++last_id] = BackgroundSubtractorMOG2(
-                history,varThreshold,bShadowDetection);
+        if ((nrhs%2)!=0)
+            mexErrMsgIdAndTxt("mexopencv:error", "Wrong number of arguments");
+        int history = 500;
+        double varThreshold = 16;
+        bool detectShadows = true;
+        for (int i=2; i<nrhs; i+=2) {
+            string key(rhs[i].toString());
+            if (key=="History")
+                history = rhs[i+1].toInt();
+            else if (key=="VarThreshold")
+                varThreshold = rhs[i+1].toDouble();
+            else if (key=="DetectShadows")
+                detectShadows = rhs[i+1].toBool();
+            else
+                mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
         }
-        else if (nrhs==2)
-            obj_[++last_id] = BackgroundSubtractorMOG2();
-        else
-            mexErrMsgIdAndTxt("mexopencv:error","Invalid arguments");
+        obj_[++last_id] = createBackgroundSubtractorMOG2(
+            history, varThreshold, detectShadows);
         plhs[0] = MxArray(last_id);
         return;
     }
 
-    BackgroundSubtractorMOG2& obj = obj_[id];
+    // Big operation switch
+    Ptr<BackgroundSubtractorMOG2> obj = obj_[id];
     if (method == "delete") {
         if (nrhs!=2 || nlhs!=0)
             mexErrMsgIdAndTxt("mexopencv:error","Output not assigned");
@@ -82,29 +76,29 @@ void mexFunction( int nlhs, mxArray *plhs[],
                 mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
         }
         Mat image(rhs[2].toMat()), fgmask;
-        obj(image, fgmask, learningRate);
+        obj->apply(image, fgmask, learningRate);
         plhs[0] = MxArray(fgmask,mxLOGICAL_CLASS);
     }
     else if (method == "getBackgroundImage") {
         if (nrhs!=2 || nlhs>1)
             mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
         Mat im;
-        obj.getBackgroundImage(im);
+        obj->getBackgroundImage(im);
         plhs[0] = MxArray(im);
     }
     else if (method == "history") {
         if (nrhs==3 && nlhs==0)
-            obj.set(method, rhs[2].toInt());
+            obj->setHistory(rhs[2].toInt());
         else if (nrhs==2 && nlhs==1)
-            plhs[0] = MxArray(obj.get<int>(method));
+            plhs[0] = MxArray(obj->getHistory());
         else
             mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
     }
     else if (method == "varThreshold") {
         if (nrhs==3 && nlhs==0)
-            obj.set(method, rhs[2].toDouble());
+            obj->setVarThreshold(rhs[2].toDouble());
         else if (nrhs==2 && nlhs==1)
-            plhs[0] = MxArray(obj.get<double>(method));
+            plhs[0] = MxArray(obj->getVarThreshold());
         else
             mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
     }
