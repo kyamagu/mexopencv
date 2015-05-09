95d558c1f711e65fb8e0b06f7ebcbc990be2587a
Amro <amroamroamro@gmail.com>
2015-05-09 09:42:19 +0300

    add BackgroundSubtractorKNN

---
+++ test/unit_tests/TestBackgroundSubtractorKNN.m
@@ -0,0 +1,13 @@
+classdef TestBackgroundSubtractorKNN
+    %TestBackgroundSubtractorKNN
+    properties (Constant)
+    end
+
+    methods (Static)
+        function test_1
+            bs = cv.BackgroundSubtractorKNN;
+        end
+    end
+
+end
+
