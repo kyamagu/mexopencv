f21294ea6bf9fdcbe190180d464e5dc0423aea94
Amro <amroamroamro@gmail.com>
2015-05-10 22:45:43 +0300

    fix and update ANN_MLP (C++, M, tests)

--- test/unit_tests/TestANN_MLP.m
+++ test/unit_tests/TestANN_MLP.m
@@ -2,20 +2,19 @@ classdef TestANN_MLP
     %TestANN_MLP
     properties (Constant)
     end
-    
+
     methods (Static)
         function test_1
             X = [randn(50,3)+1;randn(50,3)-1];
             Y = [ones(50,1);-ones(50,1)];
-            classifier = cv.ANN_MLP([3,1]);
+            classifier = cv.ANN_MLP();
+            classifier.LayerSizes = [3,1];
             classifier.train(X,Y);
             Yhat = classifier.predict(X);
-            n_layers = classifier.get_layer_count();
-            layers =   classifier.get_layer_sizes();
-            w0 =       classifier.get_weights(0);
-            w1 =       classifier.get_weights(1);
+            layers =   classifier.LayerSizes;
+            w0 =       classifier.getWeights(0);
+            w1 =       classifier.getWeights(1);
         end
     end
-    
-end
 
+end
