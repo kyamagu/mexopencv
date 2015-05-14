1d31379dbadc32fc2e61265eea9b0439d85ebe2b
Amro <amroamroamro@gmail.com>
2015-05-15 00:55:57 +0300

    limited update to corresponding MATLAB classes and tests

--- test/unit_tests/TestBOWImgDescriptorExtractor.m
+++ test/unit_tests/TestBOWImgDescriptorExtractor.m
@@ -7,14 +7,14 @@ classdef TestBOWImgDescriptorExtractor
         function test_1
             im = imread(fullfile(mexopencv.root(),'test','cat.jpg'));
             im = rgb2gray(im);
-            detector = cv.FeatureDetector('FAST');
+            detector = cv.FeatureDetector('FastFeatureDetector');
             kpts = detector.detect(im);
-            extractor = cv.DescriptorExtractor('BRIEF');
+            extractor = cv.DescriptorExtractor('BRISK');
             descs = extractor.compute(im,kpts);
             trainer = cv.BOWKMeansTrainer(100);
             vocab = trainer.cluster(descs);
 
-            bowextractor = cv.BOWImgDescriptorExtractor('BRIEF');
+            bowextractor = cv.BOWImgDescriptorExtractor('BRISK');
             bowextractor.Vocabulary = uint8(vocab);
 
             [bow,idx] = bowextractor.compute(im,kpts);
@@ -22,7 +22,7 @@ classdef TestBOWImgDescriptorExtractor
 
         function test_error_1
             try
-                cv.BOWImgDescriptorExtractor();
+                cv.BOWImgDescriptorExtractor('foo', 'bar');
                 throw('UnitTest:Fail');
             catch e
                 assert(strcmp(e.identifier,'mexopencv:error'));
