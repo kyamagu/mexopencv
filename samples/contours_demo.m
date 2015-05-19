ae0663782a932358fd64c5ba47e6429fc2a992e7
Amro <amroamroamro@gmail.com>
2015-05-20 02:03:55 +0300

    add new samples for findContours and drawContours

---
+++ samples/contours_demo.m
@@ -0,0 +1,31 @@
+%% Contours demo
+% The example below shows how to retrieve connected components from a binary
+% image and label them.
+
+%% Input Image
+% some binary (black-n-white) image
+src = imread('blobs.png');  % class(src) = logical
+imshow(src), title('Source')
+snapnow
+
+%% Connected Components
+[contours, hierarchy] = cv.findContours(src, ...
+    'Mode','CComp', 'Method','Simple');
+
+%%
+% iterate through all the top-level contours,
+% draw each connected component with its own random color
+dst = zeros([size(src) 3], 'uint8');
+idx = 0;
+while idx >= 0
+    clr = randi([0 255], [1 3], 'uint8');
+    dst = cv.drawContours(dst, contours, ...
+        'Hierarchy',hierarchy, 'ContourIdx',idx, ...
+        'Color',clr, 'Thickness','Filled', 'LineType',8);
+    idx = hierarchy{idx+1}(1);
+end
+
+%%
+% show result
+imshow(dst), title('Components')
+snapnow
