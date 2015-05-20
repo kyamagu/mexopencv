5590fbb9ecf079a1a20f5d825f23518fd98af391
Amro <amroamroamro@gmail.com>
2015-05-20 13:39:33 +0300

    update matchTemplate and add Mask option

--- test/unit_tests/TestMatchTemplate.m
+++ test/unit_tests/TestMatchTemplate.m
@@ -3,14 +3,14 @@ classdef TestMatchTemplate
     properties (Constant)
         img = imread(fullfile(mexopencv.root(),'test','img001.jpg'));
     end
-    
+
     methods (Static)
         function test_1
             im = rgb2gray(TestMatchTemplate.img);
             tmpl = im(120:150,180:210,:);
             result = cv.matchTemplate(im,tmpl);
         end
-        
+
         function test_error_1
             try
                 cv.matchTemplate();
@@ -20,6 +20,5 @@ classdef TestMatchTemplate
             end
         end
     end
-    
-end
 
+end
