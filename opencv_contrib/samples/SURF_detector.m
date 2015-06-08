e7ca8a522784d9ce29c33ef9ab852ec9009d9f16
Amro <amroamroamro@gmail.com>
2015-06-08 12:58:54 +0300

    edit samples to produce nicer output when published

--- opencv_contrib/samples/SURF_detector.m
+++ opencv_contrib/samples/SURF_detector.m
@@ -1,16 +1,16 @@
-function SURF_detector
-%SURF_DETECTOR  feature detector demo
-%
-% Before start, addpath('/path/to/mexopencv');
-%
+%% SURF Feature Detection demo
 
+%%
+% Read image and convert it to grayscale
 im = imread(fullfile(mexopencv.root(),'test','img001.jpg'));
 im = cv.cvtColor(im,'RGB2GRAY');
 
+%%
+% Detect keypoints using SURF Detector
 detector = cv.FeatureDetector('SURF');
 keypoints = detector.detect(im);
 
+%%
+% Draw keypoints
 im_keypoints = cv.drawKeypoints(im,keypoints);
 imshow(im_keypoints);
-
-end
--- samples/facedetect.m
+++ samples/facedetect.m
@@ -1,19 +1,22 @@
-function facedetect
-%FACEDETECT  face detection demo
-%
-% Before start, addpath('/path/to/mexopencv');
-%
-disp('Face detection demo. Press any key when done.');
+%%  Face Detection demo
+% Here is an example that illustrate how to detect faces
+% in a live video stream.
 
+%%
 % Load cascade file
 xml_file = fullfile(mexopencv.root(),'test','haarcascade_frontalface_alt2.xml');
 classifier = cv.CascadeClassifier(xml_file);
 
+%%
 % Set up camera
 camera = cv.VideoCapture;
 pause(3); % Necessary in some environment. See help cv.VideoCapture
 
-% Set up display window
+disp('Face detection demo. Press any key when done.');
+
+%%
+% Set up display window, and start the main loop
+
 window = figure('KeyPressFcn',@(obj,evt)setappdata(obj,'flag',true));
 setappdata(window,'flag',false);
 
@@ -24,16 +27,17 @@ while true
     im = cv.resize(im,0.5);
     gr = cv.cvtColor(im,'RGB2GRAY');
     gr = cv.equalizeHist(gr);
-    % Detect
+
+    % Detect faces
     boxes = classifier.detect(gr,'ScaleFactor',1.3,...
-                                 'MinNeighbors',2,...
-                                 'MinSize',[30,30]);
+        'MinNeighbors',2, 'MinSize',[30,30]);
+
     % Draw results
     imshow(im);
     for i = 1:numel(boxes)
         rectangle('Position',boxes{i},'EdgeColor','g','LineWidth',2);
     end
-    
+
     % Terminate if any user input
     flag = getappdata(window,'flag');
     if isempty(flag)||flag, break; end
@@ -41,6 +45,6 @@ while true
 end
 
 % Close
+snapnow
 close(window);
-
-end
+clear camera
