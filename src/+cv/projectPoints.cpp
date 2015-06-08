9637bc9b40567f2520d9d2dd9829067e3f0773e8
Amro <amroamroamro@gmail.com>
2015-06-08 18:55:10 +0300

    change optional positional argument to key-value pairs
    also cv.projectPoints works with 64F type

--- src/+cv/projectPoints.cpp
+++ src/+cv/projectPoints.cpp
@@ -19,41 +19,42 @@ void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[] )
 {
     // Check the number of arguments
-    if (nrhs<4 || ((nrhs%2)!=1) || nlhs>2)
+    if (nrhs<4 || (nrhs%2)!=0 || nlhs>2)
         mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
-    
+
     // Argument vector
     vector<MxArray> rhs(prhs,prhs+nrhs);
-    
+
     // Option processing
-    double aspectRatio=0;
-    for (int i=5; i<nrhs; i+=2) {
-        string key = rhs[i].toString();
-        if (key=="AspectRatio")
+    Mat distCoeffs;
+    double aspectRatio = 0;
+    for (int i=4; i<nrhs; i+=2) {
+        string key(rhs[i].toString());
+        if (key=="DistCoeffs")
+            distCoeffs = rhs[i+1].toMat(CV_64F);
+        else if (key=="AspectRatio")
             aspectRatio = rhs[i+1].toDouble();
         else
             mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     }
 
     // Process
-    Mat rvec(rhs[1].toMat(CV_32F)), tvec(rhs[2].toMat(CV_32F));
-    Mat cameraMatrix(rhs[3].toMat(CV_32F));
-    Mat distCoeffs = (nrhs>4) ? rhs[4].toMat(CV_32F) : Mat();
-    Mat jacobian;
+    Mat rvec(rhs[1].toMat(CV_64F)), tvec(rhs[2].toMat(CV_64F));
+    Mat cameraMatrix(rhs[3].toMat(CV_64F)), jacobian;
     if (rhs[0].isNumeric()) {
-        Mat objectPoints(rhs[0].toMat(CV_32F)), imagePoints;
+        Mat objectPoints(rhs[0].toMat(CV_64F)), imagePoints;
         projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs,
             imagePoints, jacobian, aspectRatio);
         plhs[0] = MxArray(imagePoints);
     }
     else if (rhs[0].isCell()) {
-        vector<Point3f> objectPoints(rhs[0].toVector<Point3f>());
-        vector<Point2f> imagePoints;
+        vector<Point3d> objectPoints(rhs[0].toVector<Point3d>());
+        vector<Point2d> imagePoints;
         projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs,
             imagePoints, jacobian, aspectRatio);
         plhs[0] = MxArray(imagePoints);
     }
-    
+
     if (nlhs>1)
         plhs[1] = MxArray(jacobian);
 }
