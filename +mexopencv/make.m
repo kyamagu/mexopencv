555395815d208c4f1057a2a1a9a68a3966a60bff
rafaelgm <rafaelgomesmonteiro@gmail.com>
2015-06-12 11:43:00 -0300

    Fix
    Change double quotes to single quotes to keep it Matlab compatible.

--- +mexopencv/make.m
+++ +mexopencv/make.m
@@ -442,7 +442,7 @@ function retval = isOctave
     persistent cacheval;  % speeds up repeated calls
 
     if isempty (cacheval)
-        cacheval = (exist ("OCTAVE_VERSION", "builtin") > 0);
+        cacheval = (exist ('OCTAVE_VERSION', 'builtin') > 0);
     end
 
     retval = cacheval;
