1c8049b9c53acee4142db0a66f894fb4e359e6f4
Amro <amroamroamro@gmail.com>
2015-07-25 10:14:41 +0300

    fix and upate findCirclesGrid
    - new "BlobDetector" option to set feature detector
    - return 2 ouputs: centers and status flag
    - convert input image as CV_8U
    - update M-docs
    - add unit-tests and a pattern image

--- +cv/findCirclesGrid.m
+++ +cv/findCirclesGrid.m
@@ -1,34 +1,55 @@
 %FINDCIRCLESGRID  Finds the centers in the grid of circles
 %
 %    centers = cv.findCirclesGrid(im, patternSize)
+%    [centers,patternFound] = cv.findCirclesGrid(im, patternSize)
 %    [...] = cv.findCirclesGrid(..., 'OptionName', optionValue, ...)
 %
 % ## Input
-% * __im__ Grid view of source circles. It must be an 8-bit grayscale or
-%        color image.
-% * __patternSize__ Number of circles per a grid row and column
-%        (`patternSize = [points_per_row, points_per_colum]`).
+% * __im__ Grid view of input circles. It must be an 8-bit grayscale or color
+%       image.
+% * __patternSize__ Number of circles per row and column
+%       (`patternSize = [points_per_row, points_per_colum]`).
 %
 % ## Output
-% * __centers__ Array of detected centers.
+% * __centers__ Cell array of detected centers `{[x,y], ...}`.
+% * __patternFound__ logical return value, see below.
 %
 % ## Options
-% * __SymmetricGrid__ Use symmetric pattern of circles. default true.
+% * __SymmetricGrid__ Use symmetric or asymmetric pattern of circles.
+%       default true.
 % * __Clustering__ Use a special algorithm for grid detection. It is more
-%        robust to perspective distortions but much more sensitive to
-%        background clutter. default false.
+%       robust to perspective distortions but much more sensitive to
+%       background clutter. default false.
+% * __BlobDetector__ feature detector that finds blobs like dark circles on
+%       light background. It can be specified as a string containing the type
+%       of feature detector, such as 'SimpleBlobDetector'. It can also be
+%       specified as a cell-array of the form `{fdetector, 'key',val, ...}`,
+%       where the first element is the type, and the remaining elements are
+%       optional parameters used to construct the specified feature detector.
+%       See cv.FeatureDetector for possible types.
+%       default is to use cv.SimpleBlobDetector with its default parameters.
 %
-% The function attempts to determine whether the input image contains a
-% grid of circles. If it is, the function locates centers of the circles.
-% The function returns a non-empty value if all of the centers have been
-% found and they have been placed in a certain order (row by row, left to
-% right in every row). Otherwise, if the function fails to find all the
-% corners or reorder them, it returns empty array.
+% The function attempts to determine whether the input image contains a grid
+% of circles. If it is, the function locates centers of the circles. The
+% function returns `true` if all of the centers have been found and they have
+% been placed in a certain order (row by row, left to right in every row).
+% Otherwise, if the function fails to find all the corners or reorder them,
+% it returns `false`.
+%
+% ## Example
+% Sample usage of detecting and drawing the centers of circles:
+%
+%    patternSize = [7,7];   % number of centers
+%    gray = imread('...');  % source 8-bit image
+%    [centers,patternfound] = cv.findCirclesGrid(gray, patternSize);
+%    img = cv.drawChessboardCorners(img, patternSize, cat(1,centers{:}), ...
+%        'PatternWasFound',patternfound);
+%    imshow(img)
 %
 % ## Note
-% The function requires white space (like a square-thick border,
-% the wider the better) around the board to make the detection more robust
-% in various environments.
+% The function requires white space (like a square-thick border, the wider the
+% better) around the board to make the detection more robust in various
+% environments.
 %
-% See also cv.findChessboardCorners
+% See also: cv.findChessboardCorners
 %
