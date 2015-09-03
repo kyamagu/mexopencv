92f628ca3a0fed0f0a1146f7f4f47785f5d6be98
Amro <amroamroamro@gmail.com>
2015-09-03 23:57:10 +0300

    new functions to calculate optical flow
    SimpleFlow and DeepFlow methods.
    also add a pair of test images.

---
+++ opencv_contrib/test/unit_tests/TestCalcOpticalFlowDF.m
@@ -0,0 +1,43 @@
+classdef TestCalcOpticalFlowDF
+    %TestCalcOpticalFlowDF
+    properties (Constant)
+        im = 255*uint8([...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 1 1 1 0 0 0 0;...
+            0 0 0 1 0 1 0 0 0 0;...
+            0 0 0 1 1 1 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+            0 0 0 0 0 0 0 0 0 0;...
+        ]);
+    end
+
+    methods (Static)
+        function test_1
+            im1 = TestCalcOpticalFlowDF.im;
+            im2 = circshift(im1, [0 1]);
+            flow = cv.calcOpticalFlowDF(im1, im2);
+        end
+
+        function test_2
+            prevImg = rgb2gray(imread(fullfile(mexopencv.root(),'test','RubberWhale1.png')));
+            nextImg = rgb2gray(imread(fullfile(mexopencv.root(),'test','RubberWhale2.png')));
+            flow = cv.calcOpticalFlowDF(prevImg, nextImg);
+            validateattributes(flow, {'single'}, ...
+                {'3d', 'size',[size(prevImg,1) size(prevImg,2) 2]});
+        end
+
+        function test_error_1
+            try
+                cv.calcOpticalFlowDF();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+
+end
