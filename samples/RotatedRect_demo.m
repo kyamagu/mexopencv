66139796e99ebf27ceb28c8bf9b63df035e53092
Amro <amroamroamro@gmail.com>
2015-09-03 22:48:57 +0300

    new test image for contours demo

--- samples/RotatedRect_demo.m
+++ samples/RotatedRect_demo.m
@@ -3,19 +3,19 @@
 %
 
 %%
-image = zeros([200 200 3], 'uint8');
+img = zeros([200 200 3], 'uint8');
 rRect = struct('center',[100 100], 'size',[100 50], 'angle',30);
 
 %%
 vertices = cv.RotatedRect.points(rRect);
 for i=1:4
-  image = cv.line(image, vertices(i,:), vertices(mod(i,4)+1,:), ...
+  img = cv.line(img, vertices(i,:), vertices(mod(i,4)+1,:), ...
       'Color',[0 255 0]);
 end
 
 %%
 brect = cv.RotatedRect.boundingRect(rRect);
-image = cv.rectangle(image, brect, 'Color',[255 0 0]);
+img = cv.rectangle(img, brect, 'Color',[255 0 0]);
 
 %%
-imshow(image); title('rectangles')
+imshow(img); title('rectangles')
--- samples/hough_lines_demo.m
+++ samples/hough_lines_demo.m
@@ -1,4 +1,6 @@
 %% Hough lines demo
+% An example using the Hough line detector.
+%
 % This program demonstrates line finding with the Hough transform.
 %
 % <https://github.com/Itseez/opencv/blob/master/samples/cpp/houghlines.cpp>
@@ -20,8 +22,11 @@ color_dst = cv.cvtColor(dst, 'GRAY2RGB');
 color_dst2 = color_dst;
 
 %% HoughLines
+tic
 lines = cv.HoughLines(dst, 'Rho',1, 'Theta',pi/180, 'Threshold',100);
+toc
 
+%%
 for i=1:numel(lines)
     rho = lines{i}(1);
     theta = lines{i}(2);
@@ -39,9 +44,12 @@ imshow(color_dst), title('Detected Lines')
 snapnow
 
 %% HoughLinesP
+tic
 lines = cv.HoughLinesP(dst, 'Rho',1, 'Theta',pi/180, 'Threshold',50, ...
     'MinLineLength',50, 'MaxLineGap',20);
+toc
 
+%%
 for i=1:numel(lines)
     color_dst2 = cv.line(color_dst2, lines{i}(1:2), lines{i}(3:4), ...
         'Color',[0,0,255], 'Thickness',2, 'LineType','AA');
