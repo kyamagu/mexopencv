42fbcff3d7e8b23150b2197e85c8b896e9f1c0e2
Amro <amroamroamro@gmail.com>
2015-09-04 12:11:39 +0300

    update estimateRigidTransform
    In case inputs are images (as opposed to points), must be 8-bit.
    Update docs and tests.

--- +cv/estimateRigidTransform.m
+++ +cv/estimateRigidTransform.m
@@ -1,41 +1,45 @@
 %ESTIMATERIGIDTRANSFORM  Computes an optimal affine transformation between two 2D point sets
 %
 %    M = cv.estimateRigidTransform(src, dst)
-%    M = cv.estimateRigidTransform(src, dst, 'OptionName', optionValue)
+%    M = cv.estimateRigidTransform(src, dst, 'OptionName', optionValue, ...)
 %
 % ## Input
-% * __src__ First input 2D point set stored cell array of 2-element vectors, or
-%         an image
-% * __dst__ Second input 2D point set of the same size and the same type as A, or
-%         another image.
+% * __src__ First input 2D point set stored in a cell array of 2-element
+%       vectors `{[x,y], ...}`, or first image (8-bit numeric array, 1- or
+%       3-channels).
+% * __dst__ Second input 2D point set of the same size and the same type as
+%       `src`, or second image (8-bit numeric array, 1- or 3-channels).
 %
 % ## Output
-% * __M__ Output matrix (see below).
+% * __M__ output 2x3 affine transformation `[A|b]` matrix (see below).
 %
 % ## Options
 % * __FullAffine__ If true, the function finds an optimal affine transformation
-%         with no additional resrictions (6 degrees of freedom). Otherwise, the
-%         class of transformations to choose from is limited to combinations of
-%         translation, rotation, and uniform scaling (5 degrees of freedom).
+%       with no additional resrictions (6 degrees of freedom). Otherwise, the
+%       class of transformations to choose from is limited to combinations of
+%       translation, rotation, and uniform scaling (5 degrees of freedom).
 %
-% The function finds an optimal affine transform [A|b] (a 2 x 3 floating-point
+% The function finds an optimal affine transform `[A|b]` (a 2x3 floating-point
 % matrix) that approximates best the affine transformation between:
 %
-%   * Two point sets
-%   * Two raster images. In this case, the function first finds some features in
-%     the src image and finds the corresponding features in dst image. After
-%     that, the problem is reduced to the first case.
+% * Two point sets
+% * Two raster images. In this case, the function first finds some features in
+%   the `src` image and finds the corresponding features in `dst` image. After
+%   that, the problem is reduced to the first case.
 %
-% In case of point sets, the problem is formulated as follows: you need to find
-% a 2x2 matrix A and 2x1 vector b so that:
+% In case of point sets, the problem is formulated as follows: you need to
+% find a 2x2 matrix `A` and 2x1 vector `b` so that:
 %
-%     [A*|b*] = argmin_{[A|b]} \sum_i || dst[i] - Asrc[i]^T - b ||^2
+%    [A*|b*] = argmin_{[A|b]} \sum_i || dst{i} - A*src{i}' - b ||^2
 %
-% where src[i] and dst[i] are the i-th points in src and dst, respectively.
-% [A|b] can be either arbitrary (when fullAffine=true) or have a form of
+% where `src{i}` and `dst{i}` are the i-th points in `src` and `dst`,
+% respectively. `[A|b]` can be either arbitrary (when `FullAffine=true`) or
+% have a form of:
 %
-%     [ a11, a12, b1;...
-%      -a12, a22, b2 ]
+%    [ a11, a12, b1;
+%     -a12, a11, b2 ]
 %
-% when fullAffine=false.
+% when `FullAffine=false`.
+%
+% See also: cv.getAffineTransform, cv.getPerspectiveTransform, cv.findHomography
 %
--- test/unit_tests/TestEstimateRigidTransform.m
+++ test/unit_tests/TestEstimateRigidTransform.m
@@ -1,7 +1,7 @@
 classdef TestEstimateRigidTransform
     %TestEstimateRigidTransform
     properties (Constant)
-        im = im2uint8([...
+        im = 255*uint8([...
             0 0 0 0 0 0 0 0 0 0;...
             0 0 0 0 0 0 0 0 0 0;...
             0 0 0 0 0 0 0 0 0 0;...
@@ -12,16 +12,35 @@ classdef TestEstimateRigidTransform
             0 0 0 0 0 0 0 0 0 0;...
             0 0 0 0 0 0 0 0 0 0;...
             0 0 0 0 0 0 0 0 0 0;...
-            ]);
+        ]);
     end
-    
+
     methods (Static)
         function test_1
             im1 = TestEstimateRigidTransform.im;
-            im2 = [zeros(10,1,'uint8'),im1(:,1:end-1)];
-            M = cv.estimateRigidTransform(im1,im2,'FullAffine',false);
+            im2 = circshift(im1, [0 1]);
+            M = cv.estimateRigidTransform(im1, im2, 'FullAffine',false);
         end
-        
+
+        function test_images_input
+            im1 = rgb2gray(imread(fullfile(mexopencv.root(),'test','RubberWhale1.png')));
+            im2 = rgb2gray(imread(fullfile(mexopencv.root(),'test','RubberWhale2.png')));
+            M = cv.estimateRigidTransform(im1, im2, 'FullAffine',true);
+            validateattributes(M, {'double'}, {'size',[2 3]});
+        end
+
+        function test_points_input
+            N = 100;
+            pts1 = rand(2,N)*10;                  % a set of 2D points
+            aff = randn(2,3);                     % true affine transformation
+            pts2 = aff * [pts1; ones(1,N)];       % transform pts1
+            pts2 = pts2 + randn(size(pts2))*0.01; % add noise
+            M = cv.estimateRigidTransform(...
+                num2cell(pts1,1), num2cell(pts2,1), 'FullAffine',true);
+            validateattributes(M, {'double'}, {'size',[2 3]});
+            norm(M - aff);
+        end
+
         function test_error_1
             try
                 cv.estimateRigidTransform();
@@ -31,6 +50,5 @@ classdef TestEstimateRigidTransform
             end
         end
     end
-    
-end
 
+end
