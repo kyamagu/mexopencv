23975986d6e97a70440f6153c1093eacbf8ee77d
Amro <amroamroamro@gmail.com>
2015-09-04 12:03:18 +0300

    update calcOpticalFlowPyrLK
    - fix how we set the GetMinEigenvals flag value.
    - accept both images or image pyramids built with buildOpticalFlowPyramid
    - only calculate err if output is requested.
    - update tests.

--- src/+cv/calcOpticalFlowPyrLK.cpp
+++ src/+cv/calcOpticalFlowPyrLK.cpp
@@ -16,26 +16,23 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<3 || ((nrhs%2)!=1) || nlhs>3)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=3 && (nrhs%2)==1 && nlhs<=3);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
-    Mat prevImg(rhs[0].toMat(CV_8U)), nextImg(rhs[1].toMat(CV_8U));
-    vector<Point2f> prevPts(rhs[2].toVector<Point2f>()), nextPts;
-
-    Size winSize = Size(21,21);
+    // Option processing
+    vector<Point2f> nextPts;
+    Size winSize(21,21);
     int maxLevel = 3;
     TermCriteria criteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
     int flags = 0;
     double minEigThreshold = 1e-4;
     for (int i=3; i<nrhs; i+=2) {
-        string key = rhs[i].toString();
+        string key(rhs[i].toString());
         if (key=="InitialFlow") {
             nextPts = rhs[i+1].toVector<Point2f>();
             flags |= cv::OPTFLOW_USE_INITIAL_FLOW;
@@ -47,7 +44,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
         else if (key=="Criteria")
             criteria = rhs[i+1].toTermCriteria();
         else if (key=="GetMinEigenvals")
-            flags |= (rhs[i+1].toBool()) ? cv::OPTFLOW_LK_GET_MIN_EIGENVALS : 0;
+            UPDATE_FLAG(flags, rhs[i+1].toBool(), cv::OPTFLOW_LK_GET_MIN_EIGENVALS);
         else if (key=="MinEigThreshold")
             minEigThreshold = rhs[i+1].toDouble();
         else
@@ -55,17 +52,29 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
 
     // Process
-    vector<uchar> status(prevPts.size());
-    vector<float> err(prevPts.size());
-
-    if (prevPts.size()>0) {
-        calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts, status, err,
-        winSize, maxLevel, criteria, flags, minEigThreshold);
+    vector<Point2f> prevPts(rhs[2].toVector<Point2f>());
+    Mat status, err;
+    if (rhs[0].isNumeric() && rhs[1].isNumeric()) {
+        // images
+        Mat prevImg(rhs[0].toMat(CV_8U)),
+            nextImg(rhs[1].toMat(CV_8U));
+        calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts, status,
+            (nlhs>2 ? err : noArray()), winSize, maxLevel, criteria, flags,
+            minEigThreshold);
     }
-
+    else if (rhs[0].isCell() && rhs[1].isCell()) {
+        // pyramids
+        vector<Mat> prevImg(rhs[0].toVector<Mat>()),
+                    nextImg(rhs[1].toVector<Mat>());
+        calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts, status,
+            (nlhs>2 ? err : noArray()), winSize, maxLevel, criteria, flags,
+            minEigThreshold);
+    }
+    else
+        mexErrMsgIdAndTxt("mexopencv:error","Invalid argument");
     plhs[0] = MxArray(nextPts);
     if (nlhs>1)
-        plhs[1] = MxArray(Mat(status));
+        plhs[1] = MxArray(status);
     if (nlhs>2)
-        plhs[2] = MxArray(Mat(err));
+        plhs[2] = MxArray(err);
 }
