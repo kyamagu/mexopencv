03039881aae1e75bf40a369008626f55849ef707
Amro <amroamroamro@gmail.com>
2015-09-07 20:22:58 +0300

    update EMD, fix bug #191
    - use 32f float type when appropriate
    - lowerBound output cannot computed if cost matrix is used (thanks @YanaiA)
    - optional flow output computed on-demand
    - other minor edits
    - update M-docs
    - add unit tests

--- src/+cv/EMD.cpp
+++ src/+cv/EMD.cpp
@@ -16,39 +16,42 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<2 || (nrhs%2)!=0 || nlhs>3)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=3);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
     int distType = cv::DIST_L2;
     Mat cost;
     float lowerBound = 0;
     for (int i=2; i<nrhs; i+=2) {
-        string key = rhs[i].toString();
-        if (key=="DistType")
+        string key(rhs[i].toString());
+        if (key == "DistType")
             distType = (rhs[i+1].isChar()) ?
                 DistType[rhs[i+1].toString()] : rhs[i+1].toInt();
-        else if (key=="Cost")
-            cost = rhs[i+1].toMat();
-        else if (key=="LowerBound")
-            lowerBound = rhs[i+1].toDouble();
+        else if (key == "Cost")
+            cost = rhs[i+1].toMat(CV_32F);
+        else if (key == "LowerBound")
+            lowerBound = rhs[i+1].toFloat();
         else
             mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     }
     if (distType==cv::DIST_USER && cost.empty())
-        mexErrMsgIdAndTxt("mexopencv:error","Cost matrix empty");
-    
+        mexErrMsgIdAndTxt("mexopencv:error",
+            "In case of user-defined distance, cost matrix must be defined");
+
     // Process
-    Mat signature1(rhs[0].toMat()), signature2(rhs[1].toMat()), flow;
-    double d = EMD(signature1, signature2, distType, cost, &lowerBound, flow);
-    plhs[0] = MxArray(d);
+    Mat signature1(rhs[0].toMat(CV_32F)),
+        signature2(rhs[1].toMat(CV_32F)),
+        flow;
+    float emd = EMD(signature1, signature2, distType, cost,
+        (nlhs>1 && cost.empty() ? &lowerBound : NULL),
+        (nlhs>2 ? flow : noArray()));
+    plhs[0] = MxArray(emd);
     if (nlhs>1)
         plhs[1] = MxArray(lowerBound);
     if (nlhs>2)
