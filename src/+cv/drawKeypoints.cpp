af8006056d4f4afbe55f9794f53bff7b40188cbf
Amro <amroamroamro@gmail.com>
2015-09-08 23:14:25 +0300

    update drawKeypoints
    - images are 8u
    - NotDrawSinglePoints option doesnt apply (meant for drawMatches)
    - edit M-docs
    - create unit test

--- src/+cv/drawKeypoints.cpp
+++ src/+cv/drawKeypoints.cpp
@@ -16,30 +16,27 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<2 || (nrhs%2)!=0 || nlhs>1)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=1);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
     Mat outImg;
-    Scalar color = Scalar::all(-1);
+    Scalar color(Scalar::all(-1));
     int flags = DrawMatchesFlags::DEFAULT;
     for (int i=2; i<nrhs; i+=2) {
         string key(rhs[i].toString());
         if (key=="Color")
             color = rhs[i+1].toScalar();
-        else if (key=="NotDrawSinglePoints")
-            UPDATE_FLAG(flags, rhs[i+1].toBool(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
         else if (key=="DrawRichKeypoints")
-            UPDATE_FLAG(flags, rhs[i+1].toBool(), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            UPDATE_FLAG(flags, rhs[i+1].toBool(),
+                DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
         else if (key=="OutImage") {
-            outImg = rhs[i+1].toMat();
+            outImg = rhs[i+1].toMat(CV_8U);
             flags |= DrawMatchesFlags::DRAW_OVER_OUTIMG;
         }
         else
@@ -47,7 +44,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
 
     // Process
-    Mat image(rhs[0].toMat());
+    Mat image(rhs[0].toMat(CV_8U));
     vector<KeyPoint> keypoints(rhs[1].toVector<KeyPoint>());
     drawKeypoints(image, keypoints, outImg, color, flags);
     plhs[0] = MxArray(outImg);
