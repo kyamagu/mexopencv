32408043484d49dd93655c63c01ca16e14a9d0f5
Amro <amroamroamro@gmail.com>
2015-09-09 07:58:17 +0300

    update drawMatches
    - images are 8u
    - edit M-docs
    - create unit tests

--- src/+cv/drawMatches.cpp
+++ src/+cv/drawMatches.cpp
@@ -16,20 +16,18 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<5 || (nrhs%2)!=1 || nlhs>1)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=5 && (nrhs%2)==1 && nlhs<=1);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
     Mat outImg;
-    Scalar matchColor = Scalar::all(-1);
-    Scalar singlePointColor = Scalar::all(-1);
+    Scalar matchColor(Scalar::all(-1));
+    Scalar singlePointColor(Scalar::all(-1));
     vector<char> matchesMask;
     int flags = DrawMatchesFlags::DEFAULT;
     for (int i=5; i<nrhs; i+=2) {
@@ -39,13 +37,15 @@ void mexFunction( int nlhs, mxArray *plhs[],
         else if (key=="SinglePointColor")
             singlePointColor = rhs[i+1].toScalar();
         else if (key=="MatchesMask")
-            rhs[i+1].toMat(CV_8U).reshape(1,1).copyTo(matchesMask);
+            rhs[i+1].toMat(CV_8S).reshape(1,1).copyTo(matchesMask);
         else if (key=="NotDrawSinglePoints")
-            UPDATE_FLAG(flags, rhs[i+1].toBool(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+            UPDATE_FLAG(flags, rhs[i+1].toBool(),
+                DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
         else if (key=="DrawRichKeypoints")
-            UPDATE_FLAG(flags, rhs[i+1].toBool(), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            UPDATE_FLAG(flags, rhs[i+1].toBool(),
+                DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
         else if (key=="OutImage") {
-            outImg = rhs[i+1].toMat();
+            outImg = rhs[i+1].toMat(CV_8U);
             flags |= DrawMatchesFlags::DRAW_OVER_OUTIMG;
         }
         else
@@ -53,8 +53,8 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
 
     // Process
-    Mat img1(rhs[0].toMat()),
-        img2(rhs[2].toMat());
+    Mat img1(rhs[0].toMat(CV_8U)),
+        img2(rhs[2].toMat(CV_8U));
     vector<KeyPoint> keypoints1(rhs[1].toVector<KeyPoint>()),
                      keypoints2(rhs[3].toVector<KeyPoint>());
     vector<DMatch> matches1to2(rhs[4].toVector<DMatch>());
---
+++ test/unit_tests/TestDrawMatches.m
@@ -0,0 +1,49 @@
+classdef TestDrawMatches
+    %TestDrawMatches
+    properties (Constant)
+        img1 = fullfile(mexopencv.root(),'test','left01.jpg');
+        img2 = fullfile(mexopencv.root(),'test','right01.jpg');
+    end
+
+    methods (Static)
+        function test_1
+            im1 = imread(TestDrawMatches.img1);
+            im2 = imread(TestDrawMatches.img2);
+            obj = cv.ORB();
+            [kpts1, feat1] = obj.detectAndCompute(im1);
+            [kpts2, feat2] = obj.detectAndCompute(im2);
+            matcher = cv.DescriptorMatcher('BruteForce-Hamming');
+            m = matcher.match(feat1, feat2);
+            out = cv.drawMatches(im1, kpts1, im2, kpts2, m);
+            validateattributes(out, {class(im1)}, {'size',...
+                [max(size(im1,1),size(im2,1)), size(im1,2)+size(im2,2), 3]});
+        end
+
+        function test_2
+            im1 = imread(TestDrawMatches.img1);
+            im2 = imread(TestDrawMatches.img2);
+            obj = cv.ORB();
+            [kpts1, feat1] = obj.detectAndCompute(im1);
+            [kpts2, feat2] = obj.detectAndCompute(im2);
+            matcher = cv.DescriptorMatcher('BruteForce-Hamming');
+            m = matcher.match(feat1, feat2);
+            mask = ([m.distance] < quantile([m.distance], 0.1));
+            out = cv.drawMatches(im1, kpts1, im2, kpts2, m, ...
+                'MatchesMask',mask, ...
+                'OutImage',repmat(cat(2,im1,im2), [1 1 3]), ...
+                'MatchColor',[255 0 0], 'SinglePointColor',[0 255 0], ...
+                'NotDrawSinglePoints',true, 'DrawRichKeypoints',true);
+            validateattributes(out, {class(im1)}, {'size',...
+                [max(size(im1,1),size(im2,1)), size(im1,2)+size(im2,2), 3]});
+        end
+
+        function test_error_1
+            try
+                cv.drawMatches();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+end
