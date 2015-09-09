ffc88bd6daa1b81728ae39ef8dd702a409977f91
Amro <amroamroamro@gmail.com>
2015-09-10 00:34:58 +0300

    update warpAffine and warpPerspective
    - add 'Dst' option for setting initial destination image
      (useful with Transparent border mode)
    - transformation matrix is 64f
    - update M-docs
    - more unit tests

--- +cv/warpAffine.m
+++ +cv/warpAffine.m
@@ -1,34 +1,55 @@
 %WARPAFFINE  Applies an affine transformation to an image
 %
 %    dst = cv.warpAffine(src, M)
-%    dst = cv.warpAffine(src, M, 'DSize', dsize, ...)
+%    dst = cv.warpAffine(src, M, 'OptionName',optionValue, ...)
 %
 % ## Input
-% * __src__ Source image.
-% * __M__ 2 x 3 transformation matrix.
+% * __src__ Input image.
+% * __M__ 2x3 transformation matrix, floating-point.
 %
 % ## Output
-% * __dst__ Destination image that has the size dsize and the same type as src.
+% * __dst__ Output image that has the size `DSize` (with
+%       `size(dst,3) == size(src,3)`) and the same type as `src`.
 %
 % ## Options
-% * __DSize__ Size of the destination image
-% * __Interpolation__ interpolation method. default: 'Linear'
+% * __DSize__ Size of the output image `[w,h]`. Default `[0,0]` means using
+%       the same size as the input `[size(src,2) size(src,1)]`.
+% * __Interpolation__ interpolation method, default 'Linear'. One of:
+%       * __Nearest__ nearest neighbor interpolation
+%       * __Linear__ bilinear interpolation
+%       * __Cubic__ bicubic interpolation
+%       * __Lanczos4__ Lanczos interpolation over 8x8 neighborhood
 % * __WarpInverse__ Logical flag to apply inverse affine transform, meaning
-%     that M is the inverse transformation (dst -> src)
+%       that `M` is the inverse transformation (`dst -> src`). default false
 % * __BorderType__ Pixel extrapolation method. When 'Transparent', it means
-%     that the pixels in the destination image corresponding to
-%     the outliers in the source image are not modified by the
-%     function. default: 'Constant'
-% * __BorderValue__ Value used in case of a constant border. default: 0
+%       that the pixels in the destination image corresponding to the
+%       "outliers" in the source image are not modified by the function.
+%       default 'Constant'
+%       * __Constant__ `iiiiii|abcdefgh|iiiiiii` with some specified `i`
+%       * __Replicate__ `aaaaaa|abcdefgh|hhhhhhh`
+%       * __Reflect__ `fedcba|abcdefgh|hgfedcb`
+%       * __Reflect101__ `gfedcb|abcdefgh|gfedcba`
+%       * __Wrap__ `cdefgh|abcdefgh|abcdefg`
+%       * __Transparent__ `uvwxyz|absdefgh|ijklmno`
+%       * __Default__ same as 'Reflect101'
+% * __BorderValue__ Value used in case of a constant border. default 0
+% * __Dst__ Optional initial image for the output. If not set, it is
+%       automatically created by the function. Note that it must match the
+%       expected size `DSize` and the type of `src`, otherwise it is ignored
+%       and recreated by the function. This option is only useful when
+%       `BorderType=Transparent`, in which case the transformed image is drawn
+%       onto the existing `Dst` without extrapolating pixels. Not set by
+%       default.
 %
-% The function warpAffine transforms the source image using the specified
-% matrix
+% The function cv.warpAffine transforms the source image using the specified
+% matrix:
 %
-%     dst(x,y) = src(M_11*x+M_12*y+M_13, M_21*x+M_22*y+M_23)
+%     dst(x,y) = src(M_11*x + M_12*y + M_13, M_21*x + M_22*y + M_23)
 %
-% when the 'WarpInverse' option is true. Otherwise, the transformation is first
+% when the `WarpInverse` option is true. Otherwise, the transformation is first
 % inverted with cv.invertAffineTransform and then put in the formula above
-% instead of M.
+% instead of `M`.
 %
-% See also cv.warpPerspective cv.resize cv.getRectSubPix
+% See also: cv.warpPerspective, cv.remap, cv.resize, cv.getRectSubPix,
+%  cv.transform, imtransform, imwarp
 %
--- src/+cv/warpPerspective.cpp
+++ src/+cv/warpPerspective.cpp
@@ -25,30 +25,36 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
-    Mat src(rhs[0].toMat());
-    Size dsize = src.size();
-    int interpolation=cv::INTER_LINEAR;
-    bool warp_inverse=false;
-    int borderType=cv::BORDER_CONSTANT;
+    Mat dst;
+    Size dsize;
+    int flags = cv::INTER_LINEAR;
+    bool warp_inverse = false;
+    int borderMode = cv::BORDER_CONSTANT;
     Scalar borderValue;
     for (int i=2; i<nrhs; i+=2) {
         string key(rhs[i].toString());
-        if (key=="DSize")
+        if (key=="Dst")
+            dst = rhs[i+1].toMat();
+        else if (key=="DSize")
             dsize = rhs[i+1].toSize();
         else if (key=="Interpolation")
-            interpolation = InterpType[rhs[i+1].toString()];
+            flags = InterpType[rhs[i+1].toString()];
         else if (key=="WarpInverse")
             warp_inverse = rhs[i+1].toBool();
         else if (key=="BorderType")
-            borderType = BorderType[rhs[i+1].toString()];
+            borderMode = BorderType[rhs[i+1].toString()];
         else if (key=="BorderValue")
             borderValue = rhs[i+1].toScalar();
         else
             mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     }
-    
-    Mat M(rhs[1].toMat()), dst;
-    int flags = interpolation | ((warp_inverse) ? cv::WARP_INVERSE_MAP : 0);
-    warpPerspective(src, dst, M, dsize, flags, borderType, borderValue);
+    flags |= (warp_inverse ? cv::WARP_INVERSE_MAP : 0);
+    if (!dst.empty())
+        dsize = dst.size();
+
+    // Process
+    Mat src(rhs[0].toMat()),
+        M(rhs[1].toMat(CV_64F));
+    warpPerspective(src, dst, M, dsize, flags, borderMode, borderValue);
     plhs[0] = MxArray(dst);
 }
