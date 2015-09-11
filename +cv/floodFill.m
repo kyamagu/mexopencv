c643ff368815b9b5b5c00a97fe360b1f494c398d
Amro <amroamroamro@gmail.com>
2015-09-11 17:39:40 +0300

    update floodFill
    - expose option to set mask fill value (bits 8-16 of flags)
    - image is 8u, 32s, or 32f
    - only compute rect if output requested
    - update M-docs
    - add unit tests

--- +cv/floodFill.m
+++ +cv/floodFill.m
@@ -1,41 +1,102 @@
 %FLOODFILL  Fills a connected component with the given color
 %
 %    dst = cv.floodFill(src, seed, newVal)
-%    dst = cv.floodFill(src, seed, newVal, 'Mask', mask, ...)
-%    [dst, rect, area] = cv.floodFill(src, seed, newVal, ...)
-%    [dst, mask, rect, area] = cv.floodFill(src, seed, newVal, 'Mask', mask, ...)
+%    [dst, rect, area] = cv.floodFill(src, seed, newVal)
+%    [dst, rect, area, mask] = cv.floodFill(..., 'Mask',mask, 'MaskOnly',true)
+%    [...] = cv.floodFill(..., 'OptionName',optionValue, ...)
 %
 % ## Input
-% * __src__ Input/output 1- or 3-channel, 8-bit, or floating-point image. It is
-%        modified by the function unless the 'MaskOnly' flag is set in the
-%        option
-% * __seed__ Starting point.
-% * __newVal__ New value of the repainted domain pixels.
+% * __src__ Input 1- or 3-channel, 8-bit, or floating-point image.
+% * __seed__ Starting seed point `[x,y]`.
+% * __newVal__ New value of the repainted domain pixels. It should match the
+%       range and dimensions of the input image: 1-element vector (brightness)
+%       for grayscale images, and 3-element vector (color) for RGB images.
 %
 % ## Output
-% * __dst__ Destination image of the same size and the same type as src.
+% * __dst__ Destination image of the same size and the same type as `src`.
+%       Contains the modified input unless the `MaskOnly` flag is set in the
+%       second variant of the function, in which case `dst` is the same as the
+%       input `src` unmodified. See the details below.
 % * __rect__ Optional output parameter set by the function to the minimum
-%        bounding rectangle of the repainted domain.
+%       bounding rectangle of the repainted domain `[x,y,w,h]`.
 % * __area__ Optional output parameter set by the function to the number of
-%        filled pixels
+%       filled pixels.
+% * __mask__ Optional output containing the updated input `Mask`. Populated in
+%       the second variant of the function with the `Mask` option. On output,
+%       pixels in the mask corresponding to filled pixels in the image are set
+%       to 1 or to the value specified in `MaskFillValue` option as described
+%       below.
 %
 % ## Options
 % * __LoDiff__ Maximal lower brightness/color difference between the currently
-%        observed pixel and one of its neighbors belonging to the
-%        component, or a seed pixel being added to the component.
+%       observed pixel and one of its neighbors belonging to the component, or
+%       a seed pixel being added to the component. default zeros
 % * __UpDiff__ Maximal upper brightness/color difference between the currently
-%        observed pixel and one of its neighbors belonging to the
-%        component, or a seed pixel being added to the component.
-% * __Connectivity__ 4 or 8
+%       observed pixel and one of its neighbors belonging to the component, or
+%       a seed pixel being added to the component. default zeros
+% * __Connectivity__ Connectivity value, 4 or 8. The default value of 4 means
+%       that only the four nearest neighbor pixels (those that share an edge
+%       are considered. A connectivity value of 8 means that the eight nearest
+%       neighbor pixels (those that share a corner) will be considered.
+%       default 4
 % * __FixedRange__ If set, the difference between the current pixel and
-%        seed pixel is considered. Otherwise, the difference between
-%        neighbor pixels is considered (that is, the range is
-%        floating).
-% * __MaskOnly__ If set, the function does not change the image ( newVal
-%        is ignored), but fills the mask. The flag can be used for
-%        the second variant only.
+%       seed pixel is considered. Otherwise, the difference between
+%       neighbor pixels is considered (that is, the range is
+%       floating). default false
+% * __Mask__ Operation mask that should be a single-channel 8-bit image,
+%       2 pixels wider and 2 pixels taller than image. Flood-filling cannot go
+%       across non-zero pixels in the input mask. For example, an edge
+%       detector output can be used as a mask to stop filling at edges. It is
+%       possible to use the same mask in multiple calls to the function to
+%       make sure the filled areas do not overlap. Not set by default.
+% * __MaskOnly__ If set, the function does not change the image in the output
+%       (`newVal` is ignored), and only fills the output `mask` with the value
+%       specified in `MaskFillValue` as described. This option only make sense
+%       in function variants that have the mask parameter. default false
+% * __MaskFillValue__ Value between 1 and 255 with which to fill the output
+%       `Mask`. This option only make sense in function variants that have the
+%       mask parameter. default 0 (which effectively flood-fills the mask by
+%       the default filling value of 1)
 %
-% The functions floodFill fill a connected component starting from the
-% seed point with the specified color. The connectivity is determined by
-% the color/brightness closeness of the neighbor pixels.
+% The function cv.floodFill fills a connected component starting from the seed
+% point with the specified color. The connectivity is determined by the
+% color/brightness closeness of the neighbor pixels. The pixel at `(x,y)` is
+% considered to belong to the repainted domain if:
+%
+% * in case of a grayscale image and floating range:
+%
+%        src(x',y') - loDiff <= src(x,y) <= src(x',y') + upDiff
+%
+% * in case of a grayscale image and fixed range:
+%
+%        src(seedPoint.x,seedPoint.y) - loDiff <= src(x,y) <= src(seedPoint.x,seedPoint.y) + upDiff
+%
+% * in case of a color image and floating range:
+%
+%        src(x',y')_r - loDiffr <= src(x,y)_r <= src(x',y')_r + upDiffr,
+%        src(x',y')_g - loDiffg <= src(x,y)_g <= src(x',y')_g + upDiffg
+%        src(x',y')_b - loDiffb <= src(x,y)_b <= src(x',y')_b + upDiffb
+%
+% * in case of a color image and fixed range:
+%
+%        src(seedPoint.x,seedPoint.y)_r - loDiffr <= src(x,y)_r <= src(seedPoint.x,seedPoint.y)_r + upDiffr,
+%        src(seedPoint.x,seedPoint.y)_g - loDiffg <= src(x,y)_g <= src(seedPoint.x,seedPoint.y)_g + upDiffg
+%        src(seedPoint.x,seedPoint.y)_b - loDiffb <= src(x,y)_b <= src(seedPoint.x,seedPoint.y)_b + upDiffb
+%
+% where `src(x',y')` is the value of one of pixel neighbors that is already
+% known to belong to the component. That is, to be added to the connected
+% component, a color/brightness of the pixel should be close enough to:
+%
+% * Color/brightness of one of its neighbors that already belong to the
+%   connected component in case of a floating range.
+% * Color/brightness of the seed point in case of a fixed range.
+%
+% Use this function to either mark a connected component with the specified
+% color, or build a mask and then extract the contour, or copy the region to
+% another image, and so on.
+%
+% **Note:** Since the mask is larger than the filled image, a pixel `(x,y)` in
+% image corresponds to the pixel `(x+1,y+1)` in the mask.
+%
+% See also: cv.findContours, imfill
 %
--- src/+cv/floodFill.cpp
+++ src/+cv/floodFill.cpp
@@ -25,63 +25,61 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
-    bool useMask = false;
     Mat mask;
     Scalar loDiff;
     Scalar upDiff;
     int connectivity = 4;
     bool fixedRange = false;
     bool maskOnly = false;
+    int maskVal = 0x00;
     for (int i=3; i<nrhs; i+=2) {
-        string key = rhs[i].toString();
-        if (key=="Mask") {
-            useMask = true;
+        string key(rhs[i].toString());
+        if (key=="Mask")
             mask = rhs[i+1].toMat(CV_8U);
-        }
         else if (key=="LoDiff")
             loDiff = rhs[i+1].toScalar();
         else if (key=="UpDiff")
             upDiff = rhs[i+1].toScalar();
-        else if (key=="Connectivity") {
+        else if (key=="Connectivity")
             connectivity = rhs[i+1].toInt();
-            if (!(connectivity==4||connectivity==8))
-                mexErrMsgIdAndTxt("mexopencv:error",
-                    "Connectivity must be either 4 or 8");
-        }
         else if (key=="FixedRange")
             fixedRange = rhs[i+1].toBool();
         else if (key=="MaskOnly")
             maskOnly = rhs[i+1].toBool();
+        else if (key=="MaskFillValue")
+            maskVal = rhs[i+1].toInt();
         else
             mexErrMsgIdAndTxt("mexopencv:error",
                 "Unrecognized option %s", key.c_str());
     }
-    
-    // Apply
-    Mat img(rhs[0].toMat());
-    Point seed = rhs[1].toPoint();
-    Scalar newVal = rhs[2].toScalar();
+    if (connectivity!=4 && connectivity!=8)
+        mexErrMsgIdAndTxt("mexopencv:error", "Connectivity must be 4 or 8");
+    if (maskVal<0 || maskVal>255)
+        mexErrMsgIdAndTxt("mexopencv:error", "Fill value between 1 and 255");
+    maskVal = (maskVal==0 && maskOnly) ? 1 : maskVal;   // no sense in filling zeros with zeros
+    int flags = connectivity |                          // lower 8 bits
+        (maskVal << 8) |                                // middle 8 bits
+        (fixedRange ? cv::FLOODFILL_FIXED_RANGE : 0) |  // higher 8 bits
+        (maskOnly   ? cv::FLOODFILL_MASK_ONLY   : 0);   // higher 8 bits
+
+    // Process
+    Mat img(rhs[0].toMat(rhs[0].isUint8() ? CV_8U :
+        (rhs[0].isInt32() ? CV_32S : CV_32F)));
+    Point seed(rhs[1].toPoint());
+    Scalar newVal(rhs[2].toScalar());
     Rect rect;
-    int flags = connectivity |
-        ((fixedRange) ? cv::FLOODFILL_FIXED_RANGE : 0) |
-        ((maskOnly)   ? cv::FLOODFILL_MASK_ONLY   : 0);
     int area = 0;
-    if (useMask) {
-        area = floodFill(img, mask, seed, newVal, &rect, loDiff, upDiff, flags);
-        plhs[0] = MxArray(img);
-        if (nlhs>1)
-            plhs[1] = MxArray(mask,mxLOGICAL_CLASS);
-        if (nlhs>2)
-            plhs[2] = MxArray(rect);
-        if (nlhs>3)
-            plhs[3] = MxArray(area);
-    }
-    else {
-        area = floodFill(img, seed, newVal, &rect, loDiff, upDiff, flags);
-        plhs[0] = MxArray(img);
-        if (nlhs>1)
-            plhs[1] = MxArray(rect);
-        if (nlhs>2)
-            plhs[2] = MxArray(area);
-    }
+    if (!mask.empty())
+        area = floodFill(img, mask, seed, newVal, (nlhs>1 ? &rect : NULL),
+            loDiff, upDiff, flags);
+    else
+        area = floodFill(img, seed, newVal, (nlhs>1 ? &rect : NULL),
+            loDiff, upDiff, flags);
+    plhs[0] = MxArray(img);
+    if (nlhs>1)
+        plhs[1] = MxArray(rect);
+    if (nlhs>2)
+        plhs[2] = MxArray(area);
+    if (nlhs>3)
+        plhs[3] = MxArray(mask); // keep it as uint8 for MaskFillValue to work
 }
