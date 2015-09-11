90632ae61551917918a958b84706cc731f20daaa
Amro <amroamroamro@gmail.com>
2015-09-11 16:43:53 +0300

    update morphologyEx
    update docs and unit tests

--- +cv/morphologyEx.m
+++ +cv/morphologyEx.m
@@ -1,30 +1,44 @@
 %MORPHOLOGYEX  Performs advanced morphological transformations
 %
 %    dst = cv.morphologyEx(src, op)
-%    dst = cv.morphologyEx(src, op, 'Element', [0,1,0;1,1,1;0,1,0], ...)
+%    dst = cv.morphologyEx(src, op, 'OptionName',optionValue, ...)
 %
 % ## Input
-% * __src__ Source image.
+% * __src__ Source image. The number of channels can be arbitrary. The depth
+%       should be one of `uint8`, `uint16`, `int16`, `single` or `double`.
 % * __op__ Type of a morphological operation that can be one of the following:
-% * __Open__ an opening operation
-% * __Close__ a closing operation
-% * __Gradient__ a morphological gradient
-% * __Tophat__ top hat
-% * __Blackhat__ black hat
+%       * __Erode__ see cv.erode
+%       * __Dilate__ see cv.dilate
+%       * __Open__ an opening operation
+%             `dst = open(src,element) = dilate(erode(src,element))`
+%       * __Close__ a closing operation
+%             `dst = close(src,element) = erode(dilate(src,element))`
+%       * __Gradient__ a morphological gradient
+%             `dst = morph_grad(src,element) = dilate(src,element) - erode(src,element)`
+%       * __Tophat__ "top hat"
+%             `dst = tophat(src,element) = src - open(src,element)`
+%       * __Blackhat__ "black hat"
+%             `dst = blackhat(src,element) = close(src,element) - src`
 %
 % ## Output
-% * __dst__ Destination image of the same size and type as src.
+% * __dst__ Destination image of the same size and type as `src`.
 %
 % ## Options
-% * __Element__ Structuring element
+% * __Element__ Structuring element kernel. It can be created using
+%       cv.getStructuringElement. Empty by default, which uses a 3x3
+%       rectangular structuring element by default.
 % * __Anchor__ Position of the anchor within the element. The default value
-%         [-1, -1] means that the anchor is at the element center.
-% * __Iterations__ Number of times erosion and dilation are applied.
-% * __BorderType__ Border mode used to extrapolate pixels outside of the
-%         image. default 'Constant'
+%       [-1,-1] means that the anchor is at the element center.
+% * __Iterations__ Number of times erosion and dilation are applied. default 1
+% * __BorderType__ Pixel extrapolation method. default 'Constant'
 % * __BorderValue__ Border value in case of a constant border. The default
-%         value has a special meaning.
+%       value has a special meaning. See cv.dilate and cv.erode for details.
 %
 % The function can perform advanced morphological transformations using an
 % erosion and dilation as basic operations.
+%
+% In case of multi-channel images, each channel is processed independently.
+%
+% See also: cv.dilate, cv.erode, cv.getStructuringElement, imopen, imclose,
+%  imtophat, imbothat
 %
