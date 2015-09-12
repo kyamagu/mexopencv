3a1111d5730d6b307a3b06078a1fb8340e583225
Amro <amroamroamro@gmail.com>
2015-09-12 13:28:26 +0300

    update matchShapes
    - new constmap to match 'Method' values
    - accepts either contours points or grayscale image
    - image or points support the same formats as cv::moments
    - update M-docs
    - update unit tests

--- src/+cv/matchShapes.cpp
+++ src/+cv/matchShapes.cpp
@@ -9,6 +9,14 @@
 using namespace std;
 using namespace cv;
 
+namespace {
+/// Shape matching methods for option processing
+const ConstMap<string,int> ShapeMatchMethodsMap = ConstMap<string,int>
+    ("I1", CV_CONTOURS_MATCH_I1)
+    ("I2", CV_CONTOURS_MATCH_I2)
+    ("I3", CV_CONTOURS_MATCH_I3);
+}
+
 /**
  * Main entry called from Matlab
  * @param nlhs number of left-hand-side arguments
@@ -29,17 +37,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     double parameter = 0;
     for (int i=2; i<nrhs; i+=2) {
         string key(rhs[i].toString());
-        if (key=="Method") {
-            string val(rhs[i+1].toString());
-            if (val=="I1")
-                method = CV_CONTOURS_MATCH_I1;
-            else if (val=="I2")
-                method = CV_CONTOURS_MATCH_I2;
-            else if (val=="I3")
-                method = CV_CONTOURS_MATCH_I3;
-            else
-                mexErrMsgIdAndTxt("mexopencv:error","Unrecognized method");
-        }
+        if (key=="Method")
+            method = ShapeMatchMethodsMap[rhs[i+1].toString()];
         else if (key=="Parameter")
             parameter = rhs[i+1].toDouble();
         else
@@ -47,17 +46,19 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     }
 
     // Process
-    if (rhs[0].isNumeric() && rhs[1].isNumeric()) {
-        Mat object1(rhs[0].toMat(CV_8U)), object2(rhs[1].toMat(CV_8U));
-        double d = matchShapes(object1,object2,method,parameter);
-        plhs[0] = MxArray(d);
+    double result = 0;
+    if ((rhs[0].isNumeric() || rhs[0].isLogical()) &&
+        (rhs[1].isNumeric() || rhs[1].isLogical())) {
+        Mat contour1(rhs[0].toMat()), // CV_8U, CV_16U, CV_16S, CV_32F, CV_64F
+            contour2(rhs[1].toMat());
+        result = matchShapes(contour1, contour2, method, parameter);
     }
     else if (rhs[0].isCell() && rhs[1].isCell()) {
-        vector<Point2f> object1(rhs[0].toVector<Point2f>());
-        vector<Point2f> object2(rhs[1].toVector<Point2f>());
-        double d = matchShapes(object1,object2,method,parameter);
-        plhs[0] = MxArray(d);
+        vector<Point2f> contour1(rhs[0].toVector<Point2f>()),
+                        contour2(rhs[1].toVector<Point2f>());
+        result = matchShapes(contour1, contour2, method, parameter);
     }
     else
         mexErrMsgIdAndTxt("mexopencv:error","Invalid argument");
+    plhs[0] = MxArray(result);
 }
