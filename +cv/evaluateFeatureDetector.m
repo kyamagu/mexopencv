a1b99578d7625f82cf362482e6afac74b28e624a
Amro <amroamroamro@gmail.com>
2015-09-12 15:48:48 +0300

    update evaluateFeatureDetector
    - add option to specify FeatureDetector
    - update M-docs
    - update unit tests

--- +cv/evaluateFeatureDetector.m
+++ +cv/evaluateFeatureDetector.m
@@ -1,6 +1,7 @@
-%EVALUATEFEATUREDETECTOR  Evalutes a feature detector
+%EVALUATEFEATUREDETECTOR  Evaluates a feature detector
 %
 %    [repeatability, correspCount] = cv.evaluateFeatureDetector(img1, img2, H1to2, keypoints1, keypoints2)
+%    [...] = cv.evaluateFeatureDetector(..., 'OptionName',optionValue, ...)
 %
 % ## Input
 % * __img1__ First image.
@@ -10,8 +11,18 @@
 % * __keypoints2__ Keypoints detected in `img2`
 %
 % ## Output
-% * __repeatability__ float value
-% * __correspCount__ int value
+% * __repeatability__ Repeatability (float value), between 0.0 and 1.0
+% * __correspCount__ Correspondences count (int value).
+%
+% ## Options
+% * __Detector__ feature detector that finds keypoints in images. It can be
+%       specified as a string containing the type of feature detector, such as
+%       'ORB'. It can also be specified as a cell-array of the form
+%       `{fdetector, 'key',val, ...}`, where the first element is the type,
+%       and the remaining elements are optional parameters used to construct
+%       the specified feature detector. See cv.FeatureDetector for possible
+%       types. Not set by default (i.e: you must supply pre-detected keypoints
+%       in the inputs).
 %
 % ## Example:
 %
@@ -20,5 +31,5 @@
 %    kp2 = detector.detect(img2)
 %    [rep,corresp] = cv.evaluateFeatureDetector(img1, img2, H1to2, kp1, kp2)
 %
-% See also: cv.FeatureDetector
+% See also: cv.computeRecallPrecisionCurve, cv.FeatureDetector
 %
