1fc1d1c5bfe7124865bf1f11fb1f0e6e5b7a70e1
Amro <amroamroamro@gmail.com>
2015-09-13 21:05:52 +0300

    update findHomography
    - add a new robust estimation method (RHO)
    - accept two new options: MaxIters and Confidence
    - accept points as numeric Nx2 or 1xNx2/Nx1x2 in addition to cell arrays
    - cast points as 32f
    - mask output is optional (fill it on demand)
    - update M-docs
    - more unit tests

--- src/+cv/findHomography.cpp
+++ src/+cv/findHomography.cpp
@@ -9,12 +9,14 @@
 using namespace std;
 using namespace cv;
 
-/** Methods for option processing
- */
-const ConstMap<std::string,int> Method = ConstMap<std::string,int>
+namespace {
+/// Estimation methods for option processing
+const ConstMap<string,int> Method = ConstMap<string,int>
     ("0",      0)
     ("Ransac", cv::RANSAC)
-    ("LMedS",  cv::LMEDS);
+    ("LMedS",  cv::LMEDS)
+    ("Rho",    cv::RHO);
+}
 
 /**
  * Main entry called from Matlab
@@ -23,23 +25,30 @@ const ConstMap<std::string,int> Method = ConstMap<std::string,int>
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<2 || ((nrhs%2)!=0) || nlhs>2)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=2);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
-    int method=0;
-    double ransacReprojThreshold=3.;
+    vector<MxArray> rhs(prhs, prhs+nrhs);
+
+    // Option processing
+    int method = 0;
+    double ransacReprojThreshold = 3.0;
+    int maxIters = 2000;
+    double confidence = 0.995;
     for (int i=2; i<nrhs; i+=2) {
-        string key = rhs[i].toString();
+        string key(rhs[i].toString());
         if (key=="Method")
-            method = Method[rhs[i+1].toString()];
+            method = (rhs[i+1].isChar()) ?
+                Method[rhs[i+1].toString()] : rhs[i+1].toInt();
         else if (key=="RansacReprojThreshold")
             ransacReprojThreshold = rhs[i+1].toDouble();
+        else if (key=="MaxIters")
+            maxIters = rhs[i+1].toInt();
+        else if (key=="Confidence")
+            confidence = rhs[i+1].toDouble();
         else
             mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     }
@@ -47,13 +56,16 @@ void mexFunction( int nlhs, mxArray *plhs[],
     // Process
     Mat mask, H;
     if (rhs[0].isNumeric() && rhs[1].isNumeric()) {
-        Mat points1(rhs[0].toMat()), points2(rhs[1].toMat());
-        H = findHomography(points1, points2, method, ransacReprojThreshold, mask);
+        Mat points1(rhs[0].toMat(CV_32F).reshape(2,0)),  // CV_32FC2
+            points2(rhs[1].toMat(CV_32F).reshape(2,0));
+        H = findHomography(points1, points2, method, ransacReprojThreshold,
+            (nlhs>1 ? mask : noArray()), maxIters, confidence);
     }
     else if (rhs[0].isCell() && rhs[1].isCell()) {
-        vector<Point2f> points1(rhs[0].toVector<Point2f>());
-        vector<Point2f> points2(rhs[1].toVector<Point2f>());
-        H = findHomography(points1, points2, method, ransacReprojThreshold, mask);
+        vector<Point2f> points1(rhs[0].toVector<Point2f>()),
+                        points2(rhs[1].toVector<Point2f>());
+        H = findHomography(points1, points2, method, ransacReprojThreshold,
+            (nlhs>1 ? mask : noArray()), maxIters, confidence);
     }
     else
         mexErrMsgIdAndTxt("mexopencv:error","Invalid argument");
--- test/unit_tests/TestFindHomography.m
+++ test/unit_tests/TestFindHomography.m
@@ -1,9 +1,35 @@
 classdef TestFindHomography
     %TestFindHomography
-    properties (Constant)
-    end
-    
+
     methods (Static)
+        function test_1
+            % generate N random points, transform them using a reference H
+            N = 12;
+            H = [0.75 -0.2 100; 0.1 0.8 50; 1e-5 1e-4 1];
+            p1 = bsxfun(@times, rand(N,2), [480 640]);
+            p2 = cv.perspectiveTransform(p1, H);
+
+            % introduce small noise in points
+            p1 = round(p1 + randn(size(p1)));
+            p2 = round(p2 + randn(size(p2)));
+
+            % introduce an extreme outlier (to test robust estimators)
+            p2(1,1) = 1000;
+
+            % estimate homography
+            estMethods = {0, 'Ransac', 'LMedS', 'Rho'};
+            for i=1:numel(estMethods)
+                [HH, mask] = cv.findHomography(p1, p2, ...
+                    'Method',estMethods{i}, 'MaxIters',2000, ...
+                    'RansacReprojThreshold',3.0, 'Confidence',0.995);
+                validateattributes(HH, {'single' 'double'}, {'size',[3 3]});
+                validateattributes(mask, {'uint8'}, ...
+                    {'vector', 'numel',N, 'binary'});
+                err = norm(H-HH);
+                %if i>1, assert(mask(1)==0); end
+            end
+        end
+
         function test_error_1
             try
                 cv.findHomography();
@@ -13,6 +39,5 @@ classdef TestFindHomography
             end
         end
     end
-    
-end
 
+end
