9fcb315cd44615df2a80987abeefc9ead5446898
Amro <amroamroamro@gmail.com>
2015-09-14 18:19:03 +0300

    update Rodrigues
    - both 32f and 64f supported
    - jacobian is optional output (fill on demand)
    - update M-docs
    - more unit tests

--- src/+cv/Rodrigues.cpp
+++ src/+cv/Rodrigues.cpp
@@ -16,21 +16,19 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs!=1 || nlhs>2)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs==1 && nlhs<=2);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Process
-    Mat src(rhs[0].toMat(CV_32F)), dst;
-    Mat jacobian;
-    Rodrigues(src,dst,jacobian);
+    Mat src(rhs[0].toMat(rhs[0].isSingle() ? CV_32F : CV_64F)),
+        dst, jacobian;
+    Rodrigues(src, dst, (nlhs>1 ? jacobian : noArray()));
+    plhs[0] = MxArray(dst);
     if (nlhs>1)
         plhs[1] = MxArray(jacobian);
-    plhs[0] = MxArray(dst);
 }
--- test/unit_tests/TestRodrigues.m
+++ test/unit_tests/TestRodrigues.m
@@ -1,16 +1,63 @@
 classdef TestRodrigues
     %TestRodrigues
-    properties (Constant)
-    end
-    
+
     methods (Static)
-        function test_1
-            M = [cos(pi/4),sin(pi/4),0;...
-                -sin(pi/4),cos(pi/4),0;...
-                         0,        0,1;];
-            dst = cv.Rodrigues(M);
+        function test_rotation_vec_mat
+            M = [cos(pi/4) sin(pi/4) 0 ; ...
+                -sin(pi/4) cos(pi/4) 0 ; ...
+                        0         0  1];
+
+            % rotation matrix to vector
+            dstVec = cv.Rodrigues(M);
+            validateattributes(dstVec, {class(M)}, {'vector', 'numel',3});
+
+            % rotation vector to matrix
+            dstMat = cv.Rodrigues(dstVec);
+            validateattributes(dstMat, {class(M)}, {'2d', 'size',[3 3]});
+            assert(norm(dstMat(:)-M(:)) < 1e-9, 'Error is too large');
         end
-        
+
+        function test_single_double_input
+            % double precision
+            [dst,jacob] = cv.Rodrigues(double(eye(3)));
+            validateattributes(dst, {'double'}, {'vector', 'numel',3});
+            validateattributes(jacob, {'double'}, {'2d', 'size',[9 3]});
+
+            % single precision
+            [dst,jacob] = cv.Rodrigues(single(eye(3)));
+            validateattributes(dst, {'single'}, {'vector', 'numel',3});
+            validateattributes(jacob, {'single'}, {'2d', 'size',[9 3]});
+        end
+
+        function test_vector_matrix_input
+            % 3x3 rotation matrix
+            dst = cv.Rodrigues(eye(3));
+            validateattributes(dst, {'double'}, {'vector', 'numel',3});
+
+            % 3x1/1x3 rotation vector
+            dst = cv.Rodrigues(zeros(3,1));
+            validateattributes(dst, {'double'}, {'2d', 'size',[3 3]});
+            dst = cv.Rodrigues(zeros(1,3));
+            validateattributes(dst, {'double'}, {'2d', 'size',[3 3]});
+        end
+
+        function test_jacobian
+            [~,jacobian] = cv.Rodrigues(eye(3));
+            validateattributes(jacobian, {'double'}, {'2d', 'size',[9 3]});
+
+            [~,jacobian] = cv.Rodrigues(zeros(3,1));
+            validateattributes(jacobian, {'double'}, {'2d', 'size',[3 9]});
+
+            [~,jacobian] = cv.Rodrigues(zeros(1,3));
+            validateattributes(jacobian, {'double'}, {'2d', 'size',[3 9]});
+        end
+
+        function test_roundtrip
+            src = [0; 0; -pi/4];
+            dst = cv.Rodrigues(cv.Rodrigues(src));
+            assert(norm(src - dst) < 1e-9);
+        end
+
         function test_error_1
             try
                 cv.Rodrigues();
@@ -20,6 +67,5 @@ classdef TestRodrigues
             end
         end
     end
-    
-end
 
+end
