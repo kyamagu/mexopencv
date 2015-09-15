4b8b8c6d90185b362d07a6a87c42b7699728a5e0
Amro <amroamroamro@gmail.com>
2015-09-15 15:28:42 +0300

    update RQDecomp3x3
    - matrix either 32f or 64f
    - compute optional outputs on demand
    - new "eulerAngles" output
    - return all optional outputs in a struct
    - update M-docs
    - update unit test

--- src/+cv/RQDecomp3x3.cpp
+++ src/+cv/RQDecomp3x3.cpp
@@ -9,6 +9,27 @@
 using namespace std;
 using namespace cv;
 
+namespace {
+/** Create a new MxArray from decomposed matrix.
+ * @param Qx Rotation matrix around x-axis.
+ * @param Qy Rotation matrix around y-axis.
+ * @param Qz Rotation matrix around z-axis.
+ * @param eulerAngles Euler angles of rotation.
+ * @return output MxArray struct object.
+ */
+MxArray toStruct(const Mat& Qx, const Mat& Qy, const Mat& Qz,
+    const Vec3d& eulerAngles)
+{
+    const char* fieldnames[] = {"Qx", "Qy", "Qz", "eulerAngles"};
+    MxArray s = MxArray::Struct(fieldnames, 4);
+    s.set("Qx",          Qx);
+    s.set("Qy",          Qy);
+    s.set("Qz",          Qz);
+    s.set("eulerAngles", eulerAngles);
+    return s;
+}
+}
+
 /**
  * Main entry called from Matlab
  * @param nlhs number of left-hand-side arguments
@@ -16,28 +37,24 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs!=1 || nlhs>5)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs==1 && nlhs<=3);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Process
-    Mat M(rhs[0].toMat(CV_32F));
-    Mat R, Q;
-    Mat Qx, Qy, Qz;
-    RQDecomp3x3(M, R, Q, Qx, Qy, Qz);
+    Mat M(rhs[0].toMat(rhs[0].isSingle() ? CV_32F : CV_64F)),
+        R, Q, Qx, Qy, Qz;
+    Vec3d eulerAngles = RQDecomp3x3(M, R, Q,
+        (nlhs>2 ? Qx : noArray()),
+        (nlhs>2 ? Qy : noArray()),
+        (nlhs>2 ? Qz : noArray()));
     plhs[0] = MxArray(R);
     if (nlhs>1)
         plhs[1] = MxArray(Q);
     if (nlhs>2)
-        plhs[2] = MxArray(Qx);
-    if (nlhs>3)
-        plhs[3] = MxArray(Qy);
-    if (nlhs>4)
-        plhs[4] = MxArray(Qz);
+        plhs[2] = toStruct(Qx, Qy, Qz, eulerAngles);
 }
--- test/unit_tests/TestRQDecomp3x3.m
+++ test/unit_tests/TestRQDecomp3x3.m
@@ -1,16 +1,29 @@
 classdef TestRQDecomp3x3
     %TestRQDecomp3x3
     properties (Constant)
+        fields = {'Qx', 'Qy', 'Qz', 'eulerAngles'};
     end
-    
+
     methods (Static)
         function test_1
-            M = [cos(pi/4),sin(pi/4),0;...
-                -sin(pi/4),cos(pi/4),0;...
-                         0,        0,1;];
-            dst = cv.RQDecomp3x3(M);
+            M = [cos(pi/4) sin(pi/4) 0 ; ...
+                -sin(pi/4) cos(pi/4) 0 ; ...
+                         0         0 1];
+            [Q,R] = qr(M);
+            [R,Q] = cv.RQDecomp3x3(M);
+            [R,Q,S] = cv.RQDecomp3x3(M);
+            validateattributes(R, {class(M)}, {'size',[3 3]});
+            assert(istriu(R), 'Not upper triangular');
+            assert(abs(norm(Q)-1) < 1e-9 && norm(Q'-inv(Q)) < 1e-9, 'Not orthogonal');
+            validateattributes(Q, {class(M)}, {'size',[3 3]});
+            validateattributes(S, {'struct'}, {'scalar'});
+            assert(all(ismember(TestRQDecomp3x3.fields, fieldnames(S))));
+            cellfun(@(f) validateattributes(S.(f), {class(M)}, ...
+                {'2d', 'size',[3 3]}), TestRQDecomp3x3.fields(1:3));
+            validateattributes(S.eulerAngles, {'double'}, ...
+                {'vector', 'numel',3});
         end
-        
+
         function test_error_1
             try
                 cv.RQDecomp3x3();
@@ -20,6 +33,5 @@ classdef TestRQDecomp3x3
             end
         end
     end
-    
-end
 
+end
