1f76d52ee21fef4730db42a9bc13bea377a461f9
Amro <amroamroamro@gmail.com>
2015-09-19 16:11:26 +0300

    update stereoRectify
    - matrices are 64f
    - change default value of flag 'ZeroDisparity' to true
    - update M-docs
    - add unit tests

--- src/+cv/stereoRectify.cpp
+++ src/+cv/stereoRectify.cpp
@@ -20,7 +20,7 @@ namespace {
  * @param roi2 ROI in rectified images in 2nd cam where all pixels are valid.
  * @return output MxArray struct object.
  */
-MxArray valueStruct(const Mat& R1, const Mat& R2, const Mat& P1, const Mat& P2,
+MxArray toStruct(const Mat& R1, const Mat& R2, const Mat& P1, const Mat& P2,
     const Mat& Q, const Rect& roi1, const Rect& roi2)
 {
     const char* fieldnames[] = {"R1", "R2", "P1", "P2", "Q", "roi1", "roi2"};
@@ -43,42 +43,42 @@ MxArray valueStruct(const Mat& R1, const Mat& R2, const Mat& P1, const Mat& P2,
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    if (nrhs<7 || ((nrhs%2)!=1) || nlhs>1)
-        mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
+    nargchk(nrhs>=7 && (nrhs%2)==1 && nlhs<=1);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
+    int flags = cv::CALIB_ZERO_DISPARITY;
     double alpha = -1;
-    Size newImageSize(0,0);
-    bool zeroDisparity = false;
+    Size newImageSize;
     for (int i=7; i<nrhs; i+=2) {
         string key(rhs[i].toString());
-        if (key=="Alpha")
+        if (key == "ZeroDisparity")
+            UPDATE_FLAG(flags, rhs[i+1].toBool(), cv::CALIB_ZERO_DISPARITY);
+        else if (key == "Alpha")
             alpha = rhs[i+1].toDouble();
-        else if (key=="NewImageSize")
+        else if (key == "NewImageSize")
             newImageSize = rhs[i+1].toSize();
-        else if (key=="ZeroDisparity")
-            zeroDisparity = rhs[i+1].toBool();
         else
             mexErrMsgIdAndTxt("mexopencv:error","Unrecognized option");
     }
-    int flags = ((zeroDisparity) ? cv::CALIB_ZERO_DISPARITY : 0);
 
     // Process
-    Mat cameraMatrix1(rhs[0].toMat()), distCoeffs1(rhs[1].toMat());
-    Mat cameraMatrix2(rhs[2].toMat()), distCoeffs2(rhs[3].toMat());
+    Mat cameraMatrix1(rhs[0].toMat(CV_64F)),
+        distCoeffs1(rhs[1].toMat(CV_64F)),
+        cameraMatrix2(rhs[2].toMat(CV_64F)),
+        distCoeffs2(rhs[3].toMat(CV_64F)),
+        R(rhs[5].toMat(CV_64F)),
+        T(rhs[6].toMat(CV_64F)),
+        R1, R2, P1, P2, Q;
     Size imageSize(rhs[4].toSize());
-    Mat R(rhs[5].toMat()), T(rhs[6].toMat());
-    Mat R1, R2, P1, P2, Q;
     Rect roi1, roi2;
     stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,
         imageSize, R, T, R1, R2, P1, P2, Q, flags, alpha, newImageSize,
         &roi1, &roi2);
-    plhs[0] = valueStruct(R1, R2, P1, P2, Q, roi1, roi2);
+    plhs[0] = toStruct(R1, R2, P1, P2, Q, roi1, roi2);
 }
--- test/unit_tests/TestStereoRectify.m
+++ test/unit_tests/TestStereoRectify.m
@@ -1,9 +1,52 @@
 classdef TestStereoRectify
     %TestStereoRectify
     properties (Constant)
+        fields = {'R1', 'R2', 'P1', 'P2', 'Q', 'roi1', 'roi2'};
     end
-    
+
     methods (Static)
+        function test_1
+            [opts, ipts1, ipts2, imgSize] = getPointsPair();
+            SS = cv.stereoCalibrate(opts, ipts1, ipts2, imgSize);
+            S = cv.stereoRectify(SS.cameraMatrix1, SS.distCoeffs1, ...
+                SS.cameraMatrix2, SS.distCoeffs2, imgSize, SS.R, SS.T);
+            validateattributes(S, {'struct'}, {'scalar'});
+            assert(all(ismember(TestStereoRectify.fields, fieldnames(S))));
+            validateattributes(S.R1, {'numeric'}, {'size',[3 3]});
+            validateattributes(S.R2, {'numeric'}, {'size',[3 3]});
+            validateattributes(S.P1, {'numeric'}, {'size',[3 4]});
+            validateattributes(S.P2, {'numeric'}, {'size',[3 4]});
+            validateattributes(S.Q, {'numeric'}, {'size',[4 4]});
+            validateattributes(S.roi1, {'numeric'}, ...
+                {'vector', 'numel',4, 'integer'});
+            validateattributes(S.roi2, {'numeric'}, ...
+                {'vector', 'numel',4, 'integer'});
+        end
+
+        function test_2
+            imageSize = [640 480];
+            cam1 = eye(3);
+            cam2 = eye(3);
+            distort1 = zeros(1,5);
+            distort2 = zeros(1,5);
+            R = [cv.getRotationMatrix2D(imageSize./2, 0, 1); 0 0 1];
+            T = [1; 0; 0];
+            S = cv.stereoRectify(cam1, distort1, cam2, distort2, imageSize, ...
+                R, T, 'ZeroDisparity',false, 'Alpha',-1, ...
+                'NewImageSize',imageSize./2);
+            validateattributes(S, {'struct'}, {'scalar'});
+            assert(all(ismember(TestStereoRectify.fields, fieldnames(S))));
+            validateattributes(S.R1, {'numeric'}, {'size',[3 3]});
+            validateattributes(S.R2, {'numeric'}, {'size',[3 3]});
+            validateattributes(S.P1, {'numeric'}, {'size',[3 4]});
+            validateattributes(S.P2, {'numeric'}, {'size',[3 4]});
+            validateattributes(S.Q, {'numeric'}, {'size',[4 4]});
+            validateattributes(S.roi1, {'numeric'}, ...
+                {'vector', 'numel',4, 'integer'});
+            validateattributes(S.roi2, {'numeric'}, ...
+                {'vector', 'numel',4, 'integer'});
+        end
+
         function test_error_1
             try
                 cv.stereoRectify();
@@ -13,6 +56,29 @@ classdef TestStereoRectify
             end
         end
     end
-    
+
 end
 
+function [opts, ipts1, ipts2, imgSize] = getPointsPair(N)
+    % consider only the first nine images
+    if nargin < 1, N = 1; end
+    assert(N <= 9);
+
+    % object points
+    patternsize = [9 6];
+    [X,Y] = ndgrid(1:patternsize(1), 1:patternsize(2));
+    opts = [X(:) Y(:) zeros(prod(patternsize),1)];  % Z=0
+
+    % image points 1
+    img = imread(fullfile(mexopencv.root(),'test',sprintf('left0%d.jpg',N)));
+    imgSize = [size(img,2) size(img,1)];
+    ipts1 = cv.findChessboardCorners(img, patternsize, 'FastCheck',true);
+    ipts1 = cv.cornerSubPix(img, ipts1);
+    ipts1 = cat(1, ipts1{:});
+
+    % image points 1
+    img = imread(fullfile(mexopencv.root(),'test',sprintf('right0%d.jpg',N)));
+    ipts2 = cv.findChessboardCorners(img, patternsize, 'FastCheck',true);
+    ipts2 = cv.cornerSubPix(img, ipts2);
+    ipts2 = cat(1, ipts2{:});
+end
