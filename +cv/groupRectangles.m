9fd94881c2a2b416ae3eb1b0f201fb3e6e104234
Amro <amroamroamro@gmail.com>
2015-09-24 18:54:28 +0300

    update groupRectangles
    - make groupThreshold an option instead of required argument
    - expose a more generic variant of the function, other overloaded
      versions call this one
    - extra outputs (weights and levelWeights) are requested on demand
    - return grouped rectangle in the same shape as input
      (numeric or cell array)
    - update M-docs
    - update unit tests

--- +cv/groupRectangles.m
+++ +cv/groupRectangles.m
@@ -1,31 +1,48 @@
 %GROUPRECTANGLES  Groups the object candidate rectangles
 %
-%    rects = cv.groupRectangles(rects, thresh)
+%    rects = cv.groupRectangles(rects)
+%    [rects,weights,levelWeights] = cv.groupRectangles(...)
 %    [...] = cv.groupRectangles(..., 'OptionName', optionValue, ...)
 %
 % ## Input
-% * __rects__ Cell array of rectangles, where each rectangle is represented
-%     as a 4-element vector [x,y,w,h].
-% * __thresh__ Minimum possible number of rectangles minus 1. The threshold
-%     is used in a group of rectangles to retain it.
+% * __rects__ Input cell array of rectangles, where each rectangle is
+%       represented as a 4-element vector `{[x,y,w,h], ...}`, or a numeric
+%       Nx4/Nx1x4/1xNx4 array.
 %
 % ## Output
-% * __rects__ Cell array of grouped rectangles. the same format to the
-%     input.
+% * __rects__ Output array of retained and grouped rectangles. Same format as
+%       input `rects` (either Nx4 numeric array or cell array). Grouped
+%       rectangles are the average of all rectangles in that cluster.
+% * __weights__ optional output of filled/updated weights, with same length as
+%       output `rects`. See `Weights` option. Corresponding grouped weights
+%       are the maximum weights of all rectangles in that cluster.
+% * __levelWeights__ optional output of filled/updated level weights, with
+%       same length as output `rects`. See `LevelWeights` option.
 %
 % ## Options
+% * __Thresh__ Minimum possible number of rectangles in a group minus 1. The
+%       threshold is used in a group of rectangles to decide whether to retain
+%       it or not. If less than or equal to zero, no grouping is performed.
+%       default 1 (i.e only groups with two or more rectangles are kept).
 % * __EPS__ Relative difference between sides of the rectangles to merge
-%     them into a group.
+%       them into a group. default 0.2
+% * __Weights__ optional vector of associated weights of same length as input
+%       `rects`. Not set by default
+% * __LevelWeights__ optional vector of doubles of same length as input
+%       `rects`. Not set by default
 %
-% The function clusters all the input rectangles using the rectangle
-% equivalence criteria that combines rectangles with similar sizes and
-% similar locations. The similarity is defined by eps. When `eps=0`, no
-% clustering is done at all. If `eps->+inf`, all the rectangles are put in
-% one cluster. Then, the small clusters containing less than or equal to
-% `thresh` rectangles are rejected. In each other cluster, the
-% average rectangle is computed and put into the output rectangle list.
+% The function is a wrapper for the generic partition function. It clusters
+% all the input rectangles using the rectangle equivalence criteria that
+% combines rectangles with similar sizes and similar locations. The similarity
+% is defined by `EPS`. When `EPS=0`, no clustering is done at all. If
+% `EPS -> +inf`, all the rectangles are put in one cluster. Then, the small
+% clusters containing less than or equal to `Thresh` rectangles are rejected.
+% In each other cluster, the average rectangle is computed and put into the
+% output rectangle list. The function also filters out small rectangles inside
+% larger ones that have more hits.
 %
 % The function is useful for non-max suppression of object detections.
 %
-% See also cv.CascadeClassifier
+% See also: cv.CascadeClassifier, cv.HOGDescriptor, cv.SimilarRects,
+%   cv.groupRectangles_meanshift, selectStrongestBbox
 %
--- src/+cv/groupRectangles.cpp
+++ src/+cv/groupRectangles.cpp
@@ -16,21 +16,29 @@ using namespace cv;
  * @param nrhs number of right-hand-side arguments
  * @param prhs pointers to mxArrays in the right-hand-side
  */
-void mexFunction( int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[] )
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Check the number of arguments
-    nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=1);
+    nargchk(nrhs>=1 && (nrhs%2)==1 && nlhs<=3);
 
     // Argument vector
-    vector<MxArray> rhs(prhs,prhs+nrhs);
+    vector<MxArray> rhs(prhs, prhs+nrhs);
 
     // Option processing
+    int groupThreshold = 1;
     double eps = 0.2;
-    for (int i=2; i<nrhs; i+=2) {
+    vector<int> weights;
+    vector<double> levelWeights;
+    for (int i=1; i<nrhs; i+=2) {
         string key(rhs[i].toString());
-        if (key=="EPS")
+        if (key == "Thresh")
+            groupThreshold = rhs[i+1].toInt();
+        else if (key == "EPS")
             eps = rhs[i+1].toDouble();
+        else if (key == "Weights")
+            weights = rhs[i+1].toVector<int>();
+        else if (key == "LevelWeights")
+            levelWeights = rhs[i+1].toVector<double>();
         else
             mexErrMsgIdAndTxt("mexopencv:error",
                 "Unrecognized option %s", key.c_str());
@@ -38,7 +46,17 @@ void mexFunction( int nlhs, mxArray *plhs[],
 
     // Process
     vector<Rect> rectList(rhs[0].toVector<Rect>());
-    int groupThreshold = rhs[1].toInt();
-    groupRectangles(rectList, groupThreshold, eps);
-    plhs[0] = MxArray(rectList);
+    if ((!weights.empty() && weights.size() != rectList.size()) ||
+        (!levelWeights.empty() && levelWeights.size() != rectList.size()))
+        mexErrMsgIdAndTxt("mexopencv:error", "Vectors are the wrong size");
+    groupRectangles(rectList, groupThreshold, eps,
+        (nlhs>1 || !weights.empty() ? &weights : NULL),
+        (nlhs>2 || !levelWeights.empty() ? &levelWeights : NULL));
+    plhs[0] = (rhs[0].isNumeric()) ?
+        MxArray(Mat(rectList, false).reshape(1,0)) :  // Nx4
+        MxArray(rectList);  // {[x,y,w,h], ...}
+    if (nlhs > 1)
+        plhs[1] = MxArray(weights);
+    if (nlhs > 2)
+        plhs[2] = MxArray(levelWeights);
 }
