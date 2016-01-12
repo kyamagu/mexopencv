a248a91ec4fe24af9e7c08b183c9ecfb7f8b0ed5
Amro <amroamroamro@gmail.com>
2016-01-12 20:06:54 +0200

    revert be074f1f: restore some previously disabled tests
    365fd93b fixed the Mat/MatND <-> MxArray conversion

--- test/unit_tests/TestCalcHist.m
+++ test/unit_tests/TestCalcHist.m
@@ -86,11 +86,8 @@ classdef TestCalcHist
             edges = {[0 1], [0 1], [0 1], [0 1]};
             histSize = [7 8 9 10];
             H = cv.calcHist(X, edges, 'HistSize',histSize, 'Uniform',true);
-            %---
-            %TODO: there's a bug in MxArray::MxArray(Mat) when mat.dims>2
-            %validateattributes(H, {'single'}, {'ndims',4, 'size',histSize});
-            %assert(isequal(sum(H(:)), h*w));
-            %---
+            validateattributes(H, {'single'}, {'ndims',4, 'size',histSize});
+            assert(isequal(sum(H(:)), h*w));
         end
 
         function test_error_1
