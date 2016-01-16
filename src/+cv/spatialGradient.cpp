9ac16567329074bd48b88cc715ebf75337a8b1fc
Amro <amroamroamro@gmail.com>
2016-01-16 15:08:30 +0200

    new spatialGradient function

---
+++ src/+cv/spatialGradient.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file spatialGradient.cpp
+ * @brief mex interface for cv::spatialGradient
+ * @ingroup imgproc
+ * @author Amro
+ * @date 2016
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check the number of arguments
+    nargchk(nrhs>=1 && (nrhs%2)==1 && nlhs<=2);
+
+    // Argument vector
+    vector<MxArray> rhs(prhs, prhs+nrhs);
+
+    // Option processing
+    int ksize = 3;
+    int borderType = cv::BORDER_DEFAULT;
+    for (int i=1; i<nrhs; i+=2) {
+        string key(rhs[i].toString());
+        if (key == "KSize")
+            ksize = rhs[i+1].toInt();
+        else if (key == "BorderType")
+            borderType = BorderType[rhs[i+1].toString()];
+        else
+            mexErrMsgIdAndTxt("mexopencv:error",
+                "Unrecognized option %s", key.c_str());
+    }
+
+    // Execute function
+    Mat src(rhs[0].toMat(CV_8U)), dx, dy;
+    spatialGradient(src, dx, dy, ksize, borderType);
+    plhs[0] = MxArray(dx);
+    if (nlhs > 1)
+        plhs[1] = MxArray(dy);
+}
