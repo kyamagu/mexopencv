07f5bc40335db472574d56b43fecd48bc9e6cf4c
Amro <amroamroamro@gmail.com>
2016-01-27 20:54:41 +0200

    [calib3d]: new sampsonDistance function

---
+++ test/unit_tests/TestSampsonDistance.m
@@ -0,0 +1,23 @@
+classdef TestSampsonDistance
+    %TestSampsonDistance
+
+    methods (Static)
+        function test_1
+            pt1 = [rand(1,2) 1];
+            pt2 = [rand(1,2) 1];
+            F = eye(3);
+            sd = cv.sampsonDistance(pt1, pt2, F);
+            validateattributes(sd, {'double'}, {'scalar'});
+        end
+
+        function test_error_1
+            try
+                cv.sampsonDistance();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+
+end
