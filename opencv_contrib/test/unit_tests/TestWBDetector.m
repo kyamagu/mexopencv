807637142ce7e6cf00989d6a28b81ee40aed34c2
Amro <amroamroamro@gmail.com>
2016-01-28 16:44:56 +0200

    new WBDetector class (fom xobjdetect module)

---
+++ opencv_contrib/test/unit_tests/TestWBDetector.m
@@ -0,0 +1,28 @@
+classdef TestWBDetector
+    %TestWBDetector
+
+    methods (Static)
+        function test_1
+            detector = cv.WBDetector();
+        end
+
+        function test_2
+            %TODO: we need a test set
+            if true
+                disp('SKIP');
+                return;
+            end
+
+            detector = cv.WBDetector();
+            detector.train('/path/to/pos', '/path/to/neg');    %TODO
+            img = cv.imread('path/to/img', 'Grayscale',true);  %TODO
+            [bboxes,conf] = detector.detect(img);
+            validateattributes(bboxes, {'cell'}, {'vector'});
+            cellfun(@(r) validateattributes(r, {'numeric'}, ...
+                {'vector', 'integer', 'numel',4}), bboxes);
+            validateattributes(conf, {'numeric'}, ...
+                {'vector', 'numel',numel(bboxes)});
+        end
+    end
+
+end
