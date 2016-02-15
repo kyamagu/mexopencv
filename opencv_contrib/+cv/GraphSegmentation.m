fe317d9241a0f1366e2b1bff7038146524f09778
Amro <amroamroamro@gmail.com>
2016-02-15 09:54:42 +0200

    ximgproc: new GraphSegmentation
    in addition to a sample demo

---
+++ opencv_contrib/+cv/GraphSegmentation.m
@@ -0,0 +1,185 @@
+classdef GraphSegmentation < handle
+    %GRAPHSEGMENTATION  Graph Based Segmentation algorithm
+    %
+    % The class implements the algorithm described in [PFF2004].
+    %
+    % ## References
+    % [PFF2004]:
+    % > Pedro F Felzenszwalb and Daniel P Huttenlocher. "Efficient graph-based
+    % > image segmentation". volume 59, pages 167-181. Springer, 2004.
+    %
+    % See also: cv.GraphSegmentation.GraphSegmentation
+    %
+
+    properties (SetAccess = private)
+        id    % Object ID
+    end
+
+    properties (Dependent)
+        % The sigma parameter, used to smooth image.
+        Sigma
+        % The k parameter of the algorithm.
+        K
+        % The minimum size of segments.
+        MinSize
+    end
+
+    methods
+        function this = GraphSegmentation(varargin)
+            %GRAPHSEGMENTATION  Creates a graph based segmentor
+            %
+            %    obj = cv.GraphSegmentation()
+            %    obj = cv.GraphSegmentation('OptionName',optionValue, ...)
+            %
+            % ## Options
+            % * __Sigma__ The sigma parameter, used to smooth image.
+            %       default 0.5
+            % * __K__ The k parameter of the algorithm. default 300
+            % * __MinSize__ The minimum size of segments. default 100
+            %
+            % See also: cv.GraphSegmentation.processImage
+            %
+            this.id = GraphSegmentation_(0, 'new', varargin{:});
+        end
+
+        function delete(this)
+            %DELETE  Destructor
+            %
+            % See also: cv.GraphSegmentation
+            %
+            GraphSegmentation_(this.id, 'delete');
+        end
+    end
+
+    %% Algorithm
+    methods
+        function clear(this)
+            %CLEAR  Clears the algorithm state
+            %
+            %    obj.clear()
+            %
+            % See also: cv.GraphSegmentation.empty, cv.GraphSegmentation.load
+            %
+            GraphSegmentation_(this.id, 'clear');
+        end
+
+        function b = empty(this)
+            %EMPTY  Checks if detector object is empty.
+            %
+            %    b = obj.empty()
+            %
+            % ## Output
+            % * __b__ Returns true if the detector object is empty (e.g in the
+            %       very beginning or after unsuccessful read).
+            %
+            % See also: cv.GraphSegmentation.clear, cv.GraphSegmentation.load
+            %
+            b = GraphSegmentation_(this.id, 'empty');
+        end
+
+        function save(this, filename)
+            %SAVE  Saves the algorithm parameters to a file
+            %
+            %    obj.save(filename)
+            %
+            % ## Input
+            % * __filename__ Name of the file to save to.
+            %
+            % This method stores the algorithm parameters in the specified
+            % XML or YAML file.
+            %
+            % See also: cv.GraphSegmentation.load
+            %
+            GraphSegmentation_(this.id, 'save', filename);
+        end
+
+        function load(this, fname_or_str, varargin)
+            %LOAD  Loads algorithm from a file or a string
+            %
+            %    obj.load(fname)
+            %    obj.load(str, 'FromString',true)
+            %    obj.load(..., 'OptionName',optionValue, ...)
+            %
+            % ## Input
+            % * __fname__ Name of the file to read.
+            % * __str__ String containing the serialized model you want to
+            %       load.
+            %
+            % ## Options
+            % * __ObjName__ The optional name of the node to read (if empty,
+            %       the first top-level node will be used). default empty
+            % * __FromString__ Logical flag to indicate whether the input is
+            %       a filename or a string containing the serialized model.
+            %       default false
+            %
+            % This method reads algorithm parameters from the specified XML or
+            % YAML file (either from disk or serialized string). The previous
+            % algorithm state is discarded.
+            %
+            % See also: cv.GraphSegmentation.save
+            %
+            GraphSegmentation_(this.id, 'load', fname_or_str, varargin{:});
+        end
+
+        function name = getDefaultName(this)
+            %GETDEFAULTNAME  Returns the algorithm string identifier
+            %
+            %    name = obj.getDefaultName()
+            %
+            % ## Output
+            % * __name__ This string is used as top level XML/YML node tag
+            %       when the object is saved to a file or string.
+            %
+            % See also: cv.GraphSegmentation.save, cv.GraphSegmentation.load
+            %
+            name = GraphSegmentation_(this.id, 'getDefaultName');
+        end
+    end
+
+    %% GraphSegmentation
+    methods
+        function dst = processImage(this, src)
+            %PROCESSIMAGE  Segment an image and store output in dst
+            %
+            %    dst = obj.processImage(src)
+            %
+            % ## Input
+            % * __src__ The input image. Any number of channel (1 (Eg: Gray),
+            %       3 (Eg: RGB), 4 (Eg: RGB-D)) can be provided.
+            %
+            % ## Output
+            % * __dst__ The output segmentation. It's a `int32` matrix with
+            %       the same number of cols and rows as input image, with an
+            %       unique, sequential, id for each pixel.
+            %
+            % See also: cv.GraphSegmentation.GraphSegmentation
+            %
+            dst = GraphSegmentation_(this.id, 'processImage', src);
+        end
+    end
+
+    %% Getters/Setters
+    methods
+        function value = get.Sigma(this)
+            value = GraphSegmentation_(this.id, 'get', 'Sigma');
+        end
+        function set.Sigma(this, value)
+            GraphSegmentation_(this.id, 'set', 'Sigma', value);
+        end
+
+        function value = get.K(this)
+            value = GraphSegmentation_(this.id, 'get', 'K');
+        end
+        function set.K(this, value)
+            GraphSegmentation_(this.id, 'set', 'K', value);
+        end
+
+        function value = get.MinSize(this)
+            value = GraphSegmentation_(this.id, 'get', 'MinSize');
+        end
+        function set.MinSize(this, value)
+            GraphSegmentation_(this.id, 'set', 'MinSize', value);
+        end
+    end
+
+end
---
+++ opencv_contrib/src/+cv/private/GraphSegmentation_.cpp
@@ -0,0 +1,147 @@
+/**
+ * @file GraphSegmentation_.cpp
+ * @brief mex interface for cv::ximgproc::segmentation::GraphSegmentation
+ * @ingroup ximgproc
+ * @author Amro
+ * @date 2016
+ */
+#include "mexopencv.hpp"
+#include "opencv2/ximgproc.hpp"
+using namespace std;
+using namespace cv;
+using namespace cv::ximgproc::segmentation;
+
+// Persistent objects
+namespace {
+/// Last object id to allocate
+int last_id = 0;
+/// Object container
+map<int,Ptr<GraphSegmentation> > obj_;
+}
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check the number of arguments
+    nargchk(nrhs>=2 && nlhs<=1);
+
+    // Argument vector
+    vector<MxArray> rhs(prhs, prhs+nrhs);
+    int id = rhs[0].toInt();
+    string method(rhs[1].toString());
+
+    // Constructor is called. Create a new object from argument
+    if (method == "new") {
+        nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=1);
+        double sigma = 0.5;
+        float k = 300;
+        int min_size = 100;
+        for (int i=2; i<nrhs; i+=2) {
+            string key(rhs[i].toString());
+            if (key == "Sigma")
+                sigma = rhs[i+1].toDouble();
+            else if (key == "K")
+                k = rhs[i+1].toFloat();
+            else if (key == "MinSize")
+                min_size = rhs[i+1].toInt();
+            else
+                mexErrMsgIdAndTxt("mexopencv:error",
+                    "Unrecognized option %s", key.c_str());
+        }
+        obj_[++last_id] = createGraphSegmentation(sigma, k, min_size);
+        plhs[0] = MxArray(last_id);
+        return;
+    }
+
+    // Big operation switch
+    Ptr<GraphSegmentation> obj = obj_[id];
+    if (method == "delete") {
+        nargchk(nrhs==2 && nlhs==0);
+        obj_.erase(id);
+    }
+    else if (method == "clear") {
+        nargchk(nrhs==2 && nlhs==0);
+        obj->clear();
+    }
+    else if (method == "load") {
+        nargchk(nrhs>=3 && (nrhs%2)==1 && nlhs==0);
+        string objname;
+        bool loadFromString = false;
+        for (int i=3; i<nrhs; i+=2) {
+            string key(rhs[i].toString());
+            if (key == "ObjName")
+                objname = rhs[i+1].toString();
+            else if (key == "FromString")
+                loadFromString = rhs[i+1].toBool();
+            else
+                mexErrMsgIdAndTxt("mexopencv:error",
+                    "Unrecognized option %s", key.c_str());
+        }
+        /*
+        obj_[id] = (loadFromString ?
+            Algorithm::loadFromString<GraphSegmentation>(rhs[2].toString(), objname) :
+            Algorithm::load<GraphSegmentation>(rhs[2].toString(), objname));
+        */
+        ///*
+        // HACK: workaround for missing GraphSegmentation::create()
+        FileStorage fs(rhs[2].toString(), FileStorage::READ +
+            (loadFromString ? FileStorage::MEMORY : 0));
+        obj->read(objname.empty() ? fs.getFirstTopLevelNode() : fs[objname]);
+        if (obj.empty())
+            mexErrMsgIdAndTxt("mexopencv:error", "Failed to load algorithm");
+        //*/
+    }
+    else if (method == "save") {
+        nargchk(nrhs==3 && nlhs==0);
+        obj->save(rhs[2].toString());
+    }
+    else if (method == "empty") {
+        nargchk(nrhs==2 && nlhs<=1);
+        plhs[0] = MxArray(obj->empty());
+    }
+    else if (method == "getDefaultName") {
+        nargchk(nrhs==2 && nlhs<=1);
+        plhs[0] = MxArray(obj->getDefaultName());
+    }
+    else if (method == "processImage") {
+        nargchk(nrhs==3 && nlhs<=1);
+        Mat src(rhs[2].toMat()), dst;
+        obj->processImage(src, dst);
+        plhs[0] = MxArray(dst);
+    }
+    else if (method == "get") {
+        nargchk(nrhs==3 && nlhs<=1);
+        string prop(rhs[2].toString());
+        if (prop == "Sigma")
+            plhs[0] = MxArray(obj->getSigma());
+        else if (prop == "K")
+            plhs[0] = MxArray(obj->getK());
+        else if (prop == "MinSize")
+            plhs[0] = MxArray(obj->getMinSize());
+        else
+            mexErrMsgIdAndTxt("mexopencv:error",
+                "Unrecognized property %s", prop.c_str());
+    }
+    else if (method == "set") {
+        nargchk(nrhs==4 && nlhs==0);
+        string prop(rhs[2].toString());
+        if (prop == "Sigma")
+            obj->setSigma(rhs[3].toDouble());
+        else if (prop == "K")
+            obj->setK(rhs[3].toFloat());
+        else if (prop == "MinSize")
+            obj->setMinSize(rhs[3].toInt());
+        else
+            mexErrMsgIdAndTxt("mexopencv:error",
+                "Unrecognized property %s", prop.c_str());
+    }
+    else
+        mexErrMsgIdAndTxt("mexopencv:error",
+            "Unrecognized operation %s", method.c_str());
+}
