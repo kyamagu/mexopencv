f3273fb8adc407db3e1309d1e0cd79917033cfa8
Amro <amroamroamro@gmail.com>
2016-08-24 14:27:24 +0300

    new "plot" contrib module

---
+++ opencv_contrib/src/+cv/private/Plot2d_.cpp
@@ -0,0 +1,147 @@
+/**
+ * @file Plot2d_.cpp
+ * @brief mex interface for cv::plot::Plot2d
+ * @ingroup plot
+ * @author Amro
+ * @date 2016
+ */
+#include "mexopencv.hpp"
+#include "opencv2/plot.hpp"
+using namespace std;
+using namespace cv;
+using namespace plot;
+
+// Persistent objects
+namespace {
+/// Last object id to allocate
+int last_id = 0;
+/// Object container
+map<int,Ptr<Plot2d> > obj_;
+}
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check the number of arguments
+    nargchk(nrhs>=2 && nlhs<=1);
+
+    // Arguments vector
+    vector<MxArray> rhs(prhs, prhs+nrhs);
+    int id = rhs[0].toInt();
+    string method(rhs[1].toString());
+
+    // Constructor is called. Create a new object from argument
+    if (method == "new") {
+        nargchk((nrhs==3||nrhs==4) && nlhs<=1);
+        Ptr<Plot2d> p;
+        if (nrhs == 3) {
+            Mat data(rhs[2].toMat(CV_64F));
+            p = createPlot2d(data);
+        }
+        else {
+            Mat dataX(rhs[2].toMat(CV_64F)),
+                dataY(rhs[3].toMat(CV_64F));
+            p = createPlot2d(dataX, dataY);
+        }
+        obj_[++last_id] = p;
+        plhs[0] = MxArray(last_id);
+        return;
+    }
+
+    // Big operation switch
+    Ptr<Plot2d> obj = obj_[id];
+    if (method == "delete") {
+        nargchk(nrhs==2 && nlhs==0);
+        obj_.erase(id);
+    }
+    else if (method == "clear") {
+        nargchk(nrhs==2 && nlhs==0);
+        obj->clear();
+    }
+    else if (method == "load") {
+        nargchk(nrhs>=3 && (nrhs%2)==1 && nlhs==0);
+        string objname;
+        bool loadFromString = false;
+        for (int i=3; i<nrhs; i+=2) {
+            string key(rhs[i].toString());
+            if (key == "ObjName")
+                objname = rhs[i+1].toString();
+            else if (key == "FromString")
+                loadFromString = rhs[i+1].toBool();
+            else
+                mexErrMsgIdAndTxt("mexopencv:error",
+                    "Unrecognized option %s", key.c_str());
+        }
+        /*
+        obj_[id] = (loadFromString ?
+            Algorithm::loadFromString<Plot2d>(rhs[2].toString(), objname) :
+            Algorithm::load<Plot2d>(rhs[2].toString(), objname));
+        */
+        ///*
+        // HACK: workaround for missing Plot2d::create()
+        FileStorage fs(rhs[2].toString(), FileStorage::READ +
+            (loadFromString ? FileStorage::MEMORY : 0));
+        obj->read(objname.empty() ? fs.getFirstTopLevelNode() : fs[objname]);
+        if (obj.empty())
+            mexErrMsgIdAndTxt("mexopencv:error", "Failed to load algorithm");
+        //*/
+    }
+    else if (method == "save") {
+        nargchk(nrhs==3 && nlhs==0);
+        obj->save(rhs[2].toString());
+    }
+    else if (method == "empty") {
+        nargchk(nrhs==2 && nlhs<=1);
+        plhs[0] = MxArray(obj->empty());
+    }
+    else if (method == "getDefaultName") {
+        nargchk(nrhs==2 && nlhs<=1);
+        plhs[0] = MxArray(obj->getDefaultName());
+    }
+    else if (method == "render") {
+        nargchk(nrhs==2 && nlhs<=1);
+        Mat plotResult;
+        obj->render(plotResult);
+        plhs[0] = MxArray(plotResult);
+    }
+    else if (method == "set") {
+        nargchk(nrhs==4 && nlhs==0);
+        string prop(rhs[2].toString());
+        if (prop == "MinX")
+            obj->setMinX(rhs[3].toDouble());
+        else if (prop == "MinY")
+            obj->setMinY(rhs[3].toDouble());
+        else if (prop == "MaxX")
+            obj->setMaxX(rhs[3].toDouble());
+        else if (prop == "MaxY")
+            obj->setMaxY(rhs[3].toDouble());
+        else if (prop == "PlotLineWidth")
+            obj->setPlotLineWidth(rhs[3].toInt());
+        else if (prop == "PlotLineColor")
+            obj->setPlotLineColor(rhs[3].toScalar());
+        else if (prop == "PlotBackgroundColor")
+            obj->setPlotBackgroundColor(rhs[3].toScalar());
+        else if (prop == "PlotAxisColor")
+            obj->setPlotAxisColor(rhs[3].toScalar());
+        else if (prop == "PlotGridColor")
+            obj->setPlotGridColor(rhs[3].toScalar());
+        else if (prop == "PlotTextColor")
+            obj->setPlotTextColor(rhs[3].toScalar());
+        else if (prop == "PlotSize") {
+            Size sz(rhs[3].toSize());
+            obj->setPlotSize(sz.width, sz.height);
+        }
+        else
+            mexErrMsgIdAndTxt("mexopencv:error",
+                "Unrecognized property %s", prop.c_str());
+    }
+    else
+        mexErrMsgIdAndTxt("mexopencv:error",
+            "Unrecognized operation %s", method.c_str());
+}
---
+++ opencv_contrib/test/unit_tests/TestPlot2d.m
@@ -0,0 +1,33 @@
+classdef TestPlot2d
+    %TestPlot2d
+
+    methods (Static)
+        function test_y
+            y = cumsum(rand(100,1) - 0.5);
+            p = cv.Plot2d(y);
+            p.PlotSize = [640 480];
+            p.PlotLineWidth = 2;
+            out = p.render();
+            validateattributes(out, {'uint8'}, {'size',[480 640 3]});
+        end
+
+        function test_xy
+            x = linspace(-2*pi, 2*pi, 100);
+            y = sin(x);
+            p = cv.Plot2d(x,y);
+            p.PlotSize = [400 300];
+            p.PlotLineColor = [255 255 0];
+            out = p.render();
+            validateattributes(out, {'uint8'}, {'size',[300 400 3]});
+        end
+
+        function test_error_1
+            try
+                cv.Plot2d();
+                throw('UnitTest:Fail');
+            catch e
+                assert(strcmp(e.identifier,'mexopencv:error'));
+            end
+        end
+    end
+end
