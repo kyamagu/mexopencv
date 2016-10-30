01bfb8e092488ade69df6faee08bdaff85fe57d1
Amro <amroamroamro@gmail.com>
2016-10-30 17:58:03 +0200

    FileStorage: more tests

--- test/unit_tests/TestFileStorage.m
+++ test/unit_tests/TestFileStorage.m
@@ -2,16 +2,80 @@ classdef TestFileStorage
     %TestFileStorage
 
     methods (Static)
-        function test_1
-            S = struct('field1', magic(4), 'field2', 'this is the second field');
-            fname = [tempname '.yml'];
-            cv.FileStorage(fname,S);
-            S2 = cv.FileStorage(fname);
-            assert(all(S.field1(:)==S2.field1(:)));
-            assert(strcmp(S.field2,S2.field2));
-            if exist(fname,'file')
-                delete(fname);
+        function test_write_read_extensions
+            S = struct('field1',magic(4), 'field2','a second field');
+            exts = {'.xml', '.yml', '.xml.gz', '.yml.gz'};
+            for i=1:numel(exts)
+                fname = [tempname() exts{i}];
+                cleanObj = onCleanup(@() delete(fname));
+
+                cv.FileStorage(fname, S);
+                assert(exist(fname, 'file') == 2, ...
+                    'Failed to write %s file', exts{i});
+
+                S2 = cv.FileStorage(fname);
+                validateattributes(S, {'struct'}, {'scalar'});
+                assert(all(ismember(fieldnames(S), fieldnames(S2))));
+                assert(isequal(S.field1, S2.field1));
+                assert(isequal(S.field2, S2.field2));
             end
+        end
+
+        function test_write_read_inmemory
+            S = struct('field1',magic(4), 'field2','hello world');
+
+            str = cv.FileStorage('dummy.yml', S);
+            validateattributes(str, {'char'}, {'row', 'nonempty'});
+
+            [S2,~] = cv.FileStorage(str);
+            validateattributes(S2, {'struct'}, {'scalar'});
+            assert(isequal(S, S2));
+        end
+
+        function test_write_variables
+            vars = {pi, 1:5, 'hello world'};
+            str = cv.FileStorage('dummy.yml', vars{:});
+            str2 = cv.FileStorage('dummy.yml', struct('dummy',{vars}));
+            assert(isequal(str, str2));
+        end
+
+        function test_numerics
+            S = struct('a',int32(-1), 'b',uint8(1), 'c',pi, ...
+                'd',eps('single'), 'e',NaN, 'f',Inf, 'g',realmax);
+            str = cv.FileStorage('dummy.yml', S);
+            [S2,~] = cv.FileStorage(str);
+            assert(isequaln(S, S2));  % NOTE: S2.* are all double
+        end
+
+        function test_struct_array
+            S = dir(mexopencv.root());  % Nx1 struct array
+            SS = struct('dummy',S);     % wrap inside a scalar struct
+            str = cv.FileStorage('dummy.yml', SS);
+            [S2,~] = cv.FileStorage(str);
+            S2 = S2.dummy;  % unwrap
+            S2 = [S2{:}];   % cell array of structs to 1xN struct array
+            assert(isequal(S(:), S2(:)));
+        end
+
+        function test_struct
+            S = struct('field1',struct('a',1, 'b',2), 'field2',struct());
+            str = cv.FileStorage('dummy.yml', S);
+            [S2,~] = cv.FileStorage(str);
+            assert(isequal(S, S2));
+        end
+
+        function test_cell
+            S = struct('field1',1, 'field2',{{2, 'hi', magic(4)}});
+            str = cv.FileStorage('dummy.yml', S);
+            [S2,~] = cv.FileStorage(str);
+            assert(isequal(S, S2));
+        end
+
+        function test_cellstr
+            S = struct('field1',1, 'field2',{{'hi','hello there'}});
+            str = cv.FileStorage('dummy.yml', S);
+            [S2,~] = cv.FileStorage(str);
+            assert(isequal(S, S2));
         end
 
         function test_error_1
