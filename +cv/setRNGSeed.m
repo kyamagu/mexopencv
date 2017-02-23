cc813c65ec4338e4e89b6c0fe83beb2696f95396
Amro <amroamroamro@gmail.com>
2017-02-23 19:59:46 +0200

    core: new setRNGSeed

---
+++ +cv/setRNGSeed.m
@@ -0,0 +1,12 @@
+%SETRNGSEED  Sets state of default random number generator
+%
+%    cv.setRNGSeed(seed)
+%
+% ## Input
+% * __seed__ integer, new state for default random number generator.
+%
+% The function cv.setRNGSeed sets state of default random number generator to
+% custom value.
+%
+% See also: rng, rand, randn
+%
---
+++ src/+cv/setRNGSeed.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file setRNGSeed.cpp
+ * @brief mex interface for cv::setRNGSeed
+ * @ingroup core
+ * @author Amro
+ * @date 2017
+ */
+#include "mexopencv.hpp"
+using namespace std;
+using namespace cv;
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check the number of arguments
+    nargchk(nrhs==1 && nlhs==0);
+
+    // Process
+    int seed = MxArray(prhs[0]).toInt();
+    setRNGSeed(seed);
+}
