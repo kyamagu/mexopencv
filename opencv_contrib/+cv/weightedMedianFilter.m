3afe78534c482b47c8e33e5549d1cc24ca0c3f27
Amro <amroamroamro@gmail.com>
2017-03-03 19:53:23 +0200

    ximgproc: new weightedMedianFilter

---
+++ opencv_contrib/+cv/weightedMedianFilter.m
@@ -0,0 +1,41 @@
+%WEIGHTEDMEDIANFILTER  Applies weighted median filter to an image
+%
+%    dst = cv.weightedMedianFilter(src, joint)
+%    dst = cv.weightedMedianFilter(src, joint, 'OptionName',optionValue, ...)
+%
+% ## Input
+% * __src__ Source 8-bit or floating-point, 1-channel or 3-channel image.
+% * __joint__ Joint 8-bit, 1-channel or 3-channel image.
+%
+% ## Output
+% * __dst__ Destination image of the same size and type as `src`.
+%
+% ## Options
+% * __Radius__ Radius of filtering kernel, should be a positive integer.
+%       Default 7
+% * __Sigma__ Filter range standard deviation for the joint image.
+%       Default 25.5
+% * __WeightType__ The type of weight definition. Specifies weight types of
+%       weighted median filter, default 'EXP'. One of:
+%       * __EXP__ `exp(-|I1-I2|^2 / (2*sigma^2))`
+%       * __IV1__ `(|I1-I2| + sigma)^-1`
+%       * __IV2__ `(|I1-I2|^2 + sigma^2)^-1`
+%       * __COS__ `dot(I1,I2) / (|I1|*|I2|)`
+%       * __JAC__ `(min(r1,r2) + min(g1,g2) + min(b1,b2)) / (max(r1,r2) + max(g1,g2) + max(b1,b2))`
+%       * __OFF__ unweighted
+% * __Mask__ A 0-1 mask that has the same size with `I`. This mask is used to
+%       ignore the effect of some pixels. If the pixel value on mask is 0, the
+%       pixel will be ignored when maintaining the joint-histogram. This is
+%       useful for applications like optical flow occlusion handling.
+%       Not set by default.
+%
+% For more details about this implementation, please see [zhang2014100+].
+%
+% ## References
+% [zhang2014100+]:
+% > Qi Zhang, Li Xu, and Jiaya Jia. "100+ Times Faster Weighted Median Filter
+% > (WMF)". In Computer Vision and Pattern Recognition (CVPR), 2014 IEEE
+% > Conference on, pages 2830-2837. IEEE, 2014.
+%
+% See also: cv.medianBlur, cv.jointBilateralFilter
+%
---
+++ opencv_contrib/src/+cv/weightedMedianFilter.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file weightedMedianFilter.cpp
+ * @brief mex interface for cv::ximgproc::weightedMedianFilter
+ * @ingroup ximgproc
+ * @author Amro
+ * @date 2017
+ */
+#include "mexopencv.hpp"
+#include "opencv2/ximgproc.hpp"
+using namespace std;
+using namespace cv;
+using namespace cv::ximgproc;
+
+namespace {
+/// weight type options of weighted median filter
+const ConstMap<string, WMFWeightType> WeightTypeMap = ConstMap<string, WMFWeightType>
+    ("EXP", cv::ximgproc::WMF_EXP)
+    ("IV1", cv::ximgproc::WMF_IV1)
+    ("IV2", cv::ximgproc::WMF_IV2)
+    ("COS", cv::ximgproc::WMF_COS)
+    ("JAC", cv::ximgproc::WMF_JAC)
+    ("OFF", cv::ximgproc::WMF_OFF);
+}
+
+/**
+ * Main entry called from Matlab
+ * @param nlhs number of left-hand-side arguments
+ * @param plhs pointers to mxArrays in the left-hand-side
+ * @param nrhs number of right-hand-side arguments
+ * @param prhs pointers to mxArrays in the right-hand-side
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check the number of arguments
+    nargchk(nrhs>=2 && (nrhs%2)==0 && nlhs<=1);
+
+    // Argument vector
+    vector<MxArray> rhs(prhs, prhs+nrhs);
+
+    // Option processing
+    int r = 7;
+    double sigma = 25.5;
+    WMFWeightType weightType = cv::ximgproc::WMF_EXP;
+    Mat mask;
+    for (int i=2; i<nrhs; i+=2) {
+        string key(rhs[i].toString());
+        if (key == "Radius")
+            r = rhs[i+1].toInt();
+        else if (key == "Sigma")
+            sigma = rhs[i+1].toDouble();
+        else if (key == "WeightType")
+            weightType = WeightTypeMap[rhs[i+1].toString()];
+        else if (key == "Mask")
+            mask = rhs[i+1].toMat(CV_8U);
+        else
+            mexErrMsgIdAndTxt("mexopencv:error",
+                "Unrecognized option %s", key.c_str());
+    }
+
+    // Process
+    Mat src(rhs[0].toMat(rhs[0].isUint8() ? CV_8U : CV_32F)),
+        joint(rhs[1].toMat(CV_8U)),
+        dst;
+    weightedMedianFilter(joint, src, dst,
+        r, sigma, weightType, mask);
+    plhs[0] = MxArray(dst);
+}
