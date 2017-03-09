29ff5aba505dd460bfa8d3e918387d5082a1ee7f
Amro <amroamroamro@gmail.com>
2017-03-09 17:57:53 +0200

    aruco: sample edits

--- opencv_contrib/samples/aruco_calibrate_camera_charuco_demo.m
+++ opencv_contrib/samples/aruco_calibrate_camera_charuco_demo.m
@@ -15,8 +15,8 @@
 vidFile = '';                   % Use video file instead of camera as input
 squaresX = 5;                   % Number of squares in X direction
 squaresY = 7;                   % Number of squares in Y direction
-squareLength = 60;              % Square side lenght (in pixels)
-markerLength = 30;              % Marker side lenght (in pixels)
+squareLength = 60;              % Square side length (in pixels)
+markerLength = 30;              % Marker side length (in pixels)
 dictionaryId = '6x6_250';       % Dictionary id
 refindStrategy = false;         % Apply refined strategy
 showChessboardCorners = false;  % Show detected chessboard corners after calibration
@@ -38,15 +38,18 @@ end
 % marker detector parameters
 detectorParams = struct();
 if false
-    detectorParams.nMarkers = 1024;
-    detectorParams.adaptiveThreshWinSizeMax = 21;
+    %detectorParams.nMarkers = 1024;
+    detectorParams.adaptiveThreshWinSizeMin = 3;
+    detectorParams.adaptiveThreshWinSizeMax = 23;
+    detectorParams.adaptiveThreshWinSizeStep = 10;
     detectorParams.adaptiveThreshConstant = 7;
     detectorParams.minMarkerPerimeterRate = 0.03;
     detectorParams.maxMarkerPerimeterRate = 4.0;
     detectorParams.polygonalApproxAccuracyRate = 0.05;
-    detectorParams.minCornerDistanceRate = 10.0;
+    detectorParams.minCornerDistanceRate = 0.05;
     detectorParams.minDistanceToBorder = 3;
-    detectorParams.minMarkerDistanceRate = 10.0;
+    detectorParams.minMarkerDistanceRate = 0.05;
+    detectorParams.doCornerRefinement = false;
     detectorParams.cornerRefinementWinSize = 5;
     detectorParams.cornerRefinementMaxIterations = 30;
     detectorParams.cornerRefinementMinAccuracy = 0.1;
@@ -54,6 +57,8 @@ if false
     detectorParams.perspectiveRemovePixelPerCell = 8;
     detectorParams.perspectiveRemoveIgnoredMarginPerCell = 0.13;
     detectorParams.maxErroneousBitsInBorderRate = 0.04;
+    detectorParams.minOtsuStdDev = 5.0;
+    detectorParams.errorCorrectionRate = 0.6;
 end
 
 % create charuco board
--- opencv_contrib/samples/aruco_detect_board_demo.m
+++ opencv_contrib/samples/aruco_detect_board_demo.m
@@ -31,15 +31,18 @@ end
 % marker detector parameters
 detectorParams = struct();
 if false
-    detectorParams.nMarkers = 1024;
-    detectorParams.adaptiveThreshWinSizeMax = 21;
+    %detectorParams.nMarkers = 1024;
+    detectorParams.adaptiveThreshWinSizeMin = 3;
+    detectorParams.adaptiveThreshWinSizeMax = 23;
+    detectorParams.adaptiveThreshWinSizeStep = 10;
     detectorParams.adaptiveThreshConstant = 7;
     detectorParams.minMarkerPerimeterRate = 0.03;
     detectorParams.maxMarkerPerimeterRate = 4.0;
     detectorParams.polygonalApproxAccuracyRate = 0.05;
-    detectorParams.minCornerDistanceRate = 10.0;
+    detectorParams.minCornerDistanceRate = 0.05;
     detectorParams.minDistanceToBorder = 3;
-    detectorParams.minMarkerDistanceRate = 10.0;
+    detectorParams.minMarkerDistanceRate = 0.05;
+    detectorParams.doCornerRefinement = false;
     detectorParams.cornerRefinementWinSize = 5;
     detectorParams.cornerRefinementMaxIterations = 30;
     detectorParams.cornerRefinementMinAccuracy = 0.1;
@@ -47,6 +50,8 @@ if false
     detectorParams.perspectiveRemovePixelPerCell = 8;
     detectorParams.perspectiveRemoveIgnoredMarginPerCell = 0.13;
     detectorParams.maxErroneousBitsInBorderRate = 0.04;
+    detectorParams.minOtsuStdDev = 5.0;
+    detectorParams.errorCorrectionRate = 0.6;
 end
 detectorParams.doCornerRefinement = true;  % do corner refinement in markers
 
