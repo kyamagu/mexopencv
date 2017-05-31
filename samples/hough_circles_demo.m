f2ffca7b331c3286641c493eb98db19609f775a8
Amro <amroamroamro@gmail.com>
2017-05-31 19:26:13 +0300

    samples: update hough detection demos
    - expand docs
    - alternative images
    - vectorized drawing calls
    - minor edits

--- samples/hough_circles_demo.m
+++ samples/hough_circles_demo.m
@@ -1,32 +1,80 @@
-%% Hough circles demo
+%% Hough Circle Transform
 % An example using the Hough circle detector.
 %
-% This program demonstrates circle finding with the Hough transform.
+% This program demonstrates circle finding with the Hough transform. We show
+% how to use the OpenCV function |cv.HoughCircles| to detect circles in an
+% image.
 %
-% <https://github.com/opencv/opencv/blob/3.1.0/samples/cpp/houghcircles.cpp>
+% <https://github.com/opencv/opencv/blob/3.2.0/samples/cpp/houghcircles.cpp>,
+% <https://github.com/opencv/opencv/blob/3.2.0/samples/python/houghcircles.py>,
+% <http://docs.opencv.org/3.2.0/d4/d70/tutorial_hough_circle.html>,
+% <https://github.com/opencv/opencv/blob/3.2.0/samples/cpp/tutorial_code/ImgTrans/HoughCircle_Demo.cpp>,
+% <http://docs.opencv.org/3.2.0/da/d53/tutorial_py_houghcircles.html>
 %
 
-%% Input image
-if mexopencv.isOctave()
-    fname = fullfile(mexopencv.root(), 'test', 'detect_blob.png');
-else
-    fname = which('coins.png');
-    assert(~isempty(fname), 'Image not found');
-end
-img = cv.imread(fname, 'Flags',1);
+%% Theory
+%
+% The Hough Circle Transform works in a roughly analogous way to the
+% Hough Line Transform.
+%
+% In the line detection case, a line was defined by two parameters
+% $(r, \theta)$. In the circle case, we need three parameters to define a
+% circle:
+%
+% $$C : ( x_{center}, y_{center}, r )$$
+%
+% where $(x_{center}, y_{center})$ define the center position (green point)
+% and $r$ is the radius, which allows us to completely define a circle, as it
+% can be seen below:
+%
+% <<http://docs.opencv.org/3.2.0/Hough_Circle_Tutorial_Theory_0.jpg>>
+%
+% For sake of efficiency, OpenCV implements a detection method slightly
+% trickier than the standard Hough Transform: _The Hough gradient method_,
+% which is made up of two main stages:
+%
+% * the first stage involves edge detection and finding the possible circle
+%   centers
+% * the second stage finds the best radius for each candidate center
+%
+% For more details, please check the book _Learning OpenCV_ or your favorite
+% Computer Vision bibliography.
+%
 
-imshow(img), title('circles')
-
-%% Processing
-% convert to grayscale
-gray = cv.cvtColor(img, 'RGB2GRAY');
+%% Code
+%
+% This program:
+%
+% * Loads an image and blur it to reduce the noise
+% * Applies the _Hough Circle Transform_ to the blurred image .
+% * Display the detected circle in a window.
+%
 
 %%
-% reduce noise, otherwise a lot of false circles may be detected
-%gray = cv.GaussianBlur(gray, 'KSize',[7,7], 'SigmaX',0.9, 'SigmaY',0.9);
-gray = cv.medianBlur(gray, 'KSize',5);
+% Input image
+if ~mexopencv.isOctave() && mexopencv.require('images')
+    fname = which('coins.png');
+else
+    fname = fullfile(mexopencv.root(), 'test', 'detect_blob.png');
+end
+assert(~isempty(fname) && exist(fname,'file')==2, 'Image not found');
+img = cv.imread(fname, 'Color',true);
+figure, imshow(img), title('source')
 
-%% HoughCircles
+%%
+% convert it to grayscale, and reduce noise to avoid false circles detection
+gray = cv.cvtColor(img, 'RGB2GRAY');
+if true
+    gray = cv.medianBlur(gray, 'KSize',5);
+else
+    gray = cv.GaussianBlur(gray, 'KSize',[7,7], 'SigmaX',0.9, 'SigmaY',0.9);
+end
+
+%%
+% Hough Circle Transform
+%
+% * change |MinDist| to detect circles with different distances to each other
+% * change |MinRadius| and |MaxRadius| to detect circles of different sizes
 tic
 circles = cv.HoughCircles(gray, 'Method','Gradient', 'DP',2, ...
     'MinDist',size(gray,1)/8, 'Param1',200, 'Param2',100, ...
@@ -34,17 +82,12 @@ circles = cv.HoughCircles(gray, 'Method','Gradient', 'DP',2, ...
 toc
 
 %%
-for i=1:numel(circles)
-    center = round(circles{i}(1:2));
-    radius = round(circles{i}(3));
-
-    % draw the circle outline
-    img = cv.circle(img, center, radius, 'Color',[0,0,255], ...
-        'Thickness',2, 'LineType','AA');
-
-    % draw the circle center
-    img = cv.circle(img, center, 3, 'Color',[0,255,0], ...
-        'Thickness','Filled', 'LineType','AA');
-end
-
-imshow(img), title('detected circles')
+% draw detected circles (outlines and centers), and display the result
+circles = cat(1, circles{:});
+center = round(circles(:,1:2));
+radius = round(circles(:,3));
+out = cv.circle(img, center, radius, 'Color',[0 0 255], ...
+    'Thickness',2, 'LineType','AA');
+out = cv.circle(out, center, 3, 'Color',[0 255 0], ...
+    'Thickness','Filled', 'LineType','AA');
+figure, imshow(out), title('detected circles')
