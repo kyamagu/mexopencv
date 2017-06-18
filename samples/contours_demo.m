6ee92a7e487d32e828f25648a659fe32e2de9b4a
Amro <amroamroamro@gmail.com>
2017-06-18 20:04:10 +0300

    samples: update contours/connected-components demo
    shows both findContours and connectedComponents functions

--- samples/contours_demo.m
+++ samples/contours_demo.m
@@ -3,19 +3,19 @@
 % image and label them.
 %
 
-%% Input Image
-% some binary (black-n-white) image, i.e class(src) = logical
-src = imread(fullfile(mexopencv.root(),'test','bw.png'));
-imshow(src), title('Source')
+%% Image
+% load binary image (black-and-white, logical)
+bw = imread(fullfile(mexopencv.root(),'test','bw.png'));
+imshow(bw), title('Source')
 
 %% Connected Components
-[contours, hierarchy] = cv.findContours(src, ...
+[contours, hierarchy] = cv.findContours(bw, ...
     'Mode','CComp', 'Method','Simple');
 
 %%
 % iterate through all the top-level contours,
 % draw each connected component with its own random color
-dst = zeros([size(src) 3], 'uint8');
+dst = zeros([size(bw) 3], 'uint8');
 idx = 0;
 while idx >= 0
     clr = randi([0 255], [1 3], 'uint8');
@@ -28,3 +28,33 @@ end
 %%
 % show result
 imshow(dst), title('Components')
+
+%% Connected Components
+% a different way to perform connected components labeling
+[labels, num, stats, centroids] = cv.connectedComponents(bw);
+
+%%
+% convert labels to one-based floating-point indices, background being label 1
+labels = double(labels) + 1;
+if mexopencv.require('stats')
+	tabulate(labels(:))
+end
+
+%%
+% color-code labels
+clr = [0 0 0; jet(num-1)];  % black for the background
+out = uint8(255 * ind2rgb(labels, clr));
+
+%%
+% draw bounding box, centroid, and area of each component
+opts = {'Color',[0 255 0]};
+for i=2:num   % skip first background label
+    out = cv.rectangle(out, stats(i,1:4), opts{:});
+    out = cv.circle(out, centroids(i,:), 2, 'Thickness','Filled', opts{:});
+    out = cv.putText(out, sprintf('%d',stats(i,5)), stats(i,1:2), ...
+        'FontFace','HersheyPlain', 'FontScale',0.8, opts{:});
+end
+
+%%
+% show result
+figure, imshow(out), title('Connected Components')
