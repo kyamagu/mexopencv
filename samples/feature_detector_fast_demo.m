488284f60467ce671ee5ba2b5bd232d5baa427b6
Amro <amroamroamro@gmail.com>
2017-06-25 19:36:51 +0300

    samples: three new demos for FAST, BRIEF, and ORB

---
+++ samples/feature_detector_fast_demo.m
@@ -0,0 +1,143 @@
+%% FAST Algorithm for Corner Detection
+%
+% In this demo, we will:
+%
+% * understand the basics of FAST algorithm
+% * find corners using OpenCV functionalities for FAST algorithm.
+%
+% <http://docs.opencv.org/3.2.0/df/d0c/tutorial_py_fast.html>
+%
+
+%% Theory
+%
+% We saw several feature detectors and many of them are really good. But when
+% looking from a real-time application point of view, they are not fast
+% enough. One best example would be SLAM (Simultaneous Localization and
+% Mapping) mobile robot which have limited computational resources.
+%
+% As a solution to this, FAST (Features from Accelerated Segment Test)
+% algorithm was proposed by Edward Rosten and Tom Drummond in their paper
+% "Machine learning for high-speed corner detection" in 2006 (later revised it
+% in 2010). A basic summary of the algorithm is presented below. Refer to the
+% original paper for more details (All the images are taken from original
+% paper).
+%
+%% Feature Detection using FAST
+%
+% * Select a pixel $p$ in the image which is to be identified as an interest
+%   point or not. Let its intensity be $I_p$.
+% * Select appropriate threshold value $t$.
+% * Consider a circle of 16 pixels around the pixel under test (See the image
+%   below)
+%
+% <<http://docs.opencv.org/3.2.0/fast_speedtest.jpg>>
+%
+% * Now the pixel $p$ is a corner if there exists a set of $n$ contiguous
+%   pixels in the circle (of 16 pixels) which are all brighter than $I_p + t$,
+%   or all darker than $I_p - t$ (Shown as white dash lines in the above
+%   image). $n$ was chosen to be 12.
+% * A *high-speed test* was proposed to exclude a large number of non-corners.
+%   This test examines only the four pixels at 1, 9, 5 and 13 (First 1 and 9
+%   are tested if they are too brighter or darker. If so, then checks 5 and
+%   13). If $p$ is a corner, then at least three of these must all be brighter
+%   than $I_p + t$ or darker than $I_p - t$. If neither of these is the case,
+%   then $p$ cannot be a corner. The full segment test criterion can then be
+%   applied to the passed candidates by examining all pixels in the circle.
+%
+% This detector in itself exhibits high performance, but there are several
+% weaknesses:
+%
+% * It does not reject as many candidates for |n < 12|.
+% * The choice of pixels is not optimal because its efficiency depends on
+%   ordering of the questions and distribution of corner appearances.
+% * Results of high-speed tests are thrown away.
+% * Multiple features are detected adjacent to one another.
+%
+% First 3 points are addressed with a machine learning approach. Last one is
+% addressed using non-maximal suppression.
+%
+%% Machine Learning a Corner Detector
+%
+% * Select a set of images for training (preferably from the target
+%   application domain)
+% * Run FAST algorithm in every images to find feature points.
+% * For every feature point, store the 16 pixels around it as a vector. Do it
+%   for all the images to get feature vector $P$.
+% * Each pixel (say $x$) in these 16 pixels can have one of the following
+%   three states:
+%
+% <<http://docs.opencv.org/3.2.0/fast_eqns.jpg>>
+%
+% * Depending on these states, the feature vector $P$ is subdivided into 3
+%   subsets, $P_d$, $P_s$, $P_b$.
+% * Define a new boolean variable, $K_p$, which is true if $p$ is a corner and
+%   false otherwise.
+% * Use the ID3 algorithm (decision tree classifier) to query each subset
+%   using the variable $K_p$ for the knowledge about the true class. It
+%   selects the $x$ which yields the most information about whether the
+%   candidate pixel is a corner, measured by the entropy of $K_p$.
+% * This is recursively applied to all the subsets until its entropy is zero.
+% * The decision tree so created is used for fast detection in other images.
+%
+%% Non-maximal Suppression
+%
+% Detecting multiple interest points in adjacent locations is another problem.
+% It is solved by using Non-maximum Suppression.
+%
+% * Compute a score function, $V$ for all the detected feature points. $V$ is
+%   the sum of absolute difference between $p$ and 16 surrounding pixels
+%   values.
+% * Consider two adjacent keypoints and compute their $V$ values.
+% * Discard the one with lower $V$ value.
+%
+%% Summary
+%
+% It is several times faster than other existing corner detectors.
+%
+% But it is not robust to high levels of noise. It is dependant on a
+% threshold.
+%
+
+%% FAST Feature Detector in OpenCV
+%
+% It is called as any other feature detector in OpenCV. If you want, you can
+% specify the threshold, whether non-maximum suppression to be applied or not,
+% the neighborhood to be used etc.
+%
+% For the neighborhood, three flags are defined, |TYPE_5_8|, |TYPE_7_12|, and
+% |TYPE_9_16|. Below is a simple code on how to detect and draw the FAST
+% feature points.
+%
+
+%%
+% load source image
+img = cv.imread(fullfile(mexopencv.root(),'test','blox.jpg'), 'Grayscale',true);
+
+%%
+% create FAST object with default values, and find keypoints
+fast = cv.FastFeatureDetector();
+kp1 = fast.detect(img);
+fprintf('%d keypoints with NonmaxSuppression\n', numel(kp1));
+
+%%
+% disable |NonmaxSuppression| and find keypoints
+fast.NonmaxSuppression = false;
+kp2 = fast.detect(img);
+fprintf('%d keypoints without NonmaxSuppression\n', numel(kp2));
+
+%%
+% draw keypoints from both cases, and show results
+out1 = cv.drawKeypoints(img, kp1, 'Color',[255 0 0]);
+out2 = cv.drawKeypoints(img, kp2, 'Color',[255 0 0]);
+subplot(121), imshow(out1), title('FAST w/ NonmaxSuppression')
+subplot(122), imshow(out2), title('FAST w/o NonmaxSuppression')
+
+%% Additional Resources
+%
+% * Edward Rosten and Tom Drummond, "Machine learning for high speed corner
+%   detection" in 9th European Conference on Computer Vision, vol. 1, 2006,
+%   pp. 430-443.
+% * Edward Rosten, Reid Porter, and Tom Drummond, "Faster and better: a
+%   machine learning approach to corner detection" in IEEE Trans. Pattern
+%   Analysis and Machine Intelligence, 2010, vol 32, pp. 105-119.
+%
---
+++ samples/orb_demo.m
@@ -0,0 +1,99 @@
+%% ORB (Oriented FAST and Rotated BRIEF)
+%
+% In this demo, we will see the basics of ORB.
+%
+% <http://docs.opencv.org/3.2.0/d1/d89/tutorial_py_orb.html>
+%
+
+%% Theory
+%
+% As an OpenCV enthusiast, the most important thing about the ORB is that it
+% came from "OpenCV Labs". This algorithm was brought up by Ethan Rublee,
+% Vincent Rabaud, Kurt Konolige and Gary R. Bradski in their paper
+% *ORB: An efficient alternative to SIFT or SURF* in 2011. As the title says,
+% it is a good alternative to SIFT and SURF in computation cost, matching
+% performance and mainly the patents. Yes, SIFT and SURF are patented and you
+% are supposed to pay them for its use. But ORB is not!
+%
+% ORB is basically a fusion of FAST keypoint detector and BRIEF descriptor
+% with many modifications to enhance the performance. First it use FAST to
+% find keypoints, then apply Harris corner measure to find top N points among
+% them. It also use pyramid to produce multiscale-features. But one problem
+% is that, FAST doesn't compute the orientation. So what about rotation
+% invariance? Authors came up with following modification.
+%
+% It computes the intensity weighted centroid of the patch with located corner
+% at center. The direction of the vector from this corner point to centroid
+% gives the orientation. To improve the rotation invariance, moments are
+% computed with x and y which should be in a circular region of radius $r$,
+% where $r$ is the size of the patch.
+%
+% Now for descriptors, ORB use BRIEF descriptors. But we have already seen
+% that BRIEF performs poorly with rotation. So what ORB does is to "steer"
+% BRIEF according to the orientation of keypoints. For any feature set of
+% $n$ binary tests at location $(x_i, y_i)$, define a $2 \times n$ matrix,
+% $S$ which contains the coordinates of these pixels. Then using the
+% orientation of patch, $\theta$, its rotation matrix is found and rotates
+% the $S$ to get steered (rotated) version $S_\theta$.
+%
+% ORB discretize the angle to increments of $2 \pi /30$ (12 degrees), and
+% construct a lookup table of precomputed BRIEF patterns. As long as the
+% keypoint orientation $\theta$ is consistent across views, the correct set of
+% points $S_\theta$ will be used to compute its descriptor.
+%
+% BRIEF has an important property that each bit feature has a large variance
+% and a mean near 0.5. But once it is oriented along keypoint direction, it
+% loses this property and become more distributed. High variance makes a
+% feature more discriminative, since it responds differentially to inputs.
+% Another desirable property is to have the tests uncorrelated, since then
+% each test will contribute to the result. To resolve all these, ORB runs a
+% greedy search among all possible binary tests to find the ones that have
+% both high variance and means close to 0.5, as well as being uncorrelated.
+% The result is called *rBRIEF*.
+%
+% For descriptor matching, multi-probe LSH which improves on the traditional
+% LSH, is used. The paper says ORB is much faster than SURF and SIFT and ORB
+% descriptor works better than SURF. ORB is a good choice in low-power devices
+% for panorama stitching etc.
+%
+
+%% ORB in OpenCV
+%
+% As usual, we have to create an ORB object with the function |cv.ORB|, or
+% using feature2d common interface. It has a number of optional parameters.
+% Most useful ones are |MaxFeatures| which denotes maximum number of features
+% to be retained (by default 500), |ScoreType| which denotes whether Harris
+% score or FAST score to rank the features (by default, Harris score) etc.
+% Another parameter, |WTA_K| decides number of points that produce each
+% element of the oriented BRIEF descriptor. By default it is two, i.e selects
+% two points at a time. In that case, for matching, |Hamming| distance is
+% used. If |WTA_K| is 3 or 4, which takes 3 or 4 points to produce BRIEF
+% descriptor, then matching distance is defined by |Hamming2|.
+%
+
+%%
+% load source image
+img = cv.imread(fullfile(mexopencv.root(),'test','butterfly.jpg'), ...
+    'Grayscale',true);
+
+%%
+% detect keypoints
+orb = cv.ORB();
+keypoints = orb.detect(img);
+whos keypoints
+
+%%
+% compute the descriptors
+descriptors = orb.compute(img, keypoints);
+whos descriptors
+
+%%
+% draw keypoints (only location, not size and orientation)
+out = cv.drawKeypoints(img, keypoints, 'Color',[255 0 0]);
+imshow(out), title('ORB')
+
+%% Additional Resources
+%
+% * Ethan Rublee, Vincent Rabaud, Kurt Konolige, Gary R. Bradski.
+%   "ORB: An efficient alternative to SIFT or SURF". ICCV 2011: 2564-2571.
+%
