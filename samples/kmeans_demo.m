9ad57b3f91d603da629a4dcaff1af4707c3ff0ac
Amro <amroamroamro@gmail.com>
2017-07-01 18:43:04 +0300

    samples: update Kmeans demo
    - expand docs
    - display compactness measure
    - plot cluster centers

--- samples/kmeans_demo.m
+++ samples/kmeans_demo.m
@@ -1,4 +1,4 @@
-%% K-means demo
+%% K-Means Clustering
 % An example on K-means clustering.
 %
 % This program demonstrates kmeans clustering.
@@ -6,7 +6,86 @@
 % cluster centers and uses kmeans to move those cluster centers to their
 % representitive location.
 %
-% <https://github.com/opencv/opencv/blob/3.1.0/samples/cpp/kmeans.cpp>
+% <https://github.com/opencv/opencv/blob/3.2.0/samples/cpp/kmeans.cpp>,
+% <http://docs.opencv.org/3.2.0/de/d4d/tutorial_py_kmeans_understanding.html>
+%
+
+%% Theory
+%
+% We will explain with an example which is commonly used, the T-shirt size
+% problem.
+%
+% Consider a company, which is going to release a new model of T-shirt to
+% market. Obviously they will have to manufacture models in different sizes to
+% satisfy people of all sizes. So the company make a data of people's height
+% and weight, and plot them on to a graph, as below:
+%
+% <<http://docs.opencv.org/3.2.0/tshirt.jpg>>
+%
+% Company can't create t-shirts with all the sizes. Instead, they divide
+% people to Small, Medium and Large, and manufacture only these 3 models which
+% will fit into all the people. This grouping of people into three groups can
+% be done by k-means clustering, and algorithm provides us best 3 sizes, which
+% will satisfy all the people. And if it doesn't, company can divide people to
+% more groups, may be five, and so on. Check image below :
+%
+% <<http://docs.opencv.org/3.2.0/tshirt_grouped.jpg>>
+%
+% This algorithm is an iterative process. We will explain it step-by-step with
+% the help of images.
+%
+% Consider a set of data as below (you can consider it as t-shirt problem).
+% We need to cluster this data into two groups.
+%
+% <<http://docs.opencv.org/3.2.0/testdata.jpg>>
+%
+% *Step 1:* Algorithm randomly chooses two centroids, $C1$ and $C2$
+% (sometimes, any two data are taken as the centroids).
+%
+% *Step 2:* It calculates the distance from each point to both centroids. If a
+% test data is more closer to $C1$, then that data is labelled with |0|. If it
+% is closer to $C2$, then labelled as |1| (If more centroids are there,
+% labelled as |2|,|3| etc).
+%
+% In our case, we will color all |0| labelled with red, and |1| labelled with
+% blue. So we get following image after above operations.
+%
+% <<http://docs.opencv.org/3.2.0/initial_labelling.jpg>>
+%
+% *Step 3:* Next we calculate the average of all blue points and red points
+% separately and that will be our new centroids. That is $C1$ and $C2$ shift
+% to newly calculated centroids. (Remember, the images shown are not true
+% values and not to true scale, it is just for demonstration only).
+%
+% And again, perform step 2 with new centroids and label data to |0| and |1|.
+% So we get result as below :
+%
+% <<http://docs.opencv.org/3.2.0/update_centroid.jpg>>
+%
+% Now step 2 and 3 are iterated until both centroids are converged to fixed
+% points. (Or it may be stopped depending on the criteria we provide, like
+% maximum number of iterations, or a specific accuracy is reached etc.).
+% These points are such that sum of distances between test data and their
+% corresponding centroids are minimum. Or simply, sum of distances between
+% $C1 \leftrightarrow Red\_Points$ and $C2 \leftrightarrow Blue\_Points$ is
+% minimum.
+%
+% $$J = \sum_{All\: Red\_Points} distance(C1,Red\_Point) +
+%       \sum_{All\: Blue\_Points} distance(C2,Blue\_Point)$$
+%
+% Final result almost looks like below :
+%
+% <<http://docs.opencv.org/3.2.0/final_clusters.jpg>>
+%
+% So this is just an intuitive understanding of K-Means Clustering. For more
+% details and mathematical explanation, please read any standard machine
+% learning textbooks or check the links below. It is just a top layer of
+% K-Means clustering. There are a lot of modifications to this algorithm like,
+% how to choose the initial centroids, how to speed up the iteration process,
+% etc.
+%
+% * <https://www.coursera.org/course/ml Machine Learning Course>, Video
+%   lectures by Prof. Andrew Ng (some of the images are taken from it)
 %
 
 %% Initialization
@@ -63,10 +142,15 @@ points = points(randperm(sampleCount),:);
 
 %%
 % cluster points
-[labels,centers] = cv.kmeans(points, clusterCount, ...
+[labels,centers,compactness] = cv.kmeans(points, clusterCount, ...
     'Criteria',struct('type','Count+EPS', 'maxCount',10, 'epsilon',0.1), ...
     'Attempts',3, 'Initialization','PP');
 display(centers)
+
+%%
+% compactness measure of clusters
+%compactness = cv.norm(points, centers(labels+1,:), 'NormType','L2Sqr');
+fprintf('Compactness = %f\n', compactness);
 
 %%
 % show clusters
@@ -76,5 +160,9 @@ for i=1:sampleCount
     img = cv.circle(img, points(i,:), 2, ...
         'Color',colorTab(clusterIdx,:), ...
         'Thickness','Filled', 'LineType','AA');
+end
+for i=1:clusterCount
+    img = cv.circle(img, centers(i,:), 40, 'Color',colorTab(i,:), ...
+        'Thickness',1, 'LineType','AA');
 end
 figure, imshow(img), title('clusters')
