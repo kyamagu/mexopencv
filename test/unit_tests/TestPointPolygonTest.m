a422446f10c61796e0ed2a3d5ceec83e0aeb39be
Amro <amroamroamro@gmail.com>
2017-07-16 19:44:06 +0300

    integer points test case for pointPolygonTest

--- test/unit_tests/TestPointPolygonTest.m
+++ test/unit_tests/TestPointPolygonTest.m
@@ -2,25 +2,29 @@ classdef TestPointPolygonTest
     %TestPointPolygonTest
 
     methods (Static)
-        function test_1
-            contour = [0 0; 1 0; 2 2; 3 3; 3 4];
-            b = cv.pointPolygonTest(contour, [2.3 2.4]);
+        function test_contour_matrix
+            cntr = [0 0; 1 0; 2 2; 3 3; 3 4];
+            b = cv.pointPolygonTest(cntr, [2.3 2.4]);
+            validateattributes(b, {'double'}, {'scalar'});
+
+            % purely-integer points (both contour and query)
+            b = cv.pointPolygonTest(int32(cntr), [2 2]);
             validateattributes(b, {'double'}, {'scalar'});
         end
 
-        function test_2
-            contour = {[0 0], [1 0], [2 2], [3 3], [3 4]};
-            b = cv.pointPolygonTest(contour, [2.3 2.4]);
+        function test_contour_cellarray
+            cntr = {[0 0], [1 0], [2 2], [3 3], [3 4]};
+            b = cv.pointPolygonTest(cntr, [2.3 2.4]);
             validateattributes(b, {'double'}, {'scalar'});
         end
 
-        function test_3
-            contour = [0 0; 1 0; 2 2; 3 3; 3 4];
+        function test_measure_dist
+            cntr = [0 0; 1 0; 2 2; 3 3; 3 4];
 
-            b = cv.pointPolygonTest(contour, [2.3,2.4], 'MeasureDist',true);
+            b = cv.pointPolygonTest(cntr, [2.3,2.4], 'MeasureDist',true);
             validateattributes(b, {'double'}, {'scalar', 'real'});
 
-            b = cv.pointPolygonTest(contour, [2.3,2.4], 'MeasureDist',false);
+            b = cv.pointPolygonTest(cntr, [2.3,2.4], 'MeasureDist',false);
             validateattributes(b, {'double'}, {'scalar', 'integer'});
             assert(b == 0 || b == 1 || b == -1);
         end
