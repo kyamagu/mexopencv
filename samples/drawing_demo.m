2a5451767a558d23a91b3039c5b853f27c8b1d02
Amro <amroamroamro@gmail.com>
2017-07-23 19:33:13 +0300

    samples: update drawing demo
    showcase more drawing functions

--- samples/drawing_demo.m
+++ samples/drawing_demo.m
@@ -18,31 +18,43 @@ img = zeros(H,W,3,'uint8');
 xlims = fix([-W/2 W*3/2]);
 ylims = fix([-H/2 H*3/2]);
 clr = @() randi([0 255], [1 3], 'uint8');
+markers = '+x*ds^v';
+marker = @() markers(randi([1 numel(markers)]));
 
 % prepare plot
 hImg = imshow(img);
 
 %% Lines
-for i=1:NUMBER
+for i=1:NUMBER*2
     pt1 = [randi(xlims) randi(ylims)];
     pt2 = [randi(xlims) randi(ylims)];
     thick = randi([1 10]);
 
-    img = cv.line(img, pt1, pt2, ...
-        'Color',clr(), 'Thickness',thick, lineType{:});
+    if rand < 0.5
+        img = cv.line(img, pt1, pt2, ...
+            'Color',clr(), 'Thickness',thick, lineType{:});
+    else
+        img = cv.arrowedLine(img, pt1, pt2, ...
+            'Color',clr(), 'Thickness',thick, lineType{:});
+    end
 
     set(hImg, 'CData',img);
     pause(DELAY);
 end
 
 %% Rectangles
-for i=1:NUMBER
+for i=1:NUMBER*2
     pt1 = [randi(xlims) randi(ylims)];
     pt2 = [randi(xlims) randi(ylims)];
     thick = max(randi([-3 10]), -1);
 
-    img = cv.rectangle(img, pt1, pt2, ...
-        'Color',clr(), 'Thickness',thick, lineType{:});
+    if rand < 0.5
+        img = cv.rectangle(img, pt1, pt2, ...
+            'Color',clr(), 'Thickness',thick, lineType{:});
+    else
+        img = cv.drawMarker(img, pt1, ...
+            'Color',clr(), 'MarkerType',marker(), 'MarkerSize',randi([30 80]));
+    end
 
     set(hImg, 'CData',img);
     pause(DELAY);
