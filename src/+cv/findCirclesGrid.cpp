7e259efcd98bffc613d27ac3fbde4d7538f55848
Amro <amroamroamro@gmail.com>
2017-08-11 19:34:39 +0300

    calib3d: new variant of findCirclesGrid
    new option to pass CirclesGridFinderParameters

--- src/+cv/findCirclesGrid.cpp
+++ src/+cv/findCirclesGrid.cpp
@@ -10,6 +10,93 @@
 using namespace std;
 using namespace cv;
 
+namespace {
+/// grid types for option processing
+const ConstMap<string,cv::CirclesGridFinderParameters::GridType> GridTypesMap =
+    ConstMap<string,cv::CirclesGridFinderParameters::GridType>
+    ("Symmetric",  cv::CirclesGridFinderParameters::SYMMETRIC_GRID)
+    ("Asymmetric", cv::CirclesGridFinderParameters::ASYMMETRIC_GRID);
+
+/** Convert MxArray to cv::CirclesGridFinderParameters
+ * @param arr MxArray object. In one of the following forms:
+ * - a scalar struct
+ * - a cell-array of the form: <tt>{GridType, ...}</tt> starting with the grid
+ *   type ("Symmetric" or "Asymmetric") followed by pairs of key-value options
+ * @return instance of CirclesGridFinderParameters object
+ */
+CirclesGridFinderParameters MxArrayToFinderParameters(const MxArray &arr)
+{
+    CirclesGridFinderParameters params;
+    if (arr.isStruct()) {
+        params.gridType = GridTypesMap[arr.at("gridType").toString()];
+        if (arr.isField("densityNeighborhoodSize"))
+            params.densityNeighborhoodSize = arr.at("densityNeighborhoodSize").toSize_<float>();
+        if (arr.isField("minDensity"))
+            params.minDensity = arr.at("minDensity").toFloat();
+        if (arr.isField("kmeansAttempts"))
+            params.kmeansAttempts = arr.at("kmeansAttempts").toInt();
+        if (arr.isField("minDistanceToAddKeypoint"))
+            params.minDistanceToAddKeypoint = arr.at("minDistanceToAddKeypoint").toInt();
+        if (arr.isField("keypointScale"))
+            params.keypointScale = arr.at("keypointScale").toInt();
+        if (arr.isField("minGraphConfidence"))
+            params.minGraphConfidence = arr.at("minGraphConfidence").toFloat();
+        if (arr.isField("vertexGain"))
+            params.vertexGain = arr.at("vertexGain").toFloat();
+        if (arr.isField("vertexPenalty"))
+            params.vertexPenalty = arr.at("vertexPenalty").toFloat();
+        if (arr.isField("existingVertexGain"))
+            params.existingVertexGain = arr.at("existingVertexGain").toFloat();
+        if (arr.isField("edgeGain"))
+            params.edgeGain = arr.at("edgeGain").toFloat();
+        if (arr.isField("edgePenalty"))
+            params.edgePenalty = arr.at("edgePenalty").toFloat();
+        if (arr.isField("convexHullFactor"))
+            params.convexHullFactor = arr.at("convexHullFactor").toFloat();
+        if (arr.isField("minRNGEdgeSwitchDist"))
+            params.minRNGEdgeSwitchDist = arr.at("minRNGEdgeSwitchDist").toFloat();
+    }
+    else {
+        vector<MxArray> args(arr.toVector<MxArray>());
+        nargchk(args.size() >= 1 && (args.size()%2)==1);
+        params.gridType = GridTypesMap[args[0].toString()];
+        for (size_t i = 1; i < args.size(); i+=2) {
+            string key(args[i].toString());
+            if (key == "DensityNeighborhoodSize")
+                params.densityNeighborhoodSize = args[i+1].toSize_<float>();
+            else if (key == "MinDensity")
+                params.minDensity = args[i+1].toFloat();
+            else if (key == "KmeansAttempts")
+                params.kmeansAttempts = args[i+1].toInt();
+            else if (key == "MinDistanceToAddKeypoint")
+                params.minDistanceToAddKeypoint = args[i+1].toInt();
+            else if (key == "KeypointScale")
+                params.keypointScale = args[i+1].toInt();
+            else if (key == "MinGraphConfidence")
+                params.minGraphConfidence = args[i+1].toFloat();
+            else if (key == "VertexGain")
+                params.vertexGain = args[i+1].toFloat();
+            else if (key == "VertexPenalty")
+                params.vertexPenalty = args[i+1].toFloat();
+            else if (key == "ExistingVertexGain")
+                params.existingVertexGain = args[i+1].toFloat();
+            else if (key == "EdgeGain")
+                params.edgeGain = args[i+1].toFloat();
+            else if (key == "EdgePenalty")
+                params.edgePenalty = args[i+1].toFloat();
+            else if (key == "ConvexHullFactor")
+                params.convexHullFactor = args[i+1].toFloat();
+            else if (key == "MinRNGEdgeSwitchDist")
+                params.minRNGEdgeSwitchDist = args[i+1].toFloat();
+            else
+                mexErrMsgIdAndTxt("mexopencv:error",
+                    "Unrecognized CirclesGridFinderParameters option %s", key.c_str());
+        }
+    }
+    return params;
+}
+}
+
 /**
  * Main entry called from Matlab
  * @param nlhs number of left-hand-side arguments
@@ -29,6 +116,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     bool symmetricGrid = true;
     bool clustering = false;
     Ptr<FeatureDetector> blobDetector;
+    CirclesGridFinderParameters params;
     for (int i=2; i<nrhs; i+=2) {
         string key(rhs[i].toString());
         if (key == "SymmetricGrid")
@@ -48,6 +136,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
                 mexErrMsgIdAndTxt("mexopencv:error",
                     "Invalid detector arguments");
         }
+        else if (key == "FinderParameters")
+            params = MxArrayToFinderParameters(rhs[i+1]);
         else
             mexErrMsgIdAndTxt("mexopencv:error",
                 "Unrecognized option %s", key.c_str());
@@ -63,7 +153,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     Size patternSize(rhs[1].toSize());
     vector<Point2f> centers;
     bool patternFound = findCirclesGrid(image, patternSize, centers, flags,
-        blobDetector);
+        blobDetector, params);
     plhs[0] = MxArray(centers);
     if (nlhs > 1)
         plhs[1] = MxArray(patternFound);
