a33753bcd3e85a8190f1bfa85c8002aef92bac07
Amro <amroamroamro@gmail.com>
2017-11-24 19:16:09 +0200

    samples: new synth video framework
    can be used in samples as a wrapper or stub for VideoCapture,
    with fallback to creating synthetic videos
    
    Useful for demos that work on a video stream to demonstrate stuff like
    face detection, motion, camera calibration, aruco boards, etc.

---
+++ samples/common/VideoSynthAruco.m
@@ -0,0 +1,217 @@
+classdef VideoSynthAruco < VideoSynthBase
+    %VIDEOSYNTHARUCO  Aruco synthetic video class
+    %
+    % ### Sources:
+    %
+    % * https://github.com/opencv/opencv/blob/3.3.0/samples/python/video.py
+    %
+    % See also: VideoSynthBase, createVideoCapture
+    %
+
+    properties (SetAccess = protected)
+        time        % current time
+        time_step   % time step
+        cam_mat     % camera matrix (intrinsic parameters)
+        dist_coeff  % lens distortion coefficients
+        fg          % foreground image (aruco imag)
+    end
+
+    methods
+        function this = VideoSynthAruco(varargin)
+            %VIDEOSYNTHARUCO  Constructor
+            %
+            %     cap = VideoSynthAruco('OptionName', optionValue, ...)
+            %
+            % ## Options
+            % Accepts the same options as the open method.
+            %
+            % See also: VideoSynthAruco.open
+            %
+
+            % explicitly call superclass constructor with specified arguments
+            this = this@VideoSynthBase(varargin{:});
+        end
+
+        function release(this)
+            %RELEASE  Clears allocated data
+            %
+            %     cap.release()
+            %
+
+            % call superclass RELEASE method
+            release@VideoSynthBase(this);
+
+            this.time = 0;
+            this.time_step = 1;
+            this.cam_mat = eye(3,3);
+            this.dist_coeff = zeros(1,4);
+            this.fg = [];
+        end
+
+        function [retval, p] = open(this, varargin)
+            %OPEN  Opens a synthetic video stream
+            %
+            %     retval = cap.open('OptionName',optionValue, ...)
+            %
+            % ## Output
+            % * __retval__ bool, true if successful
+            %
+            % ## Options
+            % Same as base class options, in addition to:
+            % * __TimeStep__ time step, default 1/30
+            % * __DistCoeffs__ radial distortion coefficients,
+            %   default [-0.2 0.1 0 0]
+            % * __FG__ aruco board image, or one of the following presets:
+            %   * __ArUcoMarker__ see cv.drawMarkerAruco
+            %   * __ChArUcoDiamond__ see cv.drawCharucoDiamond
+            %   * __ArUcoBoard__ see cv.drawPlanarBoard
+            %   * __ChArUcoBoard__ see cv.drawCharucoBoard
+            %
+            % See also: VideoSynthBase.open
+            %
+
+            % call superclass OPEN method
+            [retval, p] = open@VideoSynthBase(this, varargin{:});
+            if ~retval, return; end
+
+            % time variable
+            this.time = 0;
+            this.time_step = p.Results.TimeStep;
+
+            % intrinsic camera parameters
+            fx = 0.9;   % focal lengths expressed in pixel-related units
+            this.cam_mat = [
+                fx*this.w     0     (this.w-1)/2 ;
+                    0     fx*this.h (this.h-1)/2 ;
+                    0         0           1
+            ];
+            this.dist_coeff = p.Results.DistCoeffs;
+
+            % aruco image
+            if ischar(p.Results.FG)
+                % one of presets
+                this.fg = create_aruco_image(p.Results.FG);
+            else
+                % any image really
+                this.fg = p.Results.FG;
+            end
+        end
+    end
+
+    methods (Access = protected)
+        function p = optionsParser(this)
+            %OPTIONSPARSER  Input arguments parser
+            %
+            %     p = cap.optionsParser()
+            %
+            % See also: VideoSynthBase.optionsParser
+            %
+
+            p = optionsParser@VideoSynthBase(this);
+            p.addParameter('TimeStep', 1/30);
+            p.addParameter('DistCoeffs', [-0.2 0.1 0 0]);
+            p.addParameter('FG', 'ArucoBoard');
+        end
+
+        function img = render(this, img)
+            %RENDER  Renders additional layers as needed on top of background
+            %
+            %     img = cap.render(img)
+            %
+            % See also: VideoSynthBase.render
+            %
+
+            fsz = [size(this.fg,2) size(this.fg,1)];
+
+            % get current pose (rotation/translation vectors)
+            [rvec, tvec] = getSyntheticRT(this.time, fsz);
+            this.time = this.time + this.time_step;
+
+            % aruco image corners
+            p3 = [0 0; fsz(1) 0; fsz(1) fsz(2); 0 fsz(2)];
+            p3(:,3) = 0;  % Z=0
+
+            % project corners from 3D to 2D
+            p2 = cv.projectPoints(p3, rvec, tvec, ...
+                this.cam_mat, 'DistCoeffs',this.dist_coeff);
+
+            % compute perspective transform (between 4 matching points)
+            if true
+                T = cv.getPerspectiveTransform(p3(:,1:2), p2);
+            else
+                T = cv.findHomography(p3(:,1:2), p2);
+            end
+
+            % apply transform to aruco image projected on top of background image
+            img = cv.warpPerspective(flip(this.fg, 2), T, ...
+                'Dst',img, 'DSize',this.sz, 'BorderType','Transparent');
+        end
+    end
+end
+
+%% Helper functions
+
+function img = create_aruco_image(config)
+    % draw aruco/charuco marker or board from one of predefined configurations
+    config = validatestring(config, ...
+        {'ArUcoMarker', 'ChArUcoDiamond', 'ArUcoBoard', 'ChArUcoBoard'});
+    dict = {'Predefined', '6x6_250'};
+    switch config
+        case 'ArUcoMarker'
+            id = randi(250) - 1;
+            img = cv.drawMarkerAruco(dict, id, 200);
+            img = cv.copyMakeBorder(img, [25 25 25 25], ...
+                'BorderType','Constant', 'Value',255);
+        case 'ChArUcoDiamond'
+            ids = randi(250, [1 4]) - 1;
+            img = cv.drawCharucoDiamond(dict, ids, 120, 60, 'MarginSize',30);
+        case 'ArUcoBoard'
+            board = {'GridBoard', 5, 7, 60, 15, dict};
+            sz = [board{2:3}] * sum([board{4:5}]) - board{5} + 2 * board{5};
+            img = cv.drawPlanarBoard(board, sz, 'MarginSize',board{5});
+        case 'ChArUcoBoard'
+            board = {5, 7, 60, 30, dict};
+            margins = board{3} - board{4};
+            sz = [board{1:2}] * board{3} + 2 * margins;
+            img = cv.drawCharucoBoard(board, sz, 'MarginSize',margins);
+    end
+    img = cv.cvtColor(img, 'GRAY2RGB');
+end
+
+function [rvec, tvec] = getSyntheticRT(t, sz)
+    % compute eye/target positions for transformed image
+    center = [0.5*sz 0];
+    phi = pi/3 + sin(t*3)*pi/8;
+    ofs = [sin(1.2*t) cos(1.8*t) 0] * sz(1) * 0.2;
+    eye_pos = center + [cos(t)*cos(phi) sin(t)*cos(phi) sin(phi)] * 15*60 + ofs;
+    target_pos = center + ofs;
+
+    % compute rotation matrix, translation vector (extrinsic parameters)
+    [R, tvec] = lookat(eye_pos(:), target_pos(:));
+    rvec = mtx2rvec(R);
+end
+
+function [R, tvec] = lookat(eyePos, targetPos, up)
+    if nargin < 3, up = [0; 0; 1]; end
+    fwd = (targetPos - eyePos);
+    fwd = fwd / norm(fwd);
+    right = cross(fwd, up);
+    right = right / norm(right);
+    down = cross(fwd, right);
+    R = [right, down, fwd]';
+    tvec = -R * eyePos(:);
+end
+
+function rvec = mtx2rvec(R)
+    % rotation vector (compact representation of a rotation matrix)
+    if true
+        rvec = cv.Rodrigues(R);
+    else
+        [U,S,V] = svd(R - eye(3));
+        p = V(:,1) + U(:,1)*S(1,1);
+        c = dot(V(:,1), p);
+        s = dot(V(:,2), p);
+        ax = cross(V(:,1), V(:,2));
+        rvec = ax * atan2(s, c);
+    end
+end
---
+++ samples/common/createVideoCapture.m
@@ -0,0 +1,197 @@
+function cap = createVideoCapture(source, fallback)
+    %CREATEVIDEOCAPTURE  Create video capture object (real class or synthetic)
+    %
+    %     cap = createVideoCapture(devid)
+    %     cap = createVideoCapture(filename)
+    %     cap = createVideoCapture(synth)
+    %     cap = createVideoCapture(..., fallback)
+    %
+    % ## Input
+    % * __devid__ integer, id of the video capturing device to open.
+    %   Passed to cv.VideoCapture class.
+    % * __filename__ string, name of video file. Passed to cv.VideoCapture
+    % * __synth__ procedural synthetic video. A string of the form
+    %   'synth|key=value|key=value'. Supported parameters:
+    %   * __class__ one of:
+    %     * __base__ noisy BG image (default if no class specified)
+    %     * __scene__ noisy BG image with a rotating FG image in front
+    %     * __chess__ noisy BG image with a moving chess board in front
+    %     * __aruco__ noisy BG image with a moving aruco board in front
+    %   * __framecount__ maximum number of frames of video stream
+    %   * __bg__ path to background image file name
+    %   * __size__ string in the form WxH specifying width/height of frames
+    %   * __noise__ amount of noise to add (number in the range [0,1])
+    %   * __timestep__, __speed__, __deformation__, __fg__ see VideoSynthScene.open
+    %   * __timestep__, __distcoeffs__, __gridsize__ see VideoSynthChess.open
+    %   * __timestep__, __distcoeffs__, __fg__ see VideoSynthAruco.open
+    % * __fallback__ fallback in case devid or filename fail to open capture.
+    %   One of the following predefined presets:
+    %   * __lena__ noisy lena image
+    %   * __chess__ noisy lena image with a moving 10x7 chess board
+    %   * __aruco__ noisy lena image with a moving ArUco board
+    %   * __charuco__ noisy lena image with a moving ChArUco board
+    %   * __book__ box image rotating in front of graffiti image
+    %   * __cube__ cube moving in front of a background image
+    %   * anything else, 640x480 white noise
+    %
+    % ### Example
+    %
+    %     cap = createVideoCapture('synth|class=chess|bg=../test/fruits.jpg|noise=0.3|size=320x240');
+    %     img = cap.read();
+    %     h = imshow(img);
+    %     while ishghandle(h)
+    %         img = cap.read();
+    %         set(h, 'CData',img)
+    %         drawnow
+    %     end
+    %
+    % ### Example
+    %
+    %     % open first webcam, if fails fallback to fake lena video
+    %     cap = createVideoCapture(0, 'lena');
+    %     assert(cap.isOpened());
+    %     cap.release();
+    %
+    % ### Sources:
+    %
+    % * https://github.com/opencv/opencv/blob/3.3.0/samples/python/video.py
+    %
+    % See also: cv.VideoCapture, VideoSynthBase, VideoSynthScene,
+    %  VideoSynthChess, VideoSynthAruco
+    %
+
+    narginchk(0,2);
+    if nargin < 1, source = 0; end
+    if nargin < 2, fallback = 'chess'; end
+    if isempty(source)
+        % presets
+        cap = createVideoCapture(synth_presets(fallback));
+    elseif isnumeric(source) || ~strncmpi(source, 'synth|', 6)
+        % devid or a filename
+        cap = cv.VideoCapture(source);
+        if ~cap.isOpened()
+            cap = createVideoCapture(synth_presets(fallback));
+        end
+    else
+        % parse 'synth|key=value|key=value' string
+        [params, klass] = parse_string(source);
+
+        % create VideoSynth of the specified class
+        switch klass
+            case 'aruco'
+                cap = VideoSynthAruco(params);
+            case 'chess'
+                cap = VideoSynthChess(params);
+            case 'scene'
+                cap = VideoSynthScene(params);
+            otherwise
+                cap = VideoSynthBase(params);
+        end
+    end
+end
+
+function [params, klass] = parse_string(source)
+    % parse key/value pairs
+    params = regexp(source(7:end), '\|', 'split');
+    params = cellfun(@(s) regexp(s, '=', 'split'), params, 'UniformOutput',false);
+    params = [params{:}];
+    params(cellfun(@isempty, params)) = [];
+    if mod(numel(params), 2) == 0
+        params(1:2:end) = lower(params(1:2:end));
+    end
+
+    % convert to struct, and parse string values
+    params = struct(params{:});
+    params = structfun(@parse_value, params, 'UniformOutput',false);
+
+    % get class param
+    if isfield(params, 'class')
+        klass = validatestring(params.class, {'base', 'scene', 'chess', 'aruco'});
+        params = rmfield(params, 'class');
+    else
+        klass = 'base';
+    end
+end
+
+function val = parse_value(str)
+    str = strtrim(str);
+
+    % string as-is (fallback case)
+    val = str;
+    if isempty(val)
+        return;
+    end
+
+    % numeric
+    tok = str2double(str);
+    if ~isnan(tok)
+        val = tok;
+        return;
+    end
+
+    % size
+    tok = regexp(str, '^(\d+)x(\d+)$', 'tokens', 'once', 'ignorecase');
+    if ~isempty(tok)
+        val = str2double(tok);
+        return;
+    end
+
+    % logical
+    tok = regexp(str, '^(true|false)$', 'tokens', 'once', 'ignorecase');
+    if ~isempty(tok)
+        val = strcmpi(tok, 'true');
+        return;
+    end
+
+    % image file path
+    [~,~,ext] = fileparts(str);
+    fmts = imformats();
+    fmts = strcat('.', [fmts.ext]);
+    if any(strcmpi(ext, fmts)) && exist(str, 'file') == 2
+        val = cv.imread(which(str), 'Color',true);
+        return;
+    end
+end
+
+function source = synth_presets(name)
+    source = {'synth', 'noise=0.1'};
+    %source{end+1} = 'framecount=300';
+    %source{end+1} = 'size=320x320';
+    switch lower(name)
+        case 'lena'
+            source{end+1} = ['bg=' mcv_which('lena.jpg')];
+        case 'chess'
+            source{end+1} = 'class=chess';
+            source{end+1} = ['bg=' mcv_which('lena.jpg')];
+        case 'aruco'
+            source{end+1} = 'class=aruco';
+            source{end+1} = ['bg=' mcv_which('lena.jpg')];
+            source{end+1} = 'fg=ArUcoBoard';
+        case 'charuco'
+            source{end+1} = 'class=aruco';
+            source{end+1} = ['bg=' mcv_which('lena.jpg')];
+            source{end+1} = 'fg=ChArUcoBoard';
+        case 'box'
+            source{end+1} = 'class=aruco';
+            source{end+1} = ['bg=' mcv_which('lena.jpg')];
+            source{end+1} = ['fg=' mcv_which('box.png')];
+        case 'book'
+            source{end+1} = 'class=scene';
+            source{end+1} = ['bg=' mcv_which('graf1.png')];
+            source{end+1} = ['fg=' mcv_which('box.png')];
+            source{end+1} = 'size=640x480';
+            source{end+1} = 'speed=1';
+        case 'cube'
+            source{end+1} = 'class=scene';
+            source{end+1} = ['bg=' mcv_which('fruits.jpg')];
+            source{end+1} = 'speed=1';
+            source{end+1} = 'deformation=true';
+        otherwise
+            source{end+1} = 'size=640x480';
+    end
+    source = strjoin(source, '|');
+end
+
+function p = mcv_which(fname)
+    p = fullfile(mexopencv.root(), 'test', fname);
+end
